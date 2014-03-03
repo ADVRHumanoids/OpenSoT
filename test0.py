6e0005389d68478cbdc63f5138f88b64e1c6f045
Enrico <Enrico.Mingo@iit.it>
2014-03-03 16:48:26 +0100

    Added tests for upper body.

--- test0.py
+++ test0.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
-
-
 import yarp
+import numpy as np
+
+print "This Test creates Trajectories for left and right arm end-effectors considering fixed Position desired"
 
 yarp.Network.init()
 
@@ -14,96 +15,73 @@ right_arm = yarp.BufferedPortBottle()
 right_arm.open("/desired/right_arm/position/ref:o")
 yarp.Network.connect("/desired/right_arm/position/ref:o", "/sot_VelKinCon/right_arm/set_ref:i")
 
-# initial posture
-bottle_left = left_arm.prepare()
-bottle_left.clear()
-bottle_left.addDouble(0.0)
-bottle_left.addDouble(0.2)
-bottle_left.addDouble(0.5)
+left_arm_x_0 = 0.119692
+left_arm_y_0 = 0.189703
+left_arm_z_0 = 0.519815
+left_arm_R_0 = 21.5
+left_arm_P_0 = -63.04
+left_arm_Y_0 = -22.06
 
-bottle_right = right_arm.prepare()
-bottle_right.clear()
-bottle_right.addDouble(0.0)
-bottle_right.addDouble(-0.2)
-bottle_right.addDouble(0.5)
-
-left_arm.write()
-right_arm.write()
-
-yarp.Time.delay(3.0)
-
-bottle_left = left_arm.prepare()
-bottle_left.clear()
-bottle_left.addDouble(0.3)
-bottle_left.addDouble(0.2)
-bottle_left.addDouble(0.5)
-
-bottle_right = right_arm.prepare()
-bottle_right.clear()
-bottle_right.addDouble(-0.3)
-bottle_right.addDouble(-0.2)
-bottle_right.addDouble(0.5)
-
-left_arm.write()
-right_arm.write()
-
-yarp.Time.delay(3.0)
-
-#second posture
-bottle_left = left_arm.prepare()
-bottle_left.clear()
-bottle_left.addDouble(0.3)
-bottle_left.addDouble(-0.1)
-bottle_left.addDouble(0.5)
-
-bottle_right = right_arm.prepare()
-bottle_right.clear()
-bottle_right.addDouble(-0.3)
-bottle_right.addDouble(0.1)
-bottle_right.addDouble(0.5)
-
-left_arm.write()
-right_arm.write()
-
-yarp.Time.delay(3.0)
-
-#closing on valve
-bottle_left = left_arm.prepare()
-bottle_left.clear()
-bottle_left.addDouble(0.3)
-bottle_left.addDouble(0.2)
-bottle_left.addDouble(0.5)
-
-bottle_right = right_arm.prepare()
-bottle_right.clear()
-bottle_right.addDouble(-0.3)
-bottle_right.addDouble(-0.2)
-bottle_right.addDouble(0.5)
-
-left_arm.write()
-right_arm.write()
-
-yarp.Time.delay(4.0)
-
-bottle_left = left_arm.prepare()
-bottle_left.clear()
-bottle_left.addDouble(0.12)
-bottle_left.addDouble(0.19)
-bottle_left.addDouble(0.52)
-
-bottle_right = right_arm.prepare()
-bottle_right.clear()
-bottle_right.addDouble(0.12)
-bottle_right.addDouble(-0.19)
-bottle_right.addDouble(0.52)
-
-left_arm.write()
-right_arm.write()
-
-yarp.Time.delay(3.0)
-
-left_arm.close()
-right_arm.close()
+left_arm_x_f = 0.119692
+left_arm_y_f = 0.189703
+left_arm_z_f = 0.819815
 
 
-yarp.Network.fini();
+right_arm_x_0 = 0.119692
+right_arm_y_0 = -0.189703
+right_arm_z_0 = 0.519815
+right_arm_R_0 = -21.5
+right_arm_P_0 = -63.04
+right_arm_Y_0 = 22.06
+
+r = 0.08
+
+t_start = yarp.Time.now()
+while(1):
+    t = yarp.Time.now() - t_start
+    l = 0.5 * (-np.cos( 0.4*t%(2 * np.pi) ) + 1)
+        
+    left_arm_x = left_arm_x_0 + l*(left_arm_x_f - left_arm_x_0)
+    left_arm_y = left_arm_y_0 + l*(left_arm_y_f - left_arm_y_0)
+    left_arm_z = left_arm_z_0 + l*(left_arm_z_f - left_arm_z_0)
+    
+    right_arm_x = right_arm_x_0
+    right_arm_y = right_arm_y_0 + r*( np.cos(0.5*t%(2 * np.pi)) - 1 )
+    right_arm_z = right_arm_z_0 + r*( np.sin(0.5*t%(2 * np.pi)) )
+
+    bottle_left = left_arm.prepare()
+    bottle_left.clear()
+    
+    bottle_tmp0 = bottle_left.addList()
+    bottle_tmp0.addString("frame")
+    bottle_tmp0.addString("base_link")        
+    
+    bottle_tmp = bottle_left.addList()
+    bottle_tmp.addString("data")
+    bottle_tmp.addDouble(left_arm_x)
+    bottle_tmp.addDouble(left_arm_y)
+    bottle_tmp.addDouble(left_arm_z)
+    bottle_tmp.addDouble(left_arm_R_0)
+    bottle_tmp.addDouble(left_arm_P_0)
+    bottle_tmp.addDouble(left_arm_Y_0)
+
+    bottle_right = right_arm.prepare()
+    bottle_right.clear()
+    
+    bottle_tmp0 = bottle_right.addList()
+    bottle_tmp0.addString("frame")
+    bottle_tmp0.addString("base_link")      
+    
+    bottle_tmp = bottle_right.addList()
+    bottle_tmp.addString("data")
+    bottle_tmp.addDouble(right_arm_x)
+    bottle_tmp.addDouble(right_arm_y)
+    bottle_tmp.addDouble(right_arm_z)
+    bottle_tmp.addDouble(right_arm_R_0)
+    bottle_tmp.addDouble(right_arm_P_0)
+    bottle_tmp.addDouble(right_arm_Y_0)
+    
+    left_arm.write()
+    right_arm.write()
+    yarp.Time.delay(0.01)
+
