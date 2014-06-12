f0a5bfda7c678870b629e3fcc7e42634cb772c6f
Alessio Rocchi <alessio.rocchi@iit.it>
2014-06-12 18:04:02 +0200

    CleanBoardWhite is now working

--- python/tests/sot_demos/sot_CleanBoardWhite.py
+++ python/tests/sot_demos/sot_CleanBoardWhite.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
-
+from __future__ import print_function
 import os
 import yarp
 import numpy as np
-from __future__ import print_function
 import PyKDL as kdl
 from urdf_parser_py.urdf import URDF
 from urdf_parser_py.urdf import Robot
@@ -12,11 +11,21 @@ from pykdl_utils.kdl_kinematics import KDLKinematics
 
 __author__ = 'Alessio Rocchi'
 
-def yarpPose2KDLPose(poseBottle):
-    l = poseBottle.get(1).asDouble()
-    v = kdl.Vector(l.get(1), l.get(2), l.get(3))
-    r = kdl.Rotation.EulerZYX(l.get(4), l.get(5), l.get(6))
+def numPyToFrame(mat):
+    v = kdl.Vector(mat[0,3],mat[1,3],mat[2,3])
+    r = kdl.Rotation()
+    frame = kdl.Frame(r, v)
+    for i in range(3):
+        for j in range(3):
+            frame.M[i,j] = mat[i,j]
 
+    return frame
+
+def yarpPose2KDLPose(poseBottle):
+    l = poseBottle.get(1).asList()
+    v = kdl.Vector(l.get(1).asDouble(), l.get(2).asDouble(), l.get(3).asDouble())
+    #r = kdl.Rotation.RPY(l.get(4).asDouble(), l.get(5).asDouble(), l.get(6).asDouble())
+    r = kdl.Rotation()
     return kdl.Frame(r, v)
 
 def KDLPose2yarpPose(kdlPose, bottle):
@@ -26,7 +35,7 @@ def KDLPose2yarpPose(kdlPose, bottle):
     tmp0.addString("frame")
     tmp0.addString("world")
     p = kdlPose.p
-    r = kdlPose.M.getEulerZYX()
+    r = kdlPose.M.GetQuaternion()
     tmp = bottle_right.addList()
     tmp.addString("data")
     tmp.addDouble(p[0])
@@ -35,18 +44,19 @@ def KDLPose2yarpPose(kdlPose, bottle):
     tmp.addDouble(r[0])
     tmp.addDouble(r[1])
     tmp.addDouble(r[2])
+    tmp.addDouble(r[3])
 
     return bottle
 
 def yarpListToTuple(listBottle):
     tuple = []
     for i in range(listBottle.size()):
-        tuple += [listBottle.get(i)]
+        tuple += [listBottle.get(i).asDouble()]
     return tuple
 
 if __name__ == '__main__':
 
-    print "This Test creates Trajectories for left and right arm end-effectors"
+    print("This Test creates Trajectories for left and right arm end-effectors")
 
     yarp.Network.init()
 
@@ -60,6 +70,7 @@ if __name__ == '__main__':
     print(tree.getNrOfSegments())
 
     base_link = robot.get_root()
+    #base_link = 'base_link'
     end_link = 'r_wrist'
     chain = tree.getChain(base_link, end_link)
     print('Right arm & waist have:', chain.getNrOfJoints(), 'joints')
@@ -69,59 +80,57 @@ if __name__ == '__main__':
 
     q_reader_right_arm = yarp.BufferedPortBottle()
     q_reader_right_arm.open("/reader/right_arm/state:i")
-    yarp.Network.connect("/reader/right_arm/state:i", "/coman/right_arm/state:o")
+    yarp.Network.connect("/coman/right_arm/state:o", "/reader/right_arm/state:i")
 
     q_reader_torso = yarp.BufferedPortBottle()
     q_reader_torso.open("/reader/torso/state:i")
-    yarp.Network.connect("/reader/torso/state:i", "/coman/torso/state:o")
+    yarp.Network.connect("/coman/torso/state:o", "/reader/torso/state:i")
 
     world_pose_reader = yarp.BufferedPortBottle()
     world_pose_reader.open("/reader/world_to_base_link_pose:i")
-    yarp.Network.connect("/reader/world_to_base_link_pose:i", "/sot_VelKinCon/world_to_base_link_pose:o")
-
-
-    world_pose = world_pose_reader.read()
+    yarp.Network.connect("/sot_VelKinCon/world_to_base_link_pose:o", "/reader/world_to_base_link_pose:i")
 
     right_arm = yarp.BufferedPortBottle()
     right_arm.open("/desired/right_arm/position/ref:o")
     yarp.Network.connect("/desired/right_arm/position/ref:o", "/sot_VelKinCon/right_arm/set_ref:i")
 
-    left_arm_x_f = 0.119692
-    left_arm_y_f = 0.189703
-    left_arm_z_f = 0.819815
-    left_arm_R_f = 0.0
-    left_arm_P_f = -45.0
-    left_arm_Y_f = 0.0
-
-    r = 0.08
-
     q_torso = yarpListToTuple(q_reader_torso.read())
     q_right_arm = yarpListToTuple(q_reader_right_arm.read())
 
-    q = np.array(q_torso+q_right_arm)
+    q = np.array(q_torso+q_right_arm) * np.pi/180
 
     world_pose = yarpPose2KDLPose(world_pose_reader.read())
-    pose = kdl_kin.forward(q) # forward kinematics (returns homogeneous 4x4 numpy.mat)
-    pose_w = world_pose*pose
-    r_wrist_pose_0 = pose_w
+    poseNumpy = kdl_kin.forward(q) # forward kinematics (returns homogeneous 4x4 numpy.mat)
+    pose = numPyToFrame(poseNumpy)
 
-    print('right arm + torso q at t0 are:', q)
+    print("world pose is:\n", world_pose)
+    print("r_wrist wrt base_link is:\n", pose)
 
-    print('r_wrist pose at t0 wrt world is:', r_wrist_pose_0)
+    w_pose = world_pose*pose
+    r_wrist_pose_0 = w_pose
+
+    print('right arm + torso q at t0 are:\n', q)
+
+    print('r_wrist pose at t0 wrt world is:\n', r_wrist_pose_0)
 
     t_start = yarp.Time.now()
     while(1):
         t = yarp.Time.now() - t_start
-        l = 0.5 * (-np.cos( 0.4*t%(2 * np.pi) ) + 1)
+        l = 0.1 * (-np.cos( 0.1 * t%(2 * np.pi) ) + 1)
 
         pose_offset = kdl.Frame()
-        pose_offset.p = kdl.Vector(0,0,l)
+        pose_offset.p = kdl.Vector(0, 0, l)
 
-        r_wrist_pose_t = r_wrist_pose_0*pose_offset
+        r_wrist_pose_t = pose_offset*r_wrist_pose_0
 
         bottle_right = right_arm.prepare()
 
-        KDLPose2yarpPose(r_wrist_pose_t,bottle_right)
+        KDLPose2yarpPose(r_wrist_pose_t, bottle_right)
+
+        print("Zero Pose = ", r_wrist_pose_0)
+        print("Desired Pose = ", r_wrist_pose_t)
 
         right_arm.write()
+
         yarp.Time.delay(0.01)
+        #yarp.Time.delay(1)
