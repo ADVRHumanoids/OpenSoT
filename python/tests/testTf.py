a3cf967fe104373e124249b745139dd8bef98c6c
Enrico Mingo <enrico.mingo@iit.it>
2014-07-01 18:20:59 +0200

    Added working test using tf

--- python/tests/testTf.py
+++ python/tests/testTf.py
@@ -7,52 +7,55 @@ import geometry_msgs.msg
 import turtlesim.srv
 import yarp
 
+print("Test TF")
+
 if __name__ == '__main__':
     rospy.init_node('testTf')
 
     yarp.Network.init()
 
-    left_arm = yarp.BufferedPortBottle()
-    left_arm.open("/desired/left_arm/position/ref:o")
-    yarp.Network.connect("/desired/left_arm/position/ref:o", "/sot_VelKinCon/left_arm/set_ref:i")
-
-    right_arm = yarp.BufferedPortBottle()
-    right_arm.open("/desired/right_arm/position/ref:o")
-    yarp.Network.connect("/desired/right_arm/position/ref:o", "/sot_VelKinCon/right_arm/set_ref:i")
+    CoM = yarp.BufferedPortBottle()
+    CoM.open("/desired/CoM/position/ref:o")
+    yarp.Network.connect("/desired/CoM/position/ref:o", "/sot_VelKinCon/com/set_ref:i")
 
     listener = tf.TransformListener()
 
     success = False
     while not success:
         try:
-            (l_sole_trans_l_wrist, l_sole_rot_l_wrist) = listener.lookupTransform('/l_sole', '/l_wrist', rospy.Time(0))
+            (l_sole_pos_CoM, l_sole_rot_CoM) = listener.lookupTransform('/l_sole', '/CoM', rospy.Time(0))
             success = True
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            print("ERROR on lookupTransform")
             continue
 
-    success = False
-    while not success:
-        try:
-            (l_sole_trans_Waist, l_sole_rot_Waist) = listener.lookupTransform('/l_sole', '/Waist', rospy.Time(0))
-            success = True
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-            continue
+    print("CoM initial position is:")
+    print l_sole_pos_CoM
+
+    com_x_d = l_sole_pos_CoM[0]
+    com_y_d= l_sole_pos_CoM[1]
+    com_z_d = l_sole_pos_CoM[2] - 0.05
+
+    bottle_CoM = CoM.prepare()
+    bottle_CoM.clear()
+
+    bottle_tmp0 = bottle_CoM.addList()
+    bottle_tmp0.addString("frame")
+    bottle_tmp0.addString("l_sole")
+
+    bottle_tmp = bottle_CoM.addList()
+    bottle_tmp.addString("data")
+    bottle_tmp.addDouble(com_x_d)
+    bottle_tmp.addDouble(com_y_d)
+    bottle_tmp.addDouble(com_z_d)
+    bottle_tmp.addDouble(0.0)
+    bottle_tmp.addDouble(0.0)
+    bottle_tmp.addDouble(0.0)
+
+    CoM.write()
+
+    yarp.Time.delay(0.01)
 
 
-    world_trans_l_sole = (l_sole_trans_Waist[0], -l_sole_trans_Waist[1], 0.0)
-    world_rot_l_sole = (1.0, 0.0, 0.0, 0.0)
 
-    world_trans_l_wrist = world_trans_l_sole + l_sole_trans_l_wrist
-    world_rot_l_wrist = l_sole_rot_l_wrist
-    print world_trans_l_wrist
-    print world_rot_l_wrist
-
-
-
-    left_arm_x_0 = 0.119692
-    left_arm_y_0 = 0.189703
-    left_arm_z_0 = 0.519815
-    left_arm_R_0 = 21.5
-    left_arm_P_0 = -63.04
-    left_arm_Y_0 = -22.06
 
