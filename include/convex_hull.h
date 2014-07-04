57b9a5f5f86851f62e75ad534ac61f6090ee7c4d
Enrico Mingo <enrico.mingo@iit.it>
2014-07-04 11:51:40 +0200

    STarting implementation of convex hull computation

--- include/convex_hull.h
+++ include/convex_hull.h
@@ -10,58 +10,16 @@
 namespace wb_sot
 {
 
-struct foot_reference_frames
-{
-    KDL::Frame upper_left;
-    KDL::Frame upper_right;
-    KDL::Frame lower_left;
-    KDL::Frame lower_right;
-
-    static std::string _UPPER_LEFT;
-    static std::string _LOWER_LEFT;
-    static std::string _UPPER_RIGHT;
-    static std::string _LOWER_RIGHT;
-};
-
 class convex_hull
 {
 public:
     convex_hull(){}
-    /**
-     * @brief convex_hull
-     * @param robot_urdf
-     * @param l_ankle_CoM pose from left ankle to CoM
-     * @param r_ankle_CoM pose from right ankle to CoM
-     */
-    void init(const boost::shared_ptr<const urdf::ModelInterface> &robot_urdf,
-                const KDL::Frame& l_ankle_CoM, const KDL::Frame& r_ankle_CoM);
     ~convex_hull();
 
-    void updateCoMPosition(const KDL::Frame& l_ankle_CoM, const KDL::Frame& r_ankle_CoM)
-    {
-        _l_ankle_CoM = l_ankle_CoM;
-        _r_ankle_CoM = r_ankle_CoM;
-    }
-
-    void computeConvexHull();
-
+    void getConvexHull(const std::list<KDL::Vector>& points);
 private:
-    boost::shared_ptr<const urdf::ModelInterface> _robot_urdf;
-    foot_reference_frames _l_foot; //from left ankle to reference frame in sole
-    foot_reference_frames _r_foot; //from right ankle to reference frame in sole
-    KDL::Frame _l_ankle_CoM; //From left ankle to CoM
-    KDL::Frame _r_ankle_CoM; //From left ankle to CoM
-
-    /**
-     * @brief getRefFrames from ankle to sole
-     * @param foot_name
-     * @param sole_name
-     * @param foot
-     */
-    void getRefFrames(const std::string& foot_name, const std::string &sole_name,
-                      foot_reference_frames& foot);
-
-    void URDFPose2KDLFrame(const urdf::Pose& pose, KDL::Frame& T);
+    pcl::PointXY fromKDLVector2PCLPointXY(const KDL::Vector& point);
+    pcl::PointCloud<pcl::PointXY> fromSTDList2PCLPointCloud(const std::list<KDL::Vector>& points);
 };
 
 }
--- src/convex_hull.cpp
+++ src/convex_hull.cpp
@@ -1,55 +1,11 @@
 #include "convex_hull.h"
+#include <pcl/surface/convex_hull.h>
 
 using namespace wb_sot;
 
-std::string foot_reference_frames::_UPPER_LEFT = "_upper_left_joint";
-std::string foot_reference_frames::_LOWER_LEFT = "_lower_left_joint";
-std::string foot_reference_frames::_UPPER_RIGHT = "_upper_right_joint";
-std::string foot_reference_frames::_LOWER_RIGHT = "_lower_right_joint";
-
-
-
-void convex_hull::init(const boost::shared_ptr<const urdf::ModelInterface> &robot_urdf,
-                         const KDL::Frame &l_ankle_CoM, const KDL::Frame &r_ankle_CoM)
+convex_hull::convex_hull()
 {
-    _robot_urdf = robot_urdf;
-    _l_ankle_CoM = l_ankle_CoM;
-    _r_ankle_CoM = r_ankle_CoM;
 
-    getRefFrames("l_foot", "l_sole_joint", _l_foot);
-    getRefFrames("r_foot", "r_sole_joint", _r_foot);
-    std::cout<<"INITIALIZED CONVEX HULL"<<std::endl;
-}
-
-void convex_hull::getRefFrames(const std::string &foot_name, const std::string &sole_name, foot_reference_frames &foot)
-{   
-    boost::shared_ptr<const urdf::Joint> joint_sole = _robot_urdf->getJoint(sole_name);
-    urdf::Pose pose = joint_sole->parent_to_joint_origin_transform;
-    KDL::Frame sole_frame;
-    URDFPose2KDLFrame(pose, sole_frame);
-
-    std::cout<<foot_name + foot_reference_frames::_LOWER_LEFT<<std::endl;
-    boost::shared_ptr<const urdf::Joint> joint = _robot_urdf->getJoint(foot_name + foot_reference_frames::_LOWER_LEFT);
-    pose = joint->parent_to_joint_origin_transform;
-    URDFPose2KDLFrame(pose, foot.lower_left);
-
-    joint = _robot_urdf->getJoint(foot_name + foot_reference_frames::_LOWER_RIGHT);
-    pose = joint->parent_to_joint_origin_transform;
-    URDFPose2KDLFrame(pose, foot.lower_right);
-
-    joint = _robot_urdf->getJoint(foot_name + foot_reference_frames::_UPPER_LEFT);
-    pose = joint->parent_to_joint_origin_transform;
-    URDFPose2KDLFrame(pose, foot.upper_left);
-
-    joint = _robot_urdf->getJoint(foot_name + foot_reference_frames::_UPPER_RIGHT);
-    pose = joint->parent_to_joint_origin_transform;
-    URDFPose2KDLFrame(pose, foot.upper_right);
-
-    //Since we need from ankle...
-    foot.lower_left = sole_frame * foot.lower_left;
-    foot.lower_right = sole_frame * foot.lower_right;
-    foot.upper_left = sole_frame * foot.upper_left;
-    foot.upper_right = sole_frame * foot.upper_right;
 }
 
 convex_hull::~convex_hull()
@@ -57,40 +13,45 @@ convex_hull::~convex_hull()
 
 }
 
-void convex_hull::URDFPose2KDLFrame(const urdf::Pose &pose, KDL::Frame &T)
-{
-    double qx, qy, qz, qw;
-    pose.rotation.getQuaternion(qx, qy, qz, qw);
-    T.M = T.M.Quaternion(qx, qy, qz, qw);
+void convex_hull::getConvexHull(const std::list<KDL::Vector>& points) {
+    pcl::PointCloud<pcl::PointXY> pointCloud = this->fromSTDList2PCLPointCloud(points);
+    pcl::PointCloud<pcl::PointXY> pointsInConvexHull;
+    std::vector<pcl::Vertices> indicesOfVertexes;
 
-    T.p = KDL::Vector(pose.position.x, pose.position.y, pose.position.z);
-}
+    // estimate the convex hull in camera frame
+    pcl::ConvexHull<PointXY> huller;
+    huller.setInputCloud (pointCloud);
+    huller.reconstruct (pointsInConvexHull,
+                        indicesOfVertexes);
+    if(indicesOfVertexes.size() != 1) {
+        std::cerr << "Error: more than one polygon found!" << std::endl;
+        return
+    } else
+        pcl::Vertices hullVertices = indicesOfVertexes[0];
 
-void convex_hull::computeConvexHull()
-{
-    //We compute the transformation from CoM to reference frames in foot
-    foot_reference_frames l_foot_CoM;
-    l_foot_CoM.upper_right = _l_ankle_CoM.Inverse() * _l_foot.upper_right;
-    l_foot_CoM.upper_left = _l_ankle_CoM.Inverse() * _l_foot.upper_left;
-    l_foot_CoM.lower_right = _l_ankle_CoM.Inverse() * _l_foot.lower_right;
-    l_foot_CoM.lower_left = _l_ankle_CoM.Inverse() * _l_foot.lower_left;
 
-    std::cout<<"l_foot upper_right:"<<std::endl;cartesian_utils::printKDLFrame(l_foot_CoM.upper_right);
-    std::cout<<"l_foot upper_left:"<<std::endl;cartesian_utils::printKDLFrame(l_foot_CoM.upper_left);
-    std::cout<<"l_foot lower_right:"<<std::endl;cartesian_utils::printKDLFrame(l_foot_CoM.lower_right);
-    std::cout<<"l_foot lower_left:"<<std::endl;cartesian_utils::printKDLFrame(l_foot_CoM.lower_left);
-
-    foot_reference_frames r_foot_CoM;
-    r_foot_CoM.upper_right = _r_ankle_CoM.Inverse() * _r_foot.upper_right;
-    r_foot_CoM.upper_left = _r_ankle_CoM.Inverse() * _r_foot.upper_left;
-    r_foot_CoM.lower_right = _r_ankle_CoM.Inverse() * _r_foot.lower_right;
-    r_foot_CoM.lower_left = _r_ankle_CoM.Inverse() * _r_foot.lower_left;
-
-    std::cout<<"r_foot upper_right:"<<std::endl;cartesian_utils::printKDLFrame(r_foot_CoM.upper_right);
-    std::cout<<"r_foot upper_left:"<<std::endl;cartesian_utils::printKDLFrame(r_foot_CoM.upper_left);
-    std::cout<<"r_foot lower_right:"<<std::endl;cartesian_utils::printKDLFrame(r_foot_CoM.lower_right);
-    std::cout<<"r_foot lower_left:"<<std::endl;cartesian_utils::printKDLFrame(r_foot_CoM.lower_left);
+    // hullVertices.vertices is the list of vertices...
+    // by taking each point and the consequent in the list
+    // (i.e. vertices[1]-vertices[0] it is possible to compute
+    // bounding segments for the hull
 
 }
 
+pcl::PointXY convex_hull::fromKDLVector2PCLPointXY(const KDL::Vector &point)
+{
+    pcl::PointXY p;
+    p.x = point.x();
+    p.y = point.y();
+    return p;
+}
 
+pcl::PointCloud<pcl::PointXY> convex_hull::fromSTDList2PCLPointCloud(
+        const std::list<KDL::Vector> &points)
+{
+    pcl::PointCloud<pcl::PointXY> pointCloud;
+    for(std::list<KDL::Vector>::iterator i = points.begin();
+        i != points.end();
+        ++i)
+        pointCloud.push_back(this->fromKDLVector2PCLPointXY(*i));
+    return pointCloud;
+}
