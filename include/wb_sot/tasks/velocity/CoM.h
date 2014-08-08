985bed6bd5da001190eb954852e4bba05ba909d6
Alessio Rocchi <alessio.rocchi@iit.it>
2014-08-08 11:38:58 +0200

    Added first version of CoM task (without constraints). Need to test it

--- include/wb_sot/tasks/velocity/CoM.h
+++ include/wb_sot/tasks/velocity/CoM.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (C) 2014 Walkman
- * Author: Alessio Rocchi
- * email:  alessio.rocchi@iit.it
+ * Authors:Alessio Rocchi, Enrico Mingo
+ * email:  alessio.rocchi@iit.it, enrico.mingo@iit.it
  * Permission is granted to copy, distribute, and/or modify this program
  * under the terms of the GNU Lesser General Public License, version 2 or any
  * later version published by the Free Software Foundation.
@@ -15,14 +15,71 @@
  * Public License for more details
 */
 
+#ifndef __TASKS_VELOCITY_CARTESIAN_H__
+#define __TASKS_VELOCITY_CARTESIAN_H__
+
  #include <wb_sot/Task.h>
+ #include <drc_shared/idynutils.h>
+ #include <drc_shared/utils/convex_hull.h>
+ #include <kdl/frames.hpp>
+ #include <yarp/sig/all.h>
+#include <yarp/os/all.h>
 
  namespace wb_sot {
     namespace tasks {
-        template <class Matrix_type, class Vector_type,
-                  unsigned int x_size>
-        class CoM : public Task<Matrix_type, Vector_type, x_size> {
-            
+        namespace velocity {
+            class CoM : public Task < yarp::sig::Matrix, yarp::sig::Vector > {
+            private:
+                iDynUtils& _robot;
+
+                /** TODO add floating base support! */
+                /**
+                 * @brief _support_foot_link_index in fixed base systems,
+                 *                                 we want to specify the CoM wrt
+                 *                                 a fixed root.
+                 */
+                int _support_foot_link_index;
+                /**
+                 * @brief _swing_foot_link_index during single foot support phase,
+                 *                               the swing foot is considered like an end-effector
+                 */
+                int _swing_foot_link_index;
+
+                /** TODO we could have the task itself, or maybe a wrapper, take care of ports? */
+                //yarp::os::BufferedPort<yarp::os::Bottle> _referenceInputPort;
+
+                yarp::sig::Vector _actualPosition;
+                yarp::sig::Vector _desiredPosition;
+
+                /** TODO should this go inside idynutils?
+                         it is true if we are owners of the robot model */
+                bool _updateModel;
+
+            public:
+
+                yarp::sig::Vector positionError;
+                yarp::sig::Vector orientationError;
+
+                /*********** TASK PARAMETERS ************/
+
+                double orientationErrorGain;
+
+                /****************************************/
+
+
+                CoM(const yarp::sig::Vector& x,
+                    iDynUtils &robot,
+                    const unsigned int x_size,
+                    const bool updateModel = false);
+
+                ~CoM();
+
+                void update(const yarp::sig::Vector& x);
+
+                void setReference(const yarp::sig::Vector& desiredPosition);
+            };
         }
     }
  }
+
+#endif
---
+++ src/wb_sot/tasks/velocity/CoM.cpp
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2014 Walkman
+ * Author: Alessio Rocchi, Enrico Mingo
+ * email:  alessio.rocchi@iit.it, enrico.mingo@iit.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU Lesser General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+#include <wb_sot/tasks/velocity/CoM.h>
+#include <yarp/math/Math.h>
+#include <drc_shared/cartesian_utils.h>
+#include <exception>
+#include <cmath>
+
+using namespace wb_sot::tasks::velocity;
+using namespace yarp::math;
+
+CoM::CoM(   const yarp::sig::Vector& x,
+            iDynUtils &robot,
+            const unsigned int x_size,
+            const bool updateModel) :
+    Task("com", x, x_size), _robot(robot), _updateModel(updateModel)
+{
+    _support_foot_link_index = _robot.left_leg.index;
+    _swing_foot_link_index = _robot.right_leg.index;
+
+    /* first update. Setting desired pose equal to the actual pose */
+    this->update(_x0);
+
+    /* initializing to zero error */
+    _desiredPosition = _actualPosition;
+    _b = _zeroVector;
+
+    //_referenceInputPort.open("/wb_sot/tasks/velocity/CoM/" + _task_id + "/set_ref:i");
+}
+
+CoM::~CoM()
+{
+   //_referenceInputPort.close();
+}
+
+void CoM::update(const yarp::sig::Vector &x) {
+    /** TODO when using a cartesian task, we could update the model at the aggregate level
+             instead of each cartesian task, to save computation time */
+    if(_updateModel)
+        _robot.updateiDyn3Model(x, _zeroVector, _zeroVector);
+
+    /************************* COMPUTING TASK *****************************/
+
+    _actualPosition = _robot.coman_iDyn3.getCOM("",_support_foot_link_index);
+    //This part of code is an HACK due to a bug in iDynTree
+    int floating_base_old_index = _robot.coman_iDyn3.getFloatingBaseLink();
+
+    _robot.coman_iDyn3.setFloatingBaseLink(_support_foot_link_index);
+    assert(_robot.coman_iDyn3.getCOMJacobian(_A));
+    _robot.coman_iDyn3.setFloatingBaseLink(floating_base_old_index);
+
+    _A = _A.removeCols(0,6);    // remove floating base
+    _A = _A.removeRows(3,3);    // remove orientation
+
+    _b = _desiredPosition - _actualPosition;
+    /**********************************************************************/
+}
+
+void CoM::setReference(const yarp::sig::Vector& desiredPosition) {
+    _desiredPosition = desiredPosition;
+}
+
+
+
