f7a45786c6c8136826fe277bb1967cfcaec113cb
Alessio Rocchi <alessio.rocchi@iit.it>
2014-10-08 12:09:43 +0200

    Test for Aggregated is more powerful. Fixes issues #1

--- tests/tasks/TestAggregated.cpp
+++ tests/tasks/TestAggregated.cpp
@@ -3,6 +3,7 @@
 #include <OpenSoT/tasks/Aggregated.h>
 #include <OpenSoT/tasks/velocity/Postural.h>
 #include <yarp/math/Math.h>
+#include <yarp/math/SVD.h>
 
 using namespace yarp::math;
 using namespace OpenSoT::tasks;
@@ -70,6 +71,57 @@ TEST_F(testAggregatedTask, testAggregatedTask_)
     EXPECT_DOUBLE_EQ(aggregated.getLambda(), K);
 
     EXPECT_TRUE(aggregated.getWeight() == yarp::sig::Matrix(q.size()*2, q.size()*2).eye());
+
+    iDynUtils idynutils;
+    yarp::sig::Vector q(idynutils.coman_iDyn3.getNrOfDOFs(), 0.0);
+    idynutils.updateiDyn3Model(q, true);
+    yarp::sig::Vector q_ref(q.size(), M_PI);
+
+    boost::shared_ptr<OpenSoT::tasks::velocity::Postural> postural_in_aggregated(
+            new OpenSoT::tasks::velocity::Postural(q));
+    postural_in_aggregated->setReference(q_ref);
+    std::list<OpenSoT::Task<yarp::sig::Matrix,yarp::sig::Vector>::TaskPtr> task_list;
+    task_list.push_back(postural_in_aggregated);
+    boost::shared_ptr<OpenSoT::tasks::Aggregated> aggregated_task(
+                new OpenSoT::tasks::Aggregated(task_list, q.size()));
+    aggregated_task->setLambda(0.1);
+
+    boost::shared_ptr<OpenSoT::tasks::velocity::Postural> postural_task(
+            new OpenSoT::tasks::velocity::Postural(q));
+    postural_task->setReference(q_ref);
+    postural_task->setLambda(0.1);
+
+//1. Here we use postural_task
+
+    yarp::sig::Vector dq(q.size(), 0.0);
+    for(unsigned int i = 0; i < 1000; ++i)
+    {
+        postural_task->update(q);
+        aggregated_task->update(q);
+        EXPECT_TRUE(aggregated_task->getA() == postural_task->getA());
+        EXPECT_TRUE(aggregated_task->getb() == postural_task->getb());
+        dq = pinv(postural_task->getA(),1E-7)*postural_task->getLambda()*postural_task->getb();
+        q += dq;
+    }
+
+    for(unsigned int i = 0; i < q_ref.size(); ++i)
+        EXPECT_NEAR(q[i],q_ref[i],1E-4);
+
+    dq.zero(); q.zero();
+
+    for(unsigned int i = 0; i < 1000; ++i)
+    {
+        postural_task->update(q);
+        aggregated_task->update(q);
+        EXPECT_TRUE(aggregated_task->getA() == postural_task->getA());
+        EXPECT_TRUE(aggregated_task->getb() == postural_task->getb()) << "aggregated_task b is " << aggregated_task->getb().toString() << std::endl
+                                                                      << " while postural_task b is " << postural_task->getb().toString();
+        dq = pinv(aggregated_task->getA(),1E-7)*aggregated_task->getLambda()*aggregated_task->getb();
+        q += dq;
+    }
+
+    for(unsigned int i = 0; i < q_ref.size(); ++i)
+        EXPECT_NEAR(q[i],q_ref[i],1E-4);
 }
 
 }
