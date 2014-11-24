5d5bae4a8e823fa6a97b171dcef469c49287e464
Alessio Rocchi <alessio.rocchi@iit.it>
2014-11-24 16:58:21 +0100

    Added Default Humanoid Stack

--- include/OpenSoT/OpenSoT.h
+++ include/OpenSoT/OpenSoT.h
@@ -28,4 +28,68 @@
  #include "tasks/Aggregated.h"
  #include "tasks/velocity/all.h"
 
+namespace OpenSoT {
+
+    class Factory {
+    private:
+
+        template <class Matrix_type, class Vector_type>
+        class Stack {
+            friend class OpenSoT::Factory;
+            typename OpenSoT::Solver<Matrix_type, Vector_type>::Stack stack;
+            virtual void update(const Vector_type& q);
+            operator typename OpenSoT::Solver<Matrix_type, Vector_type>::Stack();
+            virtual ~Stack() {}
+        protected:
+            Stack();
+            Stack(typename OpenSoT::Solver<Matrix_type, Vector_type>::Stack stack);
+        };
+
+        Factory();
+
+    public:
+        ~Factory() {}
+
+        class DefaultHumanoidStack : public Stack<yarp::sig::Matrix, yarp::sig::Vector>
+        {
+        private:
+            iDynUtils model;
+            iDynUtils model_com;
+
+            DefaultHumanoidStack(std::string srdfModelFileName,
+                                 const double dT);
+
+        public:
+            ~DefaultHumanoidStack() {}
+            friend class OpenSoT::Factory;
+            typedef boost::shared_ptr<OpenSoT::Factory::DefaultHumanoidStack> Ptr;
+
+            // tasks
+            const tasks::velocity::Cartesian::Ptr leftArm;
+            const tasks::velocity::Cartesian::Ptr rightArm;
+            const tasks::velocity::Cartesian::Ptr waist2LeftArm;
+            const tasks::velocity::Cartesian::Ptr waist2RightArm;
+            const tasks::velocity::Cartesian::Ptr leftLeg;
+            const tasks::velocity::Cartesian::Ptr rightLeg;
+            const tasks::velocity::Cartesian::Ptr rightLeg2LeftLeg;
+            const tasks::velocity::Cartesian::Ptr leftLeg2RightLeg;
+            const tasks::velocity::CoM::Ptr com;
+            const tasks::velocity::MinimumEffort::Ptr minimumEffort;
+            const tasks::velocity::Postural::Ptr postural;
+
+            // constraints
+            const constraints::velocity::CoMVelocity::Ptr comVelocity;
+            const constraints::velocity::ConvexHull::Ptr convexHull;
+            const constraints::velocity::JointLimits::Ptr jointLimits;
+            const constraints::velocity::VelocityLimits::Ptr velocityLimits;
+
+            void update(const yarp::sig::Vector& q);
+        };
+
+        DefaultHumanoidStack::Ptr getDefaultHumanoidStack(std::string srdfModelFileName,
+                                                          const double dT);
+    private:
+        static DefaultHumanoidStack::Ptr defaultHumanoidStack;
+    };
+}
 #endif
