b4062911e6059abd790475c38c4118bb09961d1f
Alessio Rocchi <alessio.rocchi@iit.it>
2015-01-06 12:06:30 +0100

    Introducing the SubTask

---
+++ include/OpenSoT/SubTask.h
@@ -0,0 +1,184 @@
+/*
+ * Copyright (C) 2014 Walkman
+ * Author: Alessio Rocchi
+ * email:  alessio.rocchi@iit.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU Lesser General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+#ifndef __SUBTASK_H__
+#define __SUBTASK_H__
+
+ #include <OpenSoT/Task.h>
+
+ #include <list>
+ #include <vector>
+ #include <string>
+ #include <cassert>
+ #include <boost/shared_ptr.hpp>
+ #include <yarp/sig/all.h>
+ #include <yarp/math/Math.h>
+ #include <yarp/math/SVD.h>
+ #include <sstream>
+
+ namespace OpenSoT {
+
+    /**
+     * @brief SubTask represents a task which is obtained as a sub task
+     * in the form \f$T(A_s,b_s)\f$,
+     * where \f$A_s\f$ is a reduced task error jacobian
+     * and \f$b_s\f$ its corresponding task error.
+     * Updating a SubTask merely calls the update method for the corresponding father
+     * task which it reduces, so when multiple SubTasks are created from the same
+     * Task, it is sufficient to update the full Task, and all the corresponding
+     * SubTasks will get automatically updated.
+     * In the same way, the constraints of the SubTask are those of the father Task,
+     * as well as the weight matrix W (the \f$W_\text{subtask}\f$ is a submatrix of \f$W\f$)
+     * On the other side, the \f$\lambda\f$ for the SubTask is unique to the SubTask.
+    */
+    class SubTask : public Task<yarp::sig::Matrix, yarp::sig::Vector> {
+
+    public:
+
+        typedef boost::shared_ptr<OpenSoT::SubTask> Ptr;
+
+        class SubTaskMap {
+        public:
+            /**
+             * @brief RowsChunk a vector<unsigned int> of rows which are contiguous
+             */
+            typedef std::vector<unsigned int> RowsChunk;
+            /**
+             * @brief ChunkList a list<vector<int>>, a list of contiguous chunks of rows
+             * e.g., {{1,2},{4},{10,11,12}}, is a list of 3 chunks of contiguous row indices,
+             * where the first chunk has size 2, the second chunk has size 1, the third chunk has size 3
+             */
+            typedef std::list< RowsChunk > ChunkList;
+        private:
+
+            ChunkList _contiguousChunks;
+            std::list<unsigned int> _rowsList;
+
+            /**
+             * @brief getNextAdjacentChunk returns an iterator pointing to the last element of a chunk
+             * @param searchBegin iterator pointing to the first element where we start looking for chunks
+             * @return the iterator pointing to the last element of a chunk
+             */
+            std::list<unsigned int>::iterator getNextAdjacentChunk(std::list<unsigned int>::iterator searchBegin);
+
+            void generateChunks();
+
+        public:
+            /**
+             * @brief SubTaskMap creates a SubTaskMap of size 1, with just one index
+             * @param i
+             */
+            SubTaskMap(unsigned int i);
+
+            SubTaskMap(const std::list<unsigned int>& rowsList);
+
+            SubTaskMap(const SubTaskMap& subTaskMap);
+
+            const ChunkList &getChunks() const;
+
+            const std::list<unsigned int> &getRowsList() const;
+
+            bool isContiguous() const;
+
+            int size() const;
+
+            static SubTaskMap range(unsigned int from, unsigned int to);
+
+            SubTaskMap operator+(const SubTaskMap& b) const;
+
+            SubTaskMap operator+(const unsigned int r) const;
+
+            bool operator==(const SubTaskMap& b) const;
+
+            operator std::string() const;
+
+            operator std::list<unsigned int>() const;
+        };
+
+    private:
+        TaskPtr _taskPtr;
+        SubTaskMap _subTaskMap;
+
+    protected:
+
+    public:
+        /**
+         * @brief SubTask create a SubTask object by specifying the father Task through a pointer,
+         * and a list of row indices. Notice the row indices start from 1 (Matlab style, rather than c style)
+         * @param taskPtr a pointer to the father task
+         * @param rowIndices a list of indices. The index to the first row is 1, not 0.
+         */
+        SubTask(TaskPtr taskPtr, const std::list<unsigned int> rowIndices);
+
+        virtual ~SubTask(){}
+
+        virtual const yarp::sig::Matrix& getA();
+
+        virtual const HessianType getHessianAtype();
+
+        virtual const yarp::sig::Vector& getb();
+
+        virtual const yarp::sig::Matrix& getWeight();
+
+        /**
+         * @brief setWeight sets the task weight.
+         * Note the Weight needs to be positive definite.
+         * If your original intent was to get a subtask
+         * (i.e., reduce the number of rows of the task Jacobian),
+         * please use the class SubTask
+         * @param W matrix weight
+         */
+        virtual void setWeight(const yarp::sig::Matrix& W);
+
+        /**
+         * @brief getConstraints return a reference to the constraint list. Use the standard list methods
+         * to add, remove, clear, ... the constraints list.
+         * e.g.:
+         *              task.getConstraints().push_back(new_constraint)
+         * Notice that in subtasks, you will get the constraint list of the father Task from which the SubTask
+         * is generated.
+         * @return the list of constraints to which the father Task is subject.
+         */
+        virtual std::list< ConstraintPtr >& getConstraints();
+
+        /** Gets the task size.
+            @return the number of rows of A */
+        virtual const unsigned int getTaskSize() const;
+
+        /** Updates the A, b, Aeq, beq, Aineq, b*Bound matrices
+            @param x variable state at the current step (input) */
+        virtual void _update(const yarp::sig::Vector &x);
+
+        /**
+         * @brief getActiveJointsMask return a vector of length NumberOfDOFs.
+         * If an element is false the corresponding column of the task jacobian is set to 0.
+         * @return a vector of bool
+         */
+        virtual std::vector<bool> getActiveJointsMask();
+
+        /**
+         * @brief setActiveJointsMask set a mask on the jacobian
+         * @param active_joints_mask
+         * @return true if success
+         */
+        virtual bool setActiveJointsMask(const std::vector<bool>& active_joints_mask);
+    };
+
+
+ }
+
+#endif
---
+++ src/tasks/SubTask.cpp
@@ -0,0 +1,286 @@
+#include "OpenSoT/SubTask.h"
+
+/******************
+ *  SUBTASKMAP    *
+ ******************/
+
+std::list<unsigned int>::iterator OpenSoT::SubTask::SubTaskMap::getNextAdjacentChunk(std::list<unsigned int>::iterator searchBegin)
+{
+    if(searchBegin != _rowsList.end())
+    {
+        for(std::list<unsigned int>::iterator i = searchBegin;
+            i != _rowsList.end();
+            ++i)
+        {
+            std::list<unsigned int>::iterator next = i; next++;
+            std::list<unsigned int>::iterator prev = i;
+
+            while(next != _rowsList.end() &&
+                  *next == *prev + 1) {
+                next++;
+                prev++;
+            }
+            return prev;
+        }
+    }
+    return _rowsList.end();
+}
+
+void OpenSoT::SubTask::SubTaskMap::generateChunks()
+{
+    _rowsList.sort(); _rowsList.unique();
+    this->_contiguousChunks.clear();
+
+    for(std::list<unsigned int>::iterator i = _rowsList.begin();
+        i!= _rowsList.end();
+        ++i)
+    {
+        std::list<unsigned int>::iterator chunkEnd =
+                this->getNextAdjacentChunk(i);
+        /* insert doesn't include the last extreme,
+         * so we need to increment chunkEnd */
+        if(chunkEnd != _rowsList.end())
+        {
+            std::list<unsigned int>::iterator chunkAfterEnd = chunkEnd;
+            ++chunkAfterEnd;
+            RowsChunk chunk; chunk.insert(chunk.end(), i, chunkAfterEnd);
+            this->_contiguousChunks.push_back(chunk);
+        }
+        i = chunkEnd;
+    }
+}
+
+OpenSoT::SubTask::SubTaskMap::SubTaskMap(unsigned int i)
+{
+    _rowsList.push_back(i);
+    this->generateChunks();
+}
+
+OpenSoT::SubTask::SubTaskMap::SubTaskMap(const std::list<unsigned int> &rowsList)
+    : _rowsList(rowsList)
+{
+    this->generateChunks();
+}
+
+OpenSoT::SubTask::SubTaskMap::SubTaskMap(const OpenSoT::SubTask::SubTaskMap &subTaskMap)
+    : _rowsList(subTaskMap.getRowsList())
+{
+    this->generateChunks();
+}
+
+const OpenSoT::SubTask::SubTaskMap::ChunkList& OpenSoT::SubTask::SubTaskMap::getChunks() const
+{
+    return _contiguousChunks;
+}
+
+const std::list<unsigned int>& OpenSoT::SubTask::SubTaskMap::getRowsList() const
+{
+    return _rowsList;
+}
+
+bool OpenSoT::SubTask::SubTaskMap::isContiguous() const
+{
+    return _contiguousChunks.size() == 1;
+}
+
+OpenSoT::SubTask::SubTaskMap OpenSoT::SubTask::SubTaskMap::range(unsigned int from, unsigned int to)
+{
+    std::list<unsigned int> rows;
+    assert(from<=to && "from must be lower or equal to to");
+    for(unsigned int row = from; row <= to; ++row)
+        rows.push_back(row);
+    return SubTaskMap(rows);
+}
+
+OpenSoT::SubTask::SubTaskMap::operator std::list<unsigned int>() const
+{
+    return this->getRowsList();
+}
+
+OpenSoT::SubTask::SubTaskMap::operator std::string() const
+{
+    std::stringstream subTaskIdSuffix;
+    if(_rowsList.size() == 0)
+        subTaskIdSuffix<<"empty";
+    else
+    {
+        // for each chunk..
+        for(ChunkList::const_iterator i = _contiguousChunks.begin();
+            i != _contiguousChunks.end();
+            ++i)
+        {
+            assert(i->size() != 0);
+
+            if(i->size() == 1) {
+                subTaskIdSuffix << i->front();
+            } else {
+                subTaskIdSuffix << i->front() << "-" << i->back();
+            }
+
+            if(i != _contiguousChunks.end() && i != --_contiguousChunks.end())
+                subTaskIdSuffix << "+";
+        }
+    }
+    subTaskIdSuffix.sync();
+
+    return subTaskIdSuffix.str();
+}
+
+OpenSoT::SubTask::SubTaskMap OpenSoT::SubTask::SubTaskMap::operator+(const OpenSoT::SubTask::SubTaskMap &b) const
+{
+    std::list<unsigned int> rows = this->_rowsList;
+    rows.insert(rows.end(), b.getRowsList().begin(), b.getRowsList().end());
+    return SubTaskMap(rows);
+}
+
+OpenSoT::SubTask::SubTaskMap OpenSoT::SubTask::SubTaskMap::operator+(const unsigned int r) const
+{
+    std::list<unsigned int> rows = this->_rowsList;
+    rows.push_back(r);
+    return SubTaskMap(rows);
+}
+
+bool OpenSoT::SubTask::SubTaskMap::operator==(const OpenSoT::SubTask::SubTaskMap &b) const
+{
+    return this->_rowsList == b.getRowsList();
+}
+
+int OpenSoT::SubTask::SubTaskMap::size() const
+{
+    return this->_rowsList.size();
+}
+
+
+
+
+
+
+
+/***************
+ *  SUBTASK    *
+ ***************/
+
+OpenSoT::SubTask::SubTask(OpenSoT::SubTask::TaskPtr taskPtr, const std::list<unsigned int> rowIndices) :
+    Task(taskPtr->getTaskID() + std::string(SubTaskMap(rowIndices)),
+         taskPtr->getXSize()),
+    _subTaskMap(rowIndices),
+    _taskPtr(taskPtr)
+{
+}
+
+const yarp::sig::Matrix &OpenSoT::SubTask::getA()
+{
+    this->_A.resize(0, this->getXSize());
+
+    for(SubTaskMap::ChunkList::const_iterator i = _subTaskMap.getChunks().begin();
+        i != _subTaskMap.getChunks().end();
+        ++i) {
+        using namespace yarp::math;
+        if(_taskPtr->getA().rows() >= i->back() - 1)
+            this->_A = pile(this->_A, _taskPtr->getA().submatrix(i->front() - 1,
+                                                                 i->back() - 1,
+                                                                 0, _x_size-1));
+    }
+
+    return this->_A;
+}
+
+const OpenSoT::HessianType OpenSoT::SubTask::getHessianAtype()
+{
+    OpenSoT::HessianType fatherHessianType = _taskPtr->getHessianAtype();
+
+    if ( fatherHessianType == HST_IDENTITY)
+        return HST_POSDEF;
+    else return fatherHessianType;
+}
+
+const yarp::sig::Vector &OpenSoT::SubTask::getb()
+{
+    this->_b.resize(0);
+
+    for(SubTaskMap::ChunkList::const_iterator i = _subTaskMap.getChunks().begin();
+        i != _subTaskMap.getChunks().end();
+        ++i) {
+        using namespace yarp::math;
+        if(_taskPtr->getb().size() >= i->back() - 1)
+            this->_b = cat(this->_b, _taskPtr->getb().subVector(i->front() - 1,
+                                                                i->back() - 1));
+    }
+
+    return this->_b;
+}
+
+const yarp::sig::Matrix &OpenSoT::SubTask::getWeight() {
+    this->_W.resize(0, this->getXSize());
+
+    for(SubTaskMap::ChunkList::const_iterator i = _subTaskMap.getChunks().begin();
+        i != _subTaskMap.getChunks().end();
+        ++i) {
+        using namespace yarp::math;
+        if(_taskPtr->getWeight().rows() >= i->back() - 1)
+            this->_W = pile(this->_W, _taskPtr->getWeight().submatrix(i->front() - 1,
+                                                                      i->back() - 1,
+                                                                      0, _x_size-1));
+    }
+
+    return this->_W;
+}
+
+void OpenSoT::SubTask::setWeight(const yarp::sig::Matrix &W)
+{
+    assert(W.rows() == this->getTaskSize());
+    assert(W.cols() == this->getXSize());
+
+    this->_W = W;
+
+    yarp::sig::Matrix fullW = _taskPtr->getWeight();
+    unsigned int offset = 0;
+    for(SubTaskMap::ChunkList::const_iterator i = _subTaskMap.getChunks().begin();
+        i != _subTaskMap.getChunks().end();
+        ++i) {
+        using namespace yarp::math;
+        if(fullW.rows() >= i->back() - 1) {
+            fullW.setSubmatrix(W.submatrix(offset,
+                                           offset + i->size() - 1,
+                                           0, _x_size-1),
+                               i->front() - 1, 0);
+            offset += i->size();
+        }
+    }
+
+    _taskPtr->setWeight(fullW);
+}
+
+std::list<OpenSoT::SubTask::ConstraintPtr> &OpenSoT::SubTask::getConstraints()
+{
+    return _taskPtr->getConstraints();
+}
+
+const unsigned int OpenSoT::SubTask::getTaskSize() const
+{
+    unsigned int size = 0;
+    for(SubTaskMap::ChunkList::const_iterator i = _subTaskMap.getChunks().begin();
+        i != _subTaskMap.getChunks().end();
+        ++i) {
+        using namespace yarp::math;
+        if(_taskPtr->getTaskSize() >= i->back()) {
+            size += i->size();
+        }
+    }
+    return size;
+}
+
+void OpenSoT::SubTask::_update(const yarp::sig::Vector &x)
+{
+    _taskPtr->update(x);
+}
+
+std::vector<bool> OpenSoT::SubTask::getActiveJointsMask()
+{
+    return _taskPtr->getActiveJointsMask();
+}
+
+bool OpenSoT::SubTask::setActiveJointsMask(const std::vector<bool> &active_joints_mask)
+{
+    return _taskPtr->setActiveJointsMask(active_joints_mask);
+}
