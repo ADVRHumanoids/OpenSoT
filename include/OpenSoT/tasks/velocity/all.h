8a1aecaa1ab6cb2c6684f881183f89a70276c88a
Alessio Rocchi <alessio.rocchi@iit.it>
2015-01-20 17:59:15 +0100

    Minor style updates in header files

--- include/OpenSoT/tasks/velocity/all.h
+++ include/OpenSoT/tasks/velocity/all.h
@@ -1,5 +1,5 @@
 #include <OpenSoT/tasks/velocity/Cartesian.h>
 #include <OpenSoT/tasks/velocity/CoM.h>
+#include <OpenSoT/tasks/velocity/MinimizeAcceleration.h>
 #include <OpenSoT/tasks/velocity/MinimumEffort.h>
 #include <OpenSoT/tasks/velocity/Postural.h>
-#include <OpenSoT/tasks/velocity/MinimizeAcceleration.h>
