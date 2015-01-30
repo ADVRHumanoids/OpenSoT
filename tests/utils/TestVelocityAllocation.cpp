e634a9fbabf4b5e05f7982971670a78001cab416
Alessio Rocchi <alessio.rocchi@iit.it>
2015-01-30 17:21:07 +0100

    Added tests on TestVelocityAllocation to check that it behaves correctly even if velocity bounds already exist at the tasks and/or stack level

--- tests/utils/TestVelocityAllocation.cpp
+++ tests/utils/TestVelocityAllocation.cpp
@@ -13,14 +13,13 @@ class testVelocityAllocation: public ::testing::Test
 {
 protected:
     iDynUtils _robot;
-    OpenSoT::DefaultHumanoidStack DHS, DHS2;
+    OpenSoT::DefaultHumanoidStack _DHS;
 
     testVelocityAllocation() :
         _robot("coman",
                std::string(OPENSOT_TESTS_ROBOTS_DIR)+"coman/coman.urdf",
                std::string(OPENSOT_TESTS_ROBOTS_DIR)+"coman/coman.srdf"),
-        DHS(_robot, 3e-3, _robot.zeros),
-        DHS2(_robot, 3e-3, _robot.zeros)
+        _DHS(_robot, 3e-3, _robot.zeros)
     {
 
     }
@@ -39,76 +38,229 @@ protected:
 
 };
 
-TEST_F(testVelocityAllocation, testConstructors )
+TEST_F(testVelocityAllocation, testAllocation )
 {
-    // defining a stack composed of size two,
-    // where the task of first priority is an aggregated of leftArm and rightArm,
-    // rightArm contains a convexHull constraint;
-    // the task at the second priority level is an aggregated of rightLeg and leftLeg,
-    // and the stack is subject to bounds jointLimits and velocityLimits
-    OpenSoT::AutoStack::Ptr autoStack =
-        (DHS.leftArm + DHS.rightArm)
-        / (DHS.rightLeg + DHS.leftLeg)
-        / (DHS.com << DHS.comVelocity)
-        / DHS.postural;
-    autoStack << DHS.jointLimits;
+    const double minimum_velocity = 0.1;
+    const double maximum_velocity = 0.3;
+    const int stack_size = 4;
 
-    OpenSoT::AutoStack::Ptr autoStack2 =
-        (DHS2.leftArm + DHS2.rightArm)
-        / (DHS2.rightLeg + DHS2.leftLeg)
-        / (DHS2.com << DHS2.comVelocity)
-        / DHS2.postural;
-    autoStack2 << DHS2.jointLimits;
-
-    OpenSoT::Solver<yarp::sig::Matrix, yarp::sig::Vector>::Stack stack = autoStack2->getStack();
-
-    OpenSoT::VelocityAllocation(autoStack,
-                                0.1,
-                                0.3);
-
-    OpenSoT::VelocityAllocation(stack,
-                                0.1,
-                                0.3);
-
-    for(unsigned int i = 0; i < stack.size(); ++i)
+    /**
+     * testing the algorithm for a 4 tasks stack,
+     * min_velocity=0.1, max_velocity=0.3
+     **/
+    for(unsigned int i = 0; i < stack_size; ++i)
     {
         if(i == 0)
-            ASSERT_DOUBLE_EQ(0.1+0.2*1,0.1);
+            ASSERT_DOUBLE_EQ(minimum_velocity+i*(maximum_velocity - minimum_velocity)/(stack_size()-1),
+                             0.1);
         else if(i == 1)
-            ASSERT_DOUBLE_EQ(0.1+0.2*1,0.1666666666666666666666666667);
+            ASSERT_DOUBLE_EQ(minimum_velocity+i*(maximum_velocity - minimum_velocity)/(stack_size()-1),
+                             0.1666666666666666666666666667);
         else if(i == 2)
-            ASSERT_DOUBLE_EQ(0.1+0.2*1,0.2333333333333333333333333334);
+            ASSERT_DOUBLE_EQ(minimum_velocity+i*(maximum_velocity - minimum_velocity)/(stack_size()-1),
+                             0.2333333333333333333333333334);
         else if(i == 3)
-            ASSERT_DOUBLE_EQ(0.1+0.2*1,0.3);
+            ASSERT_DOUBLE_EQ(minimum_velocity+i*(maximum_velocity - minimum_velocity)/(stack_size()-1),
+                             0.3);
     }
+}
 
-    for(OpenSoT::Task<yarp::sig::Matrix, yarp::sig::Vector>::TaskPtr task :
-        autoStack->getStack())
+TEST_F(testVelocityAllocation, testConstructorStack)
+{
+    // testing constructor applies velocity bounds correctly when no velocity bounds existing
     {
-        EXPECT_DOUBLE_EQ(
-        std::cout << "Task "<< i << " has velocity bounds" <<
-            boost::dynamic_pointer_cast<
+        // defining a stack of size four,
+        // where the task of first priority is an aggregated of leftArm and rightArm,
+        // rightArm contains a convexHull constraint;
+        // the task at the second priority level is an aggregated of rightLeg and leftLeg,
+        // and the stack is subject to bounds jointLimits and velocityLimits
+        OpenSoT::AutoStack::Ptr autoStack =
+            (_DHS.leftArm + _DHS.rightArm)
+            / (_DHS.rightLeg + _DHS.leftLeg)
+            / _DHS.com
+            / _DHS.postural;
+        autoStack << _DHS.jointLimits;
+
+        OpenSoT::Solver<yarp::sig::Matrix, yarp::sig::Vector>::Stack stack = autoStack->getStack();
+
+
+        const double minimum_velocity = 0.1;
+        const double maximum_velocity = 0.3;
+
+        OpenSoT::VelocityAllocation(autoStack,
+                                    minimum_velocity,
+                                    maximum_velocity);
+
+
+        for(OpenSoT::Task<yarp::sig::Matrix, yarp::sig::Vector>::TaskPtr task :
+            autoStack->getStack())
+        {
+            ASSERT_EQ(boost::dynamic_pointer_cast<
+                      OpenSoT::constraints::velocity::VelocityLimits>(
+                          task->getConstraints().size(),1);
+            EXPECT_DOUBLE_EQ(boost::dynamic_pointer_cast<
                 OpenSoT::constraints::velocity::VelocityLimits>(
                     task->getConstraints().front()
-                         )->getVelocityLimits(),0.1+i*0.2/3.0)
+                            )->getVelocityLimits(),
+                minimum_velocity+i*(maximum_velocity - minimum_velocity)/(stack().size()-1));
 
-        ++i;
+            ++i;
+        }
     }
 
-    for(OpenSoT::Task<yarp::sig::Matrix, yarp::sig::Vector>::TaskPtr task :
-        stack)
+    // testing constructor applies velocity bounds correctly velocity bounds exist on tasks
     {
-        EXPECT_DOUBLE_EQ(
-        std::cout << "Task "<< i << " has velocity bounds" <<
-            boost::dynamic_pointer_cast<
+        OpenSoT::DefaultHumanoidStack DHS(_robot, 3e-3, _robot.zeros);
+
+        OpenSoT::AutoStack::Ptr autoStack =
+            (DHS.leftArm + DHS.rightArm)
+            / (DHS.rightLeg + DHS.leftLeg)
+            / (DHS.com << DHS.comVelocity)
+            / DHS.postural;
+        autoStack << DHS.jointLimits;
+
+        const double high_velocity = 0.8;
+
+        DHS.postural->getConstraints().push_back(
+            new OpenSoT::constraints::velocity::VelocityLimits(high_velocity,
+                                                               1e-3,
+                                                               DHS.postural->getXSize()));
+
+        OpenSoT::Solver<yarp::sig::Matrix, yarp::sig::Vector>::Stack stack = autoStack->getStack();
+        ASSERT_EQ(stack[3]->getConstraints().size(),1);
+
+        ASSERT_EQ(boost::dynamic_pointer_cast<
+                    OpenSoT::constraints::velocity::VelocityLimits>(
+                        stack[3])->getVelocityLimits(),high_velocity);
+
+        const double minimum_velocity = 0.1;
+        const double maximum_velocity = 0.3;
+
+        OpenSoT::VelocityAllocation(stack,
+                                    minimum_velocity,
+                                    maximum_velocity);
+
+        ASSERT_EQ(stack[3]->getConstraints().size(),1);
+        ASSERT_EQ(boost::dynamic_pointer_cast<
+                    OpenSoT::constraints::velocity::VelocityLimits>(
+                        stack[3])->getVelocityLimits(),maximum_velocity);
+
+    }
+}
+
+TEST_F(testVelocityAllocation, testConstructorAutoStack)
+{
+    // testing constructor applies velocity bounds correctly when no velocity bounds exist on tasks,
+    // and no velocity bounds existing on stack
+    {
+        // defining a stack of size four,
+        // where the task of first priority is an aggregated of leftArm and rightArm,
+        // rightArm contains a convexHull constraint;
+        // the task at the second priority level is an aggregated of rightLeg and leftLeg,
+        // and the stack is subject to bounds jointLimits and velocityLimits
+        OpenSoT::AutoStack::Ptr autoStack =
+            (_DHS.leftArm + _DHS.rightArm)
+            / (_DHS.rightLeg + _DHS.leftLeg)
+            / _DHS.com
+            / _DHS.postural;
+        autoStack << _DHS.jointLimits;
+
+        const double minimum_velocity = 0.1;
+        const double maximum_velocity = 0.3;
+
+        OpenSoT::Solver<yarp::sig::Matrix, yarp::sig::Vector>::Stack stack = autoStack->getStack();
+
+        OpenSoT::VelocityAllocation(stack,
+                                    minimum_velocity,
+                                    maximum_velocity);
+
+
+
+        unsigned int i = 0;
+        for(OpenSoT::Task<yarp::sig::Matrix, yarp::sig::Vector>::TaskPtr task :
+            stack)
+        {
+            ASSERT_EQ(boost::dynamic_pointer_cast<
+                      OpenSoT::constraints::velocity::VelocityLimits>(
+                          task->getConstraints().size(),1);
+            EXPECT_DOUBLE_EQ(boost::dynamic_pointer_cast<
                 OpenSoT::constraints::velocity::VelocityLimits>(
                     task->getConstraints().front()
-                         )->getVelocityLimits(),0.1+i*0.2/3.0)
+                        )->getVelocityLimits(),
+                minimum_velocity+i*(maximum_velocity - minimum_velocity)/(stack().size()-1));
 
-        ++i;
+            ++i;
+        }
     }
 
+    // testing constructor applies velocity bounds correctly when no velocity bounds exist on tasks,
+    // and velocity bounds exist on stack
+    {
+        OpenSoT::DefaultHumanoidStack DHS(_robot, 3e-3, _robot.zeros);
 
+        OpenSoT::AutoStack::Ptr autoStack =
+            (DHS.leftArm + DHS.rightArm)
+            / (DHS.rightLeg + DHS.leftLeg)
+            / (DHS.com << DHS.comVelocity)
+            / DHS.postural;
+        autoStack << DHS.jointLimits;
+
+        const double high_velocity = 0.8;
+
+        DHS.postural->getConstraints().push_back(
+            new OpenSoT::constraints::velocity::VelocityLimits(high_velocity,
+                                                               1e-3,
+                                                               DHS.postural->getXSize()));
+
+        OpenSoT::Solver<yarp::sig::Matrix, yarp::sig::Vector>::Stack stack = autoStack->getStack();
+        ASSERT_EQ(stack[3]->getConstraints().size(),1);
+
+        ASSERT_EQ(boost::dynamic_pointer_cast<
+                    OpenSoT::constraints::velocity::VelocityLimits>(
+                        stack[3])->getVelocityLimits(),high_velocity);
+
+        const double minimum_velocity = 0.1;
+        const double maximum_velocity = 0.3;
+
+        OpenSoT::VelocityAllocation(stack,
+                                    minimum_velocity,
+                                    maximum_velocity);
+
+        ASSERT_EQ(stack[3]->getConstraints().size(),1);
+        ASSERT_EQ(boost::dynamic_pointer_cast<
+                    OpenSoT::constraints::velocity::VelocityLimits>(
+                        stack[3])->getVelocityLimits(),maximum_velocity);
+    }
+
+    // testing constructor applies velocity bounds correctly velocity bounds exist on tasks,
+    // and no velocity bounds exist on stack
+    {
+        OpenSoT::DefaultHumanoidStack DHS(_robot, 3e-3, _robot.zeros);
+
+        OpenSoT::AutoStack::Ptr autoStack =
+            (DHS.leftArm + DHS.rightArm)
+            / (DHS.rightLeg + DHS.leftLeg)
+            / (DHS.com << DHS.comVelocity)
+            / DHS.postural;
+
+        ASSERT_EQ(autoStack->getBoundsList().size(),0);
+        const double high_velocity = 0.8;
+        OpenSoT::constraints::velocity::VelocityLimits::Ptr velocityLimits =
+            new OpenSoT::constraints::velocity::VelocityLimits(high_velocity,
+                                                               1e-3,
+                                                               DHS.postural->getXSize());
+        autoStack << velocityLimits;
+        ASSERT_EQ(autoStack->getBoundsList().size(),1);
+
+        const double minimum_velocity = 0.1;
+        const double maximum_velocity = 0.3;
+
+        OpenSoT::VelocityAllocation(autoStack,
+                                    minimum_velocity,
+                                    maximum_velocity);
+
+        ASSERT_EQ(autoStack->getBoundsList().size(),0);
+    }
 }
 
 }
