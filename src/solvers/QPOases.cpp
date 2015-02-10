f1c41b9a41eb0bab6a66f2aafeabc799534ade57
Alessio Rocchi <alessio.rocchi@iit.it>
2015-02-10 18:32:54 +0100

    QPOases uses a generic constraint rather than an aggregated for Bounds

--- src/solvers/QPOases.cpp
+++ src/solvers/QPOases.cpp
@@ -20,7 +20,8 @@ QPOases_sot::QPOases_sot(Stack &stack_of_tasks, const double eps_regularisation)
 }
 
 QPOases_sot::QPOases_sot(Stack &stack_of_tasks,
-                         boost::shared_ptr<constraints::Aggregated> &bounds, const double eps_regularisation):
+                         ConstraintPtr bounds,
+                         const double eps_regularisation):
     Solver(stack_of_tasks, bounds),
     _epsRegularisation(eps_regularisation)
 {
