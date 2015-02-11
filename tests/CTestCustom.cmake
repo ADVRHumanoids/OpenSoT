58c9bc72d615511237ff2b37d1f888a8590d3b05
Alessio Rocchi <alessio.rocchi@iit.it>
2015-02-11 17:06:15 +0100

    Trying utility for automatic generation of plots

---
+++ tests/CTestCustom.cmake
@@ -0,0 +1,5 @@
+if(WIN32)
+    SET(CTEST_CUSTOM_POST_TEST "matlab.exe -nosplash -nodisplay -r \"cd ${CMAKE_CURRENT_BINARY_DIR}\tests; try, run ('plot_test_results.m'); end; quit\"")
+else()
+    SET(CTEST_CUSTOM_POST_TEST "${MATLAB_ROOT}/bin/matlab -nosplash -nodisplay -r \"cd ${CMAKE_CURRENT_BINARY_DIR}/tests; try, run ('plot_test_results.m'); end; quit\"")
+endif()
