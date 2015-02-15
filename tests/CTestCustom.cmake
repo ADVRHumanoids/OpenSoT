fb419aa0d2e4c241c4e6008f3b334383957bc189
Alessio Rocchi <alessio.rocchi@iit.it>
2015-02-15 12:29:39 +0100

    Fixed CTestCustom.cmake - now plots are correctly autogenerated

--- tests/CTestCustom.cmake
+++ tests/CTestCustom.cmake
@@ -1,5 +1,5 @@
 if(WIN32)
-    SET(CTEST_CUSTOM_POST_TEST "${MATLAB_EXECUTABLE} -nosplash -nodisplay -r \"cd ${CMAKE_CURRENT_BINARY_DIR}\tests; try, run ('plot_test_results.m'); end; quit\"")
+    SET(CTEST_CUSTOM_POST_TEST "${MATLAB_EXECUTABLE} -nosplash -nodisplay -r \"cd ${CMAKE_CURRENT_BINARY_DIR}; try, run ('plot_test_results.m'); end; quit\"")
 else()
-    SET(CTEST_CUSTOM_POST_TEST "${MATLAB_EXECUTABLE} -nosplash -nodisplay -r \"cd ${CMAKE_CURRENT_BINARY_DIR}/tests; try, run ('plot_test_results.m'); end; quit\"")
+    SET(CTEST_CUSTOM_POST_TEST "${MATLAB_EXECUTABLE} -nosplash -nodisplay -r \"cd ${CMAKE_CURRENT_BINARY_DIR}; try, run ('plot_test_results.m'); end; quit\"")
 endif()
