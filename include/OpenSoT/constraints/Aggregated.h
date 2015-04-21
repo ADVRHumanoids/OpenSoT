d6b4ceb84c1624d7eac0e7869df304167461144d
Alessio Rocchi <alessio.rocchi@iit.it>
2015-04-21 14:01:28 +0200

    Fixed behavior where a auto_stack->getBounds()->update(q) was required before feeding it to the solver constructor. Now you don't have to call update(q) on auto_stack, and you can use it immediately after creating it via MoT

--- include/OpenSoT/constraints/Aggregated.h
+++ include/OpenSoT/constraints/Aggregated.h
@@ -60,7 +60,6 @@
             std::list< ConstraintPtr > _bounds;
             unsigned int _aggregationPolicy;
 
-            void generateAll();
             void checkSizes();
 
         public:
@@ -106,6 +105,8 @@
             void update(const yarp::sig::Vector &x);
 
             std::list< ConstraintPtr >& getConstraintsList() { return _bounds; }
+
+            void generateAll();
         };
     }
  }
