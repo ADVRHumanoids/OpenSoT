8e96d477ab2fe29316a9b58047ffcc565a9b8e96
Alessio Rocchi <alessio.rocchi@iit.it>
2015-05-05 19:13:58 +0200

    First commit of a minimum velocity task

---
+++ include/OpenSoT/tasks/velocity/MinimumVelocity.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2014 Walkman
+ * Authors:Alessio Rocchi, Enrico Mingo
+ * email:  alessio.rocchi@iit.it, enrico.mingo@iit.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU Lesser General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+#ifndef __TASKS_VELOCITY_MINIMUMVELOCITY_H__
+#define __TASKS_VELOCITY_MINIMUMVELOCITY_H__
+
+ #include <OpenSoT/Task.h>
+ #include <idynutils/idynutils.h>
+ #include <kdl/frames.hpp>
+ #include <yarp/sig/all.h>
+ #include <yarp/os/all.h>
+
+/**
+ * @example example_MinimumVelocity.cpp
+ * The MinimumVelocity class implements a task that tries to minimize the velocity.
+ */
+
+ namespace OpenSoT {
+    namespace tasks {
+        namespace velocity {
+            /**
+             * @brief The MinimumVelocity class implements a task that tries to bring the robust posture to a reference posture.
+             * You can see an example of it in @ref example_MinimumVelocity.cpp
+             */
+            class MinimumVelocity : public Task < yarp::sig::Matrix, yarp::sig::Vector > {
+            public:
+                typedef boost::shared_ptr<MinimumVelocity> Ptr;
+            protected:
+
+            public:
+
+                MinimumVelocity(const int& x_size);
+
+                ~MinimumVelocity();
+
+                void _update(const yarp::sig::Vector& x);
+            };
+        }
+    }
+ }
+
+#endif
