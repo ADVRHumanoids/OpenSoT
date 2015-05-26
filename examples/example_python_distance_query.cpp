1db9adb6c2408355b9d36888345f18d2b5bb715b
Alessio Rocchi <alessio.rocchi@iit.it>
2015-05-26 12:13:55 +0200

    Added dependencies in CMakeLists where they were missing. Now example_python_distance_query plots lines

--- examples/example_python_distance_query.cpp
+++ examples/example_python_distance_query.cpp
@@ -15,7 +15,34 @@ static const std::string JOINT_STATE_TOPIC = "/joint_states";
 static const std::string RESULT_MARKER_TOPIC = "distance_query/result_marker";
 
 int id_counter = 1;
+int id_lines = 0;
 std::string base_frame = "base_link";
+
+const int kelly_colors_hex_size = 20;
+const int kelly_colors_hex[] = {
+    0xFFB300, // Vivid Yellow
+    0x803E75, // Strong Purple
+    0xFF6800, // Vivid Orange
+    0xA6BDD7, // Very Light Blue
+    0xC10020, // Vivid Red
+    0xCEA262, // Grayish Yellow
+    0x817066, // Medium Gray
+
+    // The following don't work well for people with defective color vision
+    0x007D34, // Vivid Green
+    0xF6768E, // Strong Purplish Pink
+    0x00538A, // Strong Blue
+    0xFF7A5C, // Strong Yellowish Pink
+    0x53377A, // Strong Violet
+    0xFF8E00, // Vivid Orange Yellow
+    0xB32851, // Strong Purplish Red
+    0xF4C800, // Vivid Greenish Yellow
+    0x7F180D, // Strong Reddish Brown
+    0x93AA00, // Vivid Yellowish Green
+    0x593315, // Deep Yellowish Brown
+    0xF13A13, // Vivid Reddish Orange
+    0x232C16  // Dark Olive Green
+};
 
 bool draw_point(const double x, const double y, const double z,
                 const std::string frame, visualization_msgs::Marker& marker, float color=1.0) {
@@ -42,11 +69,50 @@ bool draw_point(const double x, const double y, const double z,
     marker.color.g = 0.0;
     marker.color.b = 0.0;
     marker.color.a = 1.0;
+    /* // use the map of kelly_colors
+    marker.color.r = (kelly_colors_hex[id_lines] & 0xff0000) >> 16;
+    marker.color.g = (kelly_colors_hex[id_lines] & 0x00ff00) >> 8;
+    marker.color.b =  kelly_colors_hex[id_lines] & 0x0000ff;
+    */
+    return true;
+}
+
+bool draw_line(const double x1, const double y1, const double z1,
+               const double x2, const double y2, const double z2,
+               visualization_msgs::Marker& marker, float color=1.0) {
+    //DRAW REFERENCE
+    marker.header.frame_id = "Waist";
+    marker.header.stamp = ros::Time().now();
+    marker.ns = "goal";
+    marker.id = ++id_counter;
+    marker.type = visualization_msgs::Marker::LINE_STRIP;
+    marker.action = visualization_msgs::Marker::ADD;
+
+    geometry_msgs::Point p1; p1.x = x1; p1.y = y1; p1.z = z1;
+    geometry_msgs::Point p2; p2.x = x2; p2.y = y2; p2.z = z2;
+    marker.points.push_back(p1);
+    marker.points.push_back(p2);
+
+    marker.scale.x = 0.005;
+    marker.scale.y = 0.005;
+    marker.scale.z = 0.005;
+
+    marker.color.r = color;
+    marker.color.g = 0.0;
+    marker.color.b = 0.0;
+    marker.color.a = 1.0;
+    /* // use the map of kelly_colors
+    marker.color.r = (kelly_colors_hex[id_lines] & 0xff0000) >> 16;
+    marker.color.g = (kelly_colors_hex[id_lines] & 0x00ff00) >> 8;
+    marker.color.b =  kelly_colors_hex[id_lines] & 0x0000ff;
+    */
+    id_lines = (id_lines+1)% kelly_colors_hex_size; // can be used in the future to index the kelly_colors array
     return true;
 }
 
 void createMarkerArray(std::list<LinkPairDistance>& results,
-                       const boost::shared_ptr<visualization_msgs::MarkerArray>& markers) {
+                       const boost::shared_ptr<visualization_msgs::MarkerArray>& markers,
+                       iDynUtils& model) {
     typedef std::list<LinkPairDistance>::iterator iter_pairs;
     unsigned int indicator = 0;
     for(iter_pairs it = results.begin(); it != results.end(); ++it)
@@ -59,14 +125,27 @@ void createMarkerArray(std::list<LinkPairDistance>& results,
                     transforms.first.p.z(),
                     linkNames.first, m1, indicator++);
 
+
         visualization_msgs::Marker m2;
         draw_point( transforms.second.p.x(),
                     transforms.second.p.y(),
                     transforms.second.p.z(),
                     linkNames.second, m2, indicator++);
 
+        visualization_msgs::Marker l12;
+        KDL::Frame p1 = model.iDyn3_model.getPositionKDL(
+                            model.iDyn3_model.getLinkIndex("Waist"),
+                            model.iDyn3_model.getLinkIndex(linkNames.first))*transforms.first;
+        KDL::Frame p2 = model.iDyn3_model.getPositionKDL(
+                            model.iDyn3_model.getLinkIndex("Waist"),
+                            model.iDyn3_model.getLinkIndex(linkNames.second))*transforms.second;
+        draw_line(  p1.p.x(), p1.p.y(), p1.p.z(),
+                    p2.p.x(), p2.p.y(), p2.p.z(),
+                    l12, indicator++);
+
         markers->markers.push_back(m1);
         markers->markers.push_back(m2);
+        markers->markers.push_back(l12);
     }
 }
 
@@ -142,8 +221,9 @@ int main(int argc, char** argv) {
         if (results.size() > 0) {
             markers->markers.clear();
             id_counter = 0;
+            id_lines = 0;
 
-            createMarkerArray(results, markers);
+            createMarkerArray(results, markers, bigman);
             resultMarkerPub.publish(markers);
         }
 
