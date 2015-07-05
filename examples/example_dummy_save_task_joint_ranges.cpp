0b79d883649284b8700dfeb6d145eb044aeda241
Alessio Rocchi <alessio.rocchi@iit.it>
2015-07-05 02:31:25 +0200

    Fixed bugs in example_dummy_save_task_joint_ranges.cpp

--- examples/example_dummy_save_task_joint_ranges.cpp
+++ examples/example_dummy_save_task_joint_ranges.cpp
@@ -2,14 +2,19 @@
  @author Alessio Rocchi
  */
 
-#include <idynutils/WalkmanUtils.h>
+#include <idynutils/RobotUtils.h>
 #include <yarp/os/Time.h>
 #include <fstream>
+#include <yarp/os/Network.h>
 
 int main(int argc, char** argv)
 {
+    yarp::os::Network::init();
 
-    WalkmanUtils walkman("save_task_joint_limits");
+    RobotUtils walkman( "save_task_joint_limits", "bigman",
+                     std::string(OPENSOT_TESTS_ROBOTS_DIR)+"bigman/bigman.urdf",
+                     std::string(OPENSOT_TESTS_ROBOTS_DIR)+"bigman/bigman.srdf");
+    yarp::os::Time::delay(1.0);
     yarp::sig::Vector q = walkman.sensePosition();
     yarp::sig::Vector q_lower = q;
     yarp::sig::Vector q_upper = q;
@@ -33,12 +38,12 @@ int main(int argc, char** argv)
             j_lims << "#copy paste into code" << std::endl;
             std::string q_lower_str = q_lower.toString();
             std::replace(q_lower_str.begin(), q_lower_str.end(), '\t',',');
-            j_lims << "q_l[] = {" << q_lower_str << "};" << std::endl;
+            j_lims << "double q_l[] = {" << q_lower_str << "};" << std::endl;
             std::string q_upper_str = q_upper.toString();
             std::replace(q_upper_str.begin(),q_upper_str.end(),'\t',',');
-            j_lims << "q_u[] = {" << q_upper_str << "};" << std::endl;
-            j_lims << "yarp::sig::Vector q_lower(sizeof(q_l),q_l);" << std::endl;
-            j_lims << "yarp::sig::Vector q_upper(sizeof(q_u),q_u);" << std::endl;
+            j_lims << "double q_u[] = {" << q_upper_str << "};" << std::endl;
+            j_lims << "yarp::sig::Vector q_lower(sizeof(q_l)/sizeof(double),q_l);" << std::endl;
+            j_lims << "yarp::sig::Vector q_upper(sizeof(q_u)/sizeof(double),q_u);" << std::endl;
             for(unsigned int i = 0; i < 64; ++i)
                 j_lims << " ";
             j_lims << std::endl;
