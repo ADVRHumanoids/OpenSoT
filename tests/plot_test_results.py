9a8b4c1d749c21770293b60240bfdb823c29f771
Alessio Rocchi <alessio.rocchi@iit.it>
2015-08-12 05:41:51 +0200

    Added funcionality for automated test reporting

--- tests/plot_test_results.py
+++ tests/plot_test_results.py
@@ -1,3 +1,7 @@
 #! /usr/bin/env python
+import os
+import sys
 
-execfile('testQPOases_VelocityAllocation.py')
+if __name__ == '__main__':
+    os.chdir(os.path.dirname(sys.argv[0]))
+    execfile('testQPOases_VelocityAllocation.py')
