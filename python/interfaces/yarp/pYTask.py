97769d1b98861f1ca57ed0b2ec3969b45a0bc065
Alessio Rocchi <alessio.rocchi@iit.it>
2015-08-13 20:05:13 -0400

    Improvements in PyTask

--- python/interfaces/yarp/pYTask.py
+++ python/interfaces/yarp/pYTask.py
@@ -55,6 +55,106 @@ def trj_msg(kdlFrame, kdlTwist, base_frame, distal_frame, bottle):
 
     return bottle
 
+class YTask(object):
+    def __init__(self, module_name, robot_name, task_name):
+        port_prefix = '/'+robot_name+'/'+module_name+'/'+task_name+'/'
+        self.set_ref = yarp.BufferedPortBottle()
+        self.set_ref.open(port_prefix+'set_ref:o')
+        yarp.Network.connect(port_prefix+'set_ref:o',
+                             port_prefix+'set_ref:i')
+        self.rpc = yarp.RpcClient()
+        self.rpc.open(port_prefix+'rpc_client')
+        self.rpc.addOutput(port_prefix+'rpc')
+        #yarp.Network.connect(port_prefix+'rpc_client',
+        #                     port_prefix+'rpc')
+
+    def help(self):
+        reply = yarp.Bottle()
+        reply.clear()
+
+        request = yarp.Bottle()
+        request.clear()
+
+        request.addString('help')
+        self.rpc.write(request, reply)
+
+        return reply
+
+    def setW(self, W):
+        return
+
+    def getW(self):
+        return
+
+    def setLambda(self, l):
+        return
+
+    def getLambda(self):
+        return
+
+
+class CartesianTask(YTask):
+    def __init__(self, module_name, robot_name, task_name):
+        super(CartesianTask, self).__init__(module_name, robot_name, task_name)
+
+    def setReference(self, ref_pose):
+        pass
+
+    def getActualPose(self):
+        reply = yarp.Bottle()
+        reply.clear()
+
+        request = yarp.Bottle()
+        request.clear()
+
+        request.addString('get actual_pose')
+        self.rpc.write(request, reply)
+        return reply
+
+    def setOrientationGain(self, orientation_gain):
+        pass
+
+    def getOrientationGain(self, orientation_gain):
+        pass
+
+class CoMTask(YTask):
+    def __init__(self, module_name, robot_name, task_name):
+        super(CoMTask, self).__init__(module_name, robot_name, task_name)
+
+    def setReference(self, ref_position):
+        pass
+
+    def getActualPosition(self):
+        reply = yarp.Bottle()
+        reply.clear()
+
+        request = yarp.Bottle()
+        request.clear()
+
+        request.addString('get')
+        request.addString('actual_position')
+        self.rpc.write(request, reply)
+        return reply
+
+class PosturalTask(YTask):
+    def __init__(self, module_name, robot_name, task_name):
+        super(PosturalTask, self).__init__(module_name, robot_name, task_name)
+
+    def setReference(self, ref_posture):
+        pass
+
+    def getActualPosture(self):
+        reply = yarp.Bottle()
+        reply.clear()
+
+        request = yarp.Bottle()
+        request.clear()
+
+        request.addString('get')
+        request.addString('actual_position')
+        self.rpc.write(request, reply)
+        return reply
+
 __author__ = ('Alessio Rocchi', 'Enrico Mingo')
 
 if __name__ == '__main__':
