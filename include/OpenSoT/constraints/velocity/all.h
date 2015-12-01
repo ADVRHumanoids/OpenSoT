9e2766a959fc84196d3db3d7a9981c585b0bfc32
Alessio Rocchi <alessio.rocchi@iit.it>
2015-12-01 16:07:41 +0100

    added Dynamics (torque limits) constraint to the DHS

--- include/OpenSoT/constraints/velocity/all.h
+++ include/OpenSoT/constraints/velocity/all.h
@@ -1,5 +1,6 @@
 #include <OpenSoT/constraints/velocity/ConvexHull.h>
 #include <OpenSoT/constraints/velocity/CoMVelocity.h>
+#include <OpenSoT/constraints/velocity/Dynamics.h>
 #include <OpenSoT/constraints/velocity/JointLimits.h>
 #include <OpenSoT/constraints/velocity/VelocityLimits.h>
 #include <OpenSoT/constraints/velocity/SelfCollisionAvoidance.h>
