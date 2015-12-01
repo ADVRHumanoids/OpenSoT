eb5e94d5a29198bfc219098f855396ef5b822d05
Alessio Rocchi <alessio.rocchi@iit.it>
2015-12-01 16:08:00 +0100

    Added the torque limits constraint to example_python.cpp

--- examples/example_python.cpp
+++ examples/example_python.cpp
@@ -23,29 +23,26 @@ int main(int argc, char **argv) {
                      std::string(OPENSOT_TESTS_ROBOTS_DIR)+"bigman/bigman.srdf");
     yarp::os::Time::delay(1.0);
     yarp::sig::Vector q = robot.sensePosition();
+
+    RobotUtils::ftPtrMap ft_sensors = robot.getftSensors();
+    std::vector<iDynUtils::ft_measure> _ft_measurements;
+    for(RobotUtils::ftPtrMap::iterator it = ft_sensors.begin();
+        it != ft_sensors.end(); it++)
+    {
+        iDynUtils::ft_measure ft_measurement;
+        ft_measurement.first = it->second->getReferenceFrame();
+        _ft_measurements.push_back(ft_measurement);
+    }
+    RobotUtils::ftReadings ft_readings = robot.senseftSensors();
+    for(unsigned int i = 0; i < ft_readings.size(); ++i)
+        _ft_measurements[i].second = ft_readings[_ft_measurements[i].first];
+
     robot.idynutils.setFloatingBaseLink(robot.idynutils.left_leg.end_effector_name);
-    robot.idynutils.updateiDyn3Model(q,true);
+    robot.idynutils.updateiDyn3Model(q, _ft_measurements, true);
     OpenSoT::DefaultHumanoidStack DHS(robot.idynutils, dT, q);
 
-
     /*                            */
-    /*       CREATING STACK       */
-    /*                            */
-
-    // defining a stack composed of size two,
-    // where the task of first priority is an aggregated of leftLeg and rightLeg,
-    // task of priority two is leftArm and rightArm,
-    // and the stack is subject to bounds jointLimits and velocityLimits
-    OpenSoT::AutoStack::Ptr autoStack = 
-        ( DHS.rightLeg ) /
-        ( (DHS.com_XY) << DHS.selfCollisionAvoidance << DHS.convexHull ) /
-        ( (DHS.leftArm + DHS.rightArm ) << DHS.selfCollisionAvoidance ) /
-        ( (DHS.postural) << DHS.selfCollisionAvoidance );
-    autoStack << DHS.jointLimits; // << DHS.velocityLimits; commented since we are using VelocityALlocation
-
-
-    /*                            */
-    /*      CONFIGURING STACK     */
+    /*      CONFIGURING DHS       */
     /*                            */
 
     DHS.rightLeg->setLambda(0.6);   DHS.rightLeg->setOrientationErrorGain(1.0);
@@ -71,6 +68,11 @@ int main(int argc, char **argv) {
            robot.idynutils.right_leg.joint_numbers[i_t]) *= amt;
     }
     DHS.postural->setWeight(pW);
+
+    yarp::sig::Vector tauLims = DHS.torqueLimits->getTorqueLimits();
+    for(unsigned int i_t = 0; i_t < 3; ++i_t)
+        tauLims[robot.idynutils.torso.joint_numbers[i_t]] *= 0.3;
+    DHS.torqueLimits->setTorqueLimits(tauLims);
 
     std::list<std::pair<std::string,std::string> > whiteList;
     // lower body - arms collision whitelist for WalkMan (for upper-body manipulation tasks - i.e. not crouching)
@@ -105,6 +107,22 @@ int main(int argc, char **argv) {
     whiteList.push_back(std::pair<std::string,std::string>("LWrMot2","RWrMot2"));
 
     DHS.selfCollisionAvoidance->setCollisionWhiteList(whiteList);
+
+
+    /*                            */
+    /*       CREATING STACK       */
+    /*                            */
+
+    // defining a stack composed of size two,
+    // where the task of first priority is an aggregated of leftLeg and rightLeg,
+    // task of priority two is leftArm and rightArm,
+    // and the stack is subject to bounds jointLimits and velocityLimits
+    OpenSoT::AutoStack::Ptr autoStack = 
+        ( DHS.rightLeg ) /
+        ( (DHS.com_XY) << DHS.selfCollisionAvoidance << DHS.convexHull ) /
+        ( (DHS.leftArm + DHS.rightArm ) << DHS.selfCollisionAvoidance ) /
+        ( (DHS.postural) << DHS.selfCollisionAvoidance );
+    autoStack << DHS.jointLimits << DHS.torqueLimits; // << DHS.velocityLimits; commented since we are using VelocityALlocation
 
     OpenSoT::VelocityAllocation(autoStack,
                                 dT,
@@ -153,7 +171,13 @@ int main(int argc, char **argv) {
     int print_mean = 0;
     while(true) {
         tic = yarp::os::Time::now();
-        robot.idynutils.updateiDyn3Model(q, true);
+
+        RobotUtils::ftReadings ft_readings = robot.senseftSensors();
+        for(unsigned int i = 0; i < _ft_measurements.size(); ++i)
+            _ft_measurements[i].second += (ft_readings[_ft_measurements[i].first]-_ft_measurements[i].second)*0.7;
+
+        robot.idynutils.updateiDyn3Model(q, dq/dT, _ft_measurements, true);
+
         autoStack->update(q);
         if(solver.solve(dq))
             q+=dq;
