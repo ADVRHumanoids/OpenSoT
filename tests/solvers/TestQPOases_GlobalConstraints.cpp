c0985a6e39884d48583f9ff154ac92e7205796b6
Alessio Rocchi <alessio.rocchi@iit.it>
2016-09-19 20:19:04 +0200

    Added support for xenial/kinetic

--- tests/solvers/TestQPOases_GlobalConstraints.cpp
+++ tests/solvers/TestQPOases_GlobalConstraints.cpp
@@ -167,7 +167,7 @@ public:
 
     }
 
-    std::map<std::string,boost::shared_ptr<fcl::CollisionGeometry> > getShapes()
+    std::map<std::string,fcl::CollisionGeometryPtr > getShapes()
     {
         return _computeDistance.shapes_;
     }
