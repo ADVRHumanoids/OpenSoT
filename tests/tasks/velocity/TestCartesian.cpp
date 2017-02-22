9a5eab22e124d9ee0ac1330d3034acb5d2626f6d
Enrico Mingo <enrico.mingo@iit.it>
2017-02-22 16:19:05 +0100

    Added test

--- tests/tasks/velocity/TestCartesian.cpp
+++ tests/tasks/velocity/TestCartesian.cpp
@@ -443,101 +443,108 @@ TEST_F(testCartesianTask, testCartesianTaskRelativeWaistNoUpdateWorld_)
 }
 
 
-//TEST_F(testCartesianTask, testCartesianTaskRelativeUpdateWorld_)
-//{
-//    bool update_world = true;
-//    // setting initial position with bent legs
-//    yarp::sig::Vector q_leg(6, 0.0),
-//                      q_whole(_robot.iDyn3_model.getNrOfDOFs(), 0.0);
-//    q_leg[0] = -25.0*M_PI/180.0;
-//    q_leg[3] =  50.0*M_PI/180.0;
-//    q_leg[5] = -25.0*M_PI/180.0;
+TEST_F(testCartesianTask, testCartesianTaskRelativeUpdateWorld_)
+{
+    yarp::sig::Vector q_whole(_robot.iDynTree_model.getNrOfDOFs(), 0.0);
 
-//    _robot.fromRobotToIDyn(q_leg, q_whole, _robot.left_leg);
+    q_whole[_robot.iDynTree_model.getDOFIndex("LHipSag")] = -25.0*M_PI/180.0;
+    q_whole[_robot.iDynTree_model.getDOFIndex("LKneeSag")] = 50.0*M_PI/180.0;
+    q_whole[_robot.iDynTree_model.getDOFIndex("LAnkSag")] = -25.0*M_PI/180.0;
+    q_whole[_robot.iDynTree_model.getDOFIndex("LShSag")] =  20.0*M_PI/180.0;
+    q_whole[_robot.iDynTree_model.getDOFIndex("LShLat")] = 10.0*M_PI/180.0;
+    q_whole[_robot.iDynTree_model.getDOFIndex("LElbj")] = -80.0*M_PI/180.0;
 
-//    yarp::sig::Vector arm(_robot.left_arm.getNrOfDOFs(), 0.0);
-//    arm[0] = 20.0 * M_PI/180.0;
-//    arm[1] = 10.0 * M_PI/180.0;
-//    arm[3] = -80.0 * M_PI/180.0;
-//    _robot.fromRobotToIDyn(arm, q_whole, _robot.left_arm);
-
-//    _robot.updateiDyn3Model(q_whole, update_world);
-
-//    OpenSoT::tasks::velocity::Cartesian cartesian("cartesian::left_leg",
-//                                                 conversion_utils_YARP::toEigen(q_whole),
-//                                                 *(_model_ptr.get()),
-//                                                 "l_wrist",
-//                                                 "l_sole");
+    _robot.switchAnchorAndFloatingBase("l_sole");
+    _robot.updateiDynTreeModel(conversion_utils_YARP::toEigen(q_whole), true);
 
 
-//    // setting x_ref with a delta offset along the z axis (-2cm)
-//    yarp::sig::Matrix delta_x(4,4); delta_x.zero();
-//                      delta_x(2,3) = -0.02;
-//    yarp::sig::Matrix x = _robot.iDyn3_model.getPosition(
-//                            _robot.left_leg.end_effector_index,
-//                            _robot.left_arm.end_effector_index);
-//    yarp::sig::Matrix x_ref = x + delta_x;
+    OpenSoT::tasks::velocity::Cartesian cartesian("cartesian::left_leg",
+                                                 conversion_utils_YARP::toEigen(q_whole),
+                                                 *(_model_ptr.get()),
+                                                 "l_wrist",
+                                                 "l_sole");
+    XBot::ModelInterface& reference_to_model_interface = *_model_ptr;
+    Eigen::MatrixXd JJJ(6, q_whole.size());
+    reference_to_model_interface.getRelativeJacobian("l_sole", "l_wrist", JJJ);
+    std::cout<<"reference_to_model_interface J: "<<JJJ<<std::endl;
 
-//    yarp::sig::Matrix J;
-//    _robot.iDyn3_model.getRelativeJacobian(_robot.left_arm.end_effector_index,
-//                                           _robot.left_leg.end_effector_index, J);
-//    //EXPECT_TRUE(cartesian.getA() == J);
-//    EXPECT_EQ(cartesian.getA().rows(), 6);
-//    EXPECT_EQ(cartesian.getb().size(), 6);
+    yarp::sig::Matrix delta_x(4,4); delta_x.zero();
+                      delta_x(2,3) = -0.02;
+    Eigen::MatrixXd x = _robot.getPosition(_robot.iDynTree_model.getLinkIndex("l_sole"),
+                                           _robot.iDynTree_model.getLinkIndex("l_wrist"));
 
-//    EXPECT_TRUE(cartesian.getWeight() == conversion_utils_YARP::toEigen(yarp::sig::Matrix(6,6).eye()));
+    std::cout<<"cartesian actual pose: "<<cartesian.getActualPose()<<std::endl;
+    std::cout<<"x: "<<x<<std::endl;
+    std::cout<<"delta_x: "<<delta_x.toString()<<std::endl;
 
-//    EXPECT_TRUE(cartesian.getConstraints().size() == 0);
+    yarp::sig::Matrix x_ref = conversion_utils_YARP::toYARP(x) + delta_x;
+    std::cout<<"x_ref: "<<x_ref.toString()<<std::endl;
 
-//    double K = 0.1;
-//    cartesian.setLambda(K);
-//    EXPECT_DOUBLE_EQ(cartesian.getLambda(), K);
+    KDL::Jacobian J; J.resize(q_whole.size());
+    _model_ptr->getRelativeJacobian("l_sole","l_wrist", J);
+    std::cout<<"getA(): "<<cartesian.getA()<<std::endl;
+    std::cout<<"J model: "<<J.data<<std::endl;
+    Eigen::MatrixXd JJ(6, q_whole.size());
+    _robot.getRelativeJacobian(_robot.iDynTree_model.getLinkIndex("l_wrist"),
+                               _robot.iDynTree_model.getLinkIndex("l_sole"), JJ);
+    std::cout<<"J robot: "<<JJ<<std::endl;
+    EXPECT_TRUE(cartesian.getA() == J.data);
+    EXPECT_EQ(cartesian.getA().rows(), 6);
+    EXPECT_EQ(cartesian.getb().size(), 6);
 
-//    cartesian.setReference(conversion_utils_YARP::toEigen(x_ref));
-//    cartesian.update(conversion_utils_YARP::toEigen(q_whole));
-//    yarp::sig::Vector positionError, orientationError;
-//    cartesian_utils::computeCartesianError(x, x_ref,
-//                                           positionError, orientationError);
+    EXPECT_TRUE(cartesian.getWeight() == conversion_utils_YARP::toEigen(yarp::sig::Matrix(6,6).eye()));
 
-//    double orientationErrorGain = 1.0;
-//    cartesian.setOrientationErrorGain(orientationErrorGain);
+    EXPECT_TRUE(cartesian.getConstraints().size() == 0);
 
-//    EXPECT_TRUE(cartesian_utils::fromEigentoYarp(cartesian.getb()) == cartesian.getLambda()*cat(positionError, -orientationErrorGain*orientationError));
+    double K = 0.1;
+    cartesian.setLambda(K);
+    EXPECT_DOUBLE_EQ(cartesian.getLambda(), K);
 
-//    yarp::sig::Matrix x_now;
-//    for(unsigned int i = 0; i < 120; ++i)
-//    {
-//        _robot.updateiDyn3Model(q_whole, update_world);
-//        cartesian._update(conversion_utils_YARP::toEigen(q_whole));
-//        q_whole += pinv(cartesian_utils::fromEigentoYarp(cartesian.getA()),1E-7)*
-//                cartesian_utils::fromEigentoYarp(cartesian.getb());
-//        _robot.updateiDyn3Model(q_whole, update_world);
-//        x_now = _robot.iDyn3_model.getPosition(
-//                      _robot.left_leg.end_effector_index,
-//                      _robot.left_arm.end_effector_index);
-//        //std::cout << "Current error at iteration " << i << " is " << x_ref(2,3) - x_now(2,3) << std::endl;
-//    }
+    cartesian.setReference(conversion_utils_YARP::toEigen(x_ref));
+    cartesian.update(conversion_utils_YARP::toEigen(q_whole));
+    Eigen::VectorXd positionError, orientationError;
+    cartesian_utils::computeCartesianError(x, conversion_utils_YARP::toEigen(x_ref),
+                                           positionError, orientationError);
+
+    double orientationErrorGain = 1.0;
+    cartesian.setOrientationErrorGain(orientationErrorGain);
+
+    EXPECT_TRUE(cartesian.getb() == conversion_utils_YARP::toEigen(cartesian.getLambda()*cat(
+                                        conversion_utils_YARP::toYARP(positionError),
+                                        -orientationErrorGain*conversion_utils_YARP::toYARP(orientationError))));
+
+    std::cout<<"cartesian.getb(): ["<<cartesian.getb()<<"]"<<std::endl;
+    std::cout<<"error: ["<<conversion_utils_YARP::toEigen(cartesian.getLambda()*cat(
+                                                              conversion_utils_YARP::toYARP(positionError),
+                                                              -orientationErrorGain*conversion_utils_YARP::toYARP(orientationError)))<<"]"<<std::endl;
+
+    Eigen::MatrixXd x_now;
+    for(unsigned int i = 0; i < 120; ++i)
+    {
+        _robot.updateiDynTreeModel(conversion_utils_YARP::toEigen(q_whole),true);
+        cartesian._update(conversion_utils_YARP::toEigen(q_whole));
+        q_whole += pinv(conversion_utils_YARP::toYARP(cartesian.getA()),1E-7)*
+                conversion_utils_YARP::toYARP(cartesian.getb());
+        _robot.updateiDynTreeModel(conversion_utils_YARP::toEigen(q_whole),true);
+        x_now = _robot.getPosition(_robot.iDynTree_model.getLinkIndex("l_sole"),
+                                   _robot.iDynTree_model.getLinkIndex("l_wrist"));
+        std::cout << "Current error at iteration " << i << " is " << x_ref(2,3) - x_now(2,3) << std::endl;
+    }
 
 
 
-//    EXPECT_LT( findMax((x_ref-x_now).subcol(0,3,3)), 1E-3 );
-//    EXPECT_LT( abs(findMin((x_ref-x_now).subcol(0,3,3))), 1E-3 );
-//    // checking for the position
-//    for(unsigned int i = 0; i < 3; ++i) {
-//        EXPECT_NEAR(x_ref(i,3),x_now(i,3),1E-4);
-//    }
-//    for(unsigned int i = 0; i < 3; ++i) {
-//        for(unsigned int j = 0; j < 3; ++j) {
-//            EXPECT_NEAR(x_ref(i,j),x_now(i,j),1E-4);
-//        }
-//    }
-
-//    for(unsigned int i = 0; i < 6; ++i)
-//        EXPECT_DOUBLE_EQ(q_whole[_robot.right_leg.joint_numbers[i]],0.0);
-//    for(unsigned int i = 0; i < 7; ++i) {
-//        EXPECT_DOUBLE_EQ(q_whole[_robot.right_arm.joint_numbers[i]],0.0);
-//    }
-//}
+    EXPECT_LT( findMax((x_ref-conversion_utils_YARP::toYARP(x_now)).subcol(0,3,3)), 1E-3 );
+    EXPECT_LT( abs(findMin((x_ref-conversion_utils_YARP::toYARP(x_now)).subcol(0,3,3))), 1E-3 );
+    // checking for the position
+    for(unsigned int i = 0; i < 3; ++i) {
+        EXPECT_NEAR(x_ref(i,3),x_now(i,3),1E-4);
+    }
+    for(unsigned int i = 0; i < 3; ++i) {
+        for(unsigned int j = 0; j < 3; ++j) {
+            EXPECT_NEAR(x_ref(i,j),x_now(i,j),1E-4);
+        }
+    }
+}
 
 //TEST_F(testCartesianTask, testCartesianTaskRelativeWaistUpdateWorld_)
 //{
