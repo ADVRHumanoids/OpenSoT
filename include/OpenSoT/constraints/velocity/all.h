f0ceeba043abd81116cefcd83d1f7c472806ac9f
Enrico Mingo <enrico.mingo@iit.it>
2017-04-24 17:08:14 +0200

    Ported TestQPOases_ConvexHull

--- include/OpenSoT/constraints/velocity/all.h
+++ include/OpenSoT/constraints/velocity/all.h
@@ -1,6 +1,11 @@
+#ifndef __OPENSOT_CONSTRAINTS_VELOCITY_ALL__
+#define __OPENSOT_CONSTRAINTS_VELOCITY_ALL__
+
 #include <OpenSoT/constraints/velocity/ConvexHull.h>
 #include <OpenSoT/constraints/velocity/CoMVelocity.h>
-#include <OpenSoT/constraints/velocity/Dynamics.h>
+//#include <OpenSoT/constraints/velocity/Dynamics.h>
 #include <OpenSoT/constraints/velocity/JointLimits.h>
 #include <OpenSoT/constraints/velocity/VelocityLimits.h>
 #include <OpenSoT/constraints/velocity/SelfCollisionAvoidance.h>
+
+#endif
--- include/OpenSoT/tasks/velocity/all.h
+++ include/OpenSoT/tasks/velocity/all.h
@@ -1,3 +1,6 @@
+#ifndef __OPENSOT_TASKS_VELOCITY_ALL__
+#define __OPENSOT_TASKS_VELOCITY_ALL__
+
 #include <OpenSoT/tasks/velocity/Cartesian.h>
 #include <OpenSoT/tasks/velocity/CoM.h>
 #include <OpenSoT/tasks/velocity/Manipulability.h>
@@ -5,3 +8,5 @@
 #include <OpenSoT/tasks/velocity/MinimumEffort.h>
 #include <OpenSoT/tasks/velocity/MinimumVelocity.h>
 #include <OpenSoT/tasks/velocity/Postural.h>
+
+#endif
