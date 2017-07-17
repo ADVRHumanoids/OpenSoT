386c5ebc64affac2d2d0a3c787ca321685ed2b9b
Enrico Mingo <enrico.mingo@iit.it>
2017-07-17 11:47:05 +0200

    Removed logger

--- include/OpenSoT/utils/logger/flushers/all.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FLUSHERS_ALL_H__
-#define __FLUSHERS_ALL_H__
-
-#include <OpenSoT/utils/logger/flushers/RobotFlusher.h>
-#include <OpenSoT/utils/logger/flushers/tasks/velocity/Cartesian.h>
-#include <OpenSoT/utils/logger/flushers/tasks/velocity/CoM.h>
-#include <OpenSoT/utils/logger/flushers/constraints/velocity/Dynamics.h>
-
-#endif
