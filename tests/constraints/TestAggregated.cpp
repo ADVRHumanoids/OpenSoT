2c2f855b5f2e643cf72175b8b012345a70c09681
Enrico Mingo <enrico.mingo@iit.it>
2017-07-28 18:30:01 +0200

    Test Aggregated is now YARP FREE

--- tests/constraints/TestAggregated.cpp
+++ tests/constraints/TestAggregated.cpp
@@ -1,20 +1,13 @@
 #include <gtest/gtest.h>
-#include <advr_humanoids_common_utils/test_utils.h>
-#include <advr_humanoids_common_utils/idynutils.h>
-#include <advr_humanoids_common_utils/conversion_utils_YARP.h>
 #include <OpenSoT/constraints/Aggregated.h>
 #include <OpenSoT/constraints/velocity/VelocityLimits.h>
 #include <OpenSoT/constraints/BilateralConstraint.h>
 #include <OpenSoT/constraints/velocity/JointLimits.h>
 #include <OpenSoT/constraints/velocity/ConvexHull.h>
-#include <yarp/math/Math.h>
 #include <string>
-#include <ModelInterfaceIDYNUTILS/ModelInterfaceIDYNUTILS.h>
+#include <XBotInterface/ModelInterface.h>
 
-using namespace yarp::math;
 
-typedef idynutils2 iDynUtils;
-static void null_deleter(iDynUtils *) {}
 std::string robotology_root = std::getenv("ROBOTOLOGY_ROOT");
 std::string relative_path = "/external/OpenSoT/tests/configs/coman/configs/config_coman.yaml";
 std::string _path_to_cfg = robotology_root + relative_path;
@@ -51,6 +44,49 @@ class testAggregated : public ::testing::Test {
   // Objects declared here can be used by all tests in the test case for Foo.
 };
 
+void initializeIfNeeded()
+{
+    static bool is_initialized = false;
+
+    if(!is_initialized) {
+        time_t seed = time(NULL);
+        seed48((unsigned short*)(&seed));
+        srand((unsigned int)(seed));
+
+        is_initialized = true;
+    }
+
+}
+
+double getRandomAngle()
+{
+    initializeIfNeeded();
+    return drand48()*2.0*M_PI-M_PI;
+}
+
+double getRandomAngle(const double min, const double max)
+{
+    initializeIfNeeded();
+    assert(min <= max);
+    if(min < -M_PI || max > M_PI)
+        return getRandomAngle();
+
+    return (double)rand()/RAND_MAX * (max-min) + min;
+}
+
+Eigen::VectorXd getRandomAngles(const Eigen::VectorXd &min,
+                                               const Eigen::VectorXd &max,
+                                               const int size)
+{
+    initializeIfNeeded();
+    Eigen::VectorXd q(size);
+    assert(min.size() >= size);
+    assert(max.size() >= size);
+    for(unsigned int i = 0; i < size; ++i)
+        q(i) = getRandomAngle(min[i],max[i]);
+    return q;
+}
+
 // Tests that the Foo::Bar() method does Abc.
 TEST_F(testAggregated, AggregatedWorks) {
     using namespace OpenSoT::constraints;
@@ -63,25 +99,18 @@ TEST_F(testAggregated, AggregatedWorks) {
                                                     )
                           );
 
-    yarp::sig::Vector q(nJ, 0.0);
-    yarp::sig::Vector q_next(nJ, M_PI - 0.01);
+    Eigen::VectorXd q(nJ); q.setZero(nJ);
+    Eigen::VectorXd q_next(nJ); q_next.setConstant(nJ, M_PI - 0.01);
 
-    yarp::sig::Matrix A(nJ,nJ); A.eye();
-    yarp::sig::Vector bUpperBound(nJ,M_PI);
-    yarp::sig::Vector bLowerBound(nJ,0.0);
+    Eigen::MatrixXd A(nJ,nJ); A.setIdentity(nJ,nJ);
+    Eigen::VectorXd bUpperBound(nJ); bUpperBound.setConstant(nJ, M_PI);
+    Eigen::VectorXd bLowerBound(nJ); bLowerBound.setZero(nJ);
     constraints.push_back(Aggregated::ConstraintPtr(
-        new BilateralConstraint(conversion_utils_YARP::toEigen(A),
-                                conversion_utils_YARP::toEigen(bUpperBound),
-                                conversion_utils_YARP::toEigen(bLowerBound))
-                                                  )
-                          );
+        new BilateralConstraint(A,bUpperBound,bLowerBound)));
 
     constraints.push_back(Aggregated::ConstraintPtr(
-        new velocity::JointLimits(conversion_utils_YARP::toEigen(q),
-                                  conversion_utils_YARP::toEigen(bUpperBound),
-                                  conversion_utils_YARP::toEigen(bLowerBound))
-                          ));
-    Aggregated::ConstraintPtr aggregated(new Aggregated(constraints, conversion_utils_YARP::toEigen(q)));
+        new velocity::JointLimits(q,bUpperBound,bLowerBound)));
+    Aggregated::ConstraintPtr aggregated(new Aggregated(constraints, q));
 
     /* we should mash joint limits and velocity limits in one */
     EXPECT_TRUE(aggregated->getLowerBound().size() == nJ);
@@ -96,7 +125,7 @@ TEST_F(testAggregated, AggregatedWorks) {
 
     Eigen::VectorXd oldLowerBound = aggregated->getLowerBound();
     Eigen::VectorXd oldUpperBound = aggregated->getUpperBound();
-    aggregated->update(conversion_utils_YARP::toEigen(q_next));
+    aggregated->update(q_next);
     Eigen::VectorXd newLowerBound = aggregated->getLowerBound();
     Eigen::VectorXd newUpperBound = aggregated->getUpperBound();
     EXPECT_FALSE(oldLowerBound == newLowerBound);
@@ -105,15 +134,8 @@ TEST_F(testAggregated, AggregatedWorks) {
 
 TEST_F(testAggregated, UnilateralToBilateralWorks) {
 
-    iDynUtils robot("coman",
-                    std::string(OPENSOT_TESTS_ROBOTS_DIR)+"coman/coman.urdf",
-                    std::string(OPENSOT_TESTS_ROBOTS_DIR)+"coman/coman.srdf");
-
-
-    XBot::ModelInterfaceIDYNUTILS::Ptr _model_ptr;
-    _model_ptr = std::dynamic_pointer_cast<XBot::ModelInterfaceIDYNUTILS>
-            (XBot::ModelInterface::getModel(_path_to_cfg));
-    _model_ptr->loadModel(boost::shared_ptr<iDynUtils>(&robot, &null_deleter));
+    XBot::ModelInterface::Ptr _model_ptr;
+    _model_ptr = XBot::ModelInterface::getModel(_path_to_cfg);
 
     if(_model_ptr)
         std::cout<<"pointer address: "<<_model_ptr.get()<<std::endl;
@@ -122,7 +144,7 @@ TEST_F(testAggregated, UnilateralToBilateralWorks) {
 
 
     Eigen::VectorXd q;
-    q.setZero(robot.iDynTree_model.getNrOfDOFs());
+    q.setZero(_model_ptr->getJointNum());
 
     std::list<std::string> _links_in_contact;
     _links_in_contact.push_back("l_foot_lower_left_link");
@@ -140,7 +162,7 @@ TEST_F(testAggregated, UnilateralToBilateralWorks) {
     constraints.push_back(convexHull);
     OpenSoT::Constraint<Eigen::MatrixXd, Eigen::VectorXd>::ConstraintPtr aggregated(
                 new OpenSoT::constraints::Aggregated(constraints,
-                                                     robot.iDynTree_model.getNrOfDOFs()));
+                                                     _model_ptr->getJointNum()));
 
     EXPECT_TRUE(aggregated->getbLowerBound().rows() == aggregated->getbUpperBound().rows()) <<
                 "bLowerBound:" << aggregated->getbLowerBound() << std::endl <<
@@ -160,15 +182,11 @@ TEST_F(testAggregated, MultipleAggregationdWork) {
     const double dT = 1;
     const double qDotMax = 50;
     constraints.push_back(  Aggregated::ConstraintPtr(
-            new velocity::VelocityLimits(qDotMax,dT,nJ)
-                                                    )
-                          );
+            new velocity::VelocityLimits(qDotMax,dT,nJ)));
 
-    yarp::sig::Vector q(nJ);
-    q = conversion_utils_YARP::toYARP(tests_utils::getRandomAngles(
-                conversion_utils_YARP::toEigen(yarp::sig::Vector(nJ,-M_PI)),
-                conversion_utils_YARP::toEigen(yarp::sig::Vector(nJ,M_PI)),
-                                     nJ));
+    Eigen::VectorXd q(nJ);
+    q = getRandomAngles(Eigen::VectorXd::Constant(nJ, -M_PI),
+                                     Eigen::VectorXd::Constant(nJ, M_PI), nJ);
 
     Eigen::MatrixXd A;
     A.setZero(nJ,nJ);
@@ -177,15 +195,12 @@ TEST_F(testAggregated, MultipleAggregationdWork) {
     Eigen::VectorXd bLowerBound;
     bLowerBound.setZero(nJ);
     constraints.push_back(Aggregated::ConstraintPtr(
-        new BilateralConstraint(A, bUpperBound, bLowerBound)
-                                                  )
-                          );
+        new BilateralConstraint(A, bUpperBound, bLowerBound)));
 
-    Aggregated::ConstraintPtr aggregated(new Aggregated(constraints, conversion_utils_YARP::toEigen(q)));
+    Aggregated::ConstraintPtr aggregated(new Aggregated(constraints, q));
 
     constraints.push_back(  Aggregated::ConstraintPtr(
-            new velocity::VelocityLimits(qDotMax/2,dT,nJ)                        )
-                          );
+            new velocity::VelocityLimits(qDotMax/2,dT,nJ)));
 
     for(typename std::list<Aggregated::ConstraintPtr>::iterator i = constraints.begin();
         i != constraints.end(); ++i) {
@@ -193,12 +208,12 @@ TEST_F(testAggregated, MultipleAggregationdWork) {
         if(b->getLowerBound().size() > 0)
             std::cout << b->getLowerBound() << std::endl;
     }
-    Aggregated::ConstraintPtr aggregated2(new Aggregated(constraints, conversion_utils_YARP::toEigen(q)));
+    Aggregated::ConstraintPtr aggregated2(new Aggregated(constraints, q));
 
     constraints.push_back(aggregated);
     constraints.push_back(aggregated2);
 
-    Aggregated::ConstraintPtr aggregated3(new Aggregated(constraints, conversion_utils_YARP::toEigen(q)));
+    Aggregated::ConstraintPtr aggregated3(new Aggregated(constraints, q));
 
     Aggregated::ConstraintPtr aggregated4(new Aggregated(aggregated2,
                                                         aggregated3, q.size()));
@@ -207,10 +222,7 @@ TEST_F(testAggregated, MultipleAggregationdWork) {
     qq<<qq.setOnes()*-0.1;
     Aggregated::ConstraintPtr aggregated5(new Aggregated(aggregated4,
                                                         Aggregated::ConstraintPtr(
-                                        new velocity::JointLimits(conversion_utils_YARP::toEigen(q),
-                                                                  qq,
-                                                                  qq)
-                                                        ), q.size()));
+                                        new velocity::JointLimits(q,qq,qq)), q.size()));
 
 
     /* testing constraints stay put */
@@ -227,14 +239,14 @@ TEST_F(testAggregated, MultipleAggregationdWork) {
     ASSERT_TRUE(aggregated2->getLowerBound() == aggregated3->getLowerBound());
     ASSERT_TRUE(aggregated2->getUpperBound() == aggregated3->getUpperBound());
     // aggregated5->getLowerBound() > aggregated4.getLowerBound()
-    yarp::sig::Vector a = conversion_utils_YARP::toYARP(aggregated5->getLowerBound());
-    yarp::sig::Vector b = conversion_utils_YARP::toYARP(aggregated4->getLowerBound());
-    double min = findMin(a-b);
+    Eigen::VectorXd a = aggregated5->getLowerBound();
+    Eigen::VectorXd b = aggregated4->getLowerBound();
+    double min = (a-b).minCoeff() ;
     ASSERT_GT(min, 0);
     // aggregated5->getUpperBound() < aggregated4.getUpperBound()
-    a = conversion_utils_YARP::toYARP(aggregated5->getUpperBound());
-    b = conversion_utils_YARP::toYARP(aggregated4->getUpperBound());
-    double max = findMax(a-b);
+    a = aggregated5->getUpperBound();
+    b = aggregated4->getUpperBound();
+    double max = (a-b).maxCoeff() ;
     ASSERT_LT(max,0);
 }
 
