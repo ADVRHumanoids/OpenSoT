be7c75309661a8cc1f19865d2ff664bb91935c81
Arturo Laurenzi <arturo.laurenzi@iit.it>
2017-12-04 09:54:31 +0100

    Added task to minimize Affine

---
+++ include/OpenSoT/tasks/MinimizeVariable.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2017 IIT-ADVR
+ * Authors: Arturo Laurenzi
+ * email:  arturo.laurenzi@iit.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU Lesser General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+
+#ifndef __CONSTRAINT_MINIMIZE_VARIABLE_H__
+#define __CONSTRAINT_MINIMIZE_VARIABLE_H__
+
+#include <OpenSoT/Task.h>
+#include <OpenSoT/utils/Affine.h>
+#include <XBotInterface/ModelInterface.h>
+
+namespace OpenSoT { namespace tasks  {
+    
+class MinimizeVariable : public Task<Eigen::MatrixXd, Eigen::VectorXd> {
+  
+public:
+    
+    typedef boost::shared_ptr<MinimizeVariable> Ptr;
+    
+    MinimizeVariable(std::string task_id, 
+                      const AffineHelper& variable
+                      );
+    
+    virtual void _update(const Eigen::VectorXd& x);
+    
+    
+    
+private:
+    
+        AffineHelper _var;
+    
+};
+    
+} } 
+
+
+
+
+
+#endif
---
+++ src/tasks/MinimizeVariable.cpp
@@ -0,0 +1,16 @@
+#include <OpenSoT/tasks/MinimizeVariable.h>
+
+
+void OpenSoT::tasks::MinimizeVariable::_update(const Eigen::VectorXd& x)
+{
+
+}
+
+OpenSoT::tasks::MinimizeVariable::MinimizeVariable(std::string task_id, 
+                                                   const OpenSoT::AffineHelper& variable): 
+    Task< Eigen::MatrixXd, Eigen::VectorXd >(task_id, variable.getInputSize()),
+    _var(variable)
+{
+    _A = _var.getM();
+    _b = -_var.getq();
+}
