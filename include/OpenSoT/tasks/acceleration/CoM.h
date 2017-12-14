d5d2edbd4c60615d34d4e834d3c6a910cc3d8d5c
Enrico Mingo <enrico.mingo@iit.it>
2017-12-14 20:09:20 +0100

    Added CoM acceleration task ,needs computation of JDotQdot and test

---
+++ include/OpenSoT/tasks/acceleration/CoM.h
@@ -0,0 +1,150 @@
+/*
+ * Copyright (C) 2017 IIT-ADVR
+ * Authors: Enrico Mingo Hoffman
+ * email:  enrico.mingo@iit.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU Lesser General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+#ifndef __OPENSOT_ACCELERATION_TASK_COM_H__
+#define __OPENSOT_ACCELERATION_TASK_COM_H__
+
+#include <OpenSoT/Task.h>
+#include <OpenSoT/utils/Affine.h>
+#include <XBotInterface/ModelInterface.h>
+#include <XBotInterface/Utils.h>
+
+
+namespace OpenSoT { namespace tasks { namespace acceleration {
+/**
+  * The CoM class implements a task that tries to control the acceleration
+  * of the CoM w.r.t. world frame.
+  */
+    class CoM : public OpenSoT::Task<Eigen::MatrixXd, Eigen::VectorXd> {
+
+    public:
+
+        typedef boost::shared_ptr<CoM> Ptr;
+
+        /**
+         * @brief CoM
+         * @param robot the robot model
+         * @param x vector of size of joints of the robot (not used anymore)
+         */
+        CoM(const XBot::ModelInterface& robot, const Eigen::VectorXd& x);
+
+        /**
+         * @brief CoM
+         * @param robot the robot model
+         * @param qddot an affine structure variable
+         */
+        CoM(const XBot::ModelInterface& robot, const AffineHelper& qddot);
+
+        /**
+         * @brief getBaseLink
+         * @return "world"
+         */
+        const std::string& getBaseLink() const;
+
+        /**
+         * @brief getDistalLink
+         * @return "CoM
+         */
+        const std::string& getDistalLink() const;
+
+        /**
+         * @brief setReference set a position reference for the CoM
+         * @param ref
+         */
+        void setReference(const Eigen::Vector3d& ref);
+
+        /**
+         * @brief setReference position and velocity reference for the CoM
+         * @param pose_ref
+         * @param vel_ref
+         */
+        void setReference(const Eigen::Vector3d& pose_ref,
+                          const Eigen::Vector3d& vel_ref);
+
+        /**
+         * @brief setReference position, velocity and acceleration references for the CoM
+         * @param pose_ref
+         * @param vel_ref
+         * @param acc_ref
+         */
+        void setReference(const Eigen::Vector3d& pose_ref,
+                          const Eigen::Vector3d& vel_ref,
+                          const Eigen::Vector3d& acc_ref);
+
+        /**
+         * @brief getReference
+         * @param ref position reference
+         */
+        void getReference(Eigen::Vector3d& ref);
+
+        /**
+         * @brief getActualPose
+         * @param actual pose of the CoM
+         */
+        void getActualPose(Eigen::Vector3d& actual);
+
+        /**
+         * @brief resetReference ste the actual position as the postion reference, set to zero
+         * velocity and acceleration references
+         */
+        void resetReference();
+
+        virtual void _update(const Eigen::VectorXd& x);
+
+        virtual void _log(XBot::MatLogger::Ptr logger);
+
+        /**
+         * @brief setLambda set position and velocity gains of the feedback errors
+         * @param lambda1 postion gain
+         * @param lambda2 velocity gain
+         */
+        void setLambda(double lambda1, double lambda2);
+
+        /**
+         * @brief setLambda set position gain of the feedback error
+         * NOTE: velocity gain is set to 2*std::sqrt(lambda)
+         * @param lambda postion gain
+         */
+        virtual void setLambda(double lambda);
+
+    private:
+
+        static const std::string world_name;
+
+        std::string _base_link, _distal_link;
+        const XBot::ModelInterface& _robot;
+        AffineHelper _qddot;
+        AffineHelper _cartesian_task;
+
+        Eigen::MatrixXd _J;
+        Eigen::Vector3d _jdotqdot;
+
+        Eigen::Vector3d _pose_ref, _pose_current;
+        Eigen::Vector3d _pose_error, _vel_ref, _vel_current, _acc_ref;
+
+        double _lambda2;
+
+    };
+
+} } }
+
+
+
+
+
+
+#endif
---
+++ src/tasks/acceleration/CoM.cpp
@@ -0,0 +1,147 @@
+#include <OpenSoT/tasks/acceleration/CoM.h>
+#include <XBotInterface/RtLog.hpp>
+using XBot::Logger;
+
+const std::string OpenSoT::tasks::acceleration::CoM::world_name = "world";
+
+OpenSoT::tasks::acceleration::CoM::CoM(const XBot::ModelInterface& robot, const Eigen::VectorXd& x):
+    Task< Eigen::MatrixXd, Eigen::VectorXd >("CoM", x.size()),
+    _robot(robot),
+    _distal_link("CoM"),
+    _base_link(world_name)
+{
+    _qddot = AffineHelper::Identity(x.size());
+
+    resetReference();
+
+    _vel_ref.setZero();
+    _acc_ref.setZero();
+
+    _lambda = 100.;
+    _lambda2 = 2.*sqrt(_lambda);
+
+    update(Eigen::VectorXd(1));
+
+    setWeight(Eigen::MatrixXd::Identity(3,3));
+}
+
+OpenSoT::tasks::acceleration::CoM::CoM(const XBot::ModelInterface &robot, const AffineHelper &qddot):
+    Task< Eigen::MatrixXd, Eigen::VectorXd >("CoM", qddot.getInputSize()),
+    _robot(robot),
+    _distal_link("CoM"),
+    _base_link(world_name),
+    _qddot(qddot)
+{
+    resetReference();
+
+    _vel_ref.setZero();
+    _acc_ref.setZero();
+
+    _lambda = 100.;
+    _lambda2 = 2.*sqrt(_lambda);
+
+    update(Eigen::VectorXd(1));
+
+    setWeight(Eigen::MatrixXd::Identity(3,3));
+}
+
+const std::string& OpenSoT::tasks::acceleration::CoM::getBaseLink() const
+{
+    return _base_link;
+}
+
+const std::string& OpenSoT::tasks::acceleration::CoM::getDistalLink() const
+{
+    return _distal_link;
+}
+
+void OpenSoT::tasks::acceleration::CoM::_update(const Eigen::VectorXd& x)
+{
+
+    _robot.getCOMJacobian(_J);
+    _robot.getCOM(_pose_current);
+    _robot.getCOMVelocity(_vel_current);
+    ///TODO: _robot.computeJdotQdot(_distal_link, Eigen::Vector3d::Zero(), _jdotqdot);
+
+
+    _pose_error = _pose_ref - _pose_current;
+
+    _cartesian_task = _J*_qddot + _jdotqdot;
+    _cartesian_task = _cartesian_task - _acc_ref
+                                      - _lambda2*(_vel_ref - _vel_current)
+                                      - _lambda*_pose_error ;
+
+    _A = _cartesian_task.getM();
+    _b = -_cartesian_task.getq();
+
+    _vel_ref.setZero();
+    _acc_ref.setZero();
+}
+
+void OpenSoT::tasks::acceleration::CoM::setReference(const Eigen::Vector3d& ref)
+{
+    _pose_ref = ref;
+    _vel_ref.setZero();
+    _acc_ref.setZero();
+}
+
+void OpenSoT::tasks::acceleration::CoM::setReference(const Eigen::Vector3d& pose_ref,
+                                                           const Eigen::Vector3d& vel_ref)
+{
+    _pose_ref = pose_ref;
+    _vel_ref = vel_ref;
+    _acc_ref.setZero();
+}
+
+void OpenSoT::tasks::acceleration::CoM::setReference(const Eigen::Vector3d& pose_ref,
+                                                           const Eigen::Vector3d& vel_ref,
+                                                           const Eigen::Vector3d& acc_ref)
+{
+    _pose_ref = pose_ref;
+    _vel_ref = vel_ref;
+    _acc_ref = acc_ref;
+}
+
+void OpenSoT::tasks::acceleration::CoM::setLambda(double lambda)
+{
+    if(lambda < 0){
+        XBot::Logger::error("in %s: illegal lambda (%f < 0) \n", __func__, lambda);
+        return;
+    }
+    _lambda = lambda;
+    _lambda2 = 2*std::sqrt(lambda);
+}
+
+void OpenSoT::tasks::acceleration::CoM::setLambda(double lambda1, double lambda2)
+{
+    if( lambda1 < 0 || lambda2 < 0 )
+    {
+        XBot::Logger::error("in %s: illegal lambda (%f < 0 || %f < 0) \n", __func__, lambda1, lambda2);
+        return;
+    }
+
+    _lambda = lambda1;
+    _lambda2 = lambda2;
+}
+
+void OpenSoT::tasks::acceleration::CoM::resetReference()
+{
+    _robot.getCOM(_pose_ref);
+    _vel_ref.setZero();
+    _acc_ref.setZero();
+}
+
+void OpenSoT::tasks::acceleration::CoM::_log(XBot::MatLogger::Ptr logger)
+{
+    logger->add(getTaskID() + "_pose_error", _pose_error);
+}
+
+void OpenSoT::tasks::acceleration::CoM::getReference(Eigen::Vector3d& ref)
+{
+    ref = _pose_ref;
+}
+
+void OpenSoT::tasks::acceleration::CoM::getActualPose(Eigen::Vector3d& actual)
+{
+    actual = _pose_current;
+}
