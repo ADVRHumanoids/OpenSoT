609777fb69a303916e942615d979a2f984fb7248
Enrico Mingo <enrico.mingo@iit.it>
2017-12-19 19:20:44 +0100

    Added base class for FloatinBaseEstimation

---
+++ include/OpenSoT/utils/FloatingBaseEstimation.h
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2017 IIT-ADVR
+ * Authors: Enrico Mingo Hoffman, Arturo Laurenzi
+ * email:  enrico.mingo@iit.it, arturo.laurenzi@iit.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU Lesser General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+#ifndef _OPENSOT_FLOATING_BASE_ESTIMATION_H_
+#define _OPENSOT_FLOATING_BASE_ESTIMATION_H_
+
+#include <boost/shared_ptr.hpp>
+#include <XBotInterface/ModelInterface.h>
+
+namespace OpenSoT {
+
+    class FloatingBaseEstimation{
+    public:
+        typedef boost::shared_ptr<FloatingBaseEstimation> Ptr;
+
+        FloatingBaseEstimation(XBot::ModelInterface::Ptr model,
+                               XBot::ImuSensor::ConstPtr imu,
+                               std::vector<std::string> contact_links,
+                               const Eigen::MatrixXd& contact_matrix):
+            _model(model),
+            _imu(imu),
+            _contact_links(contact_links),
+            _contact_matrix(contact_matrix)
+        {
+            if(_contact_matrix.rows()>6)
+                throw std::invalid_argument("contact_matrix rows > 6 is invalid");
+            if(_contact_matrix.cols() != 6)
+                throw std::invalid_argument("contact_matrix cols != 6 is invalid");
+
+            update(0);
+        }
+
+        ~FloatingBaseEstimation();
+
+        virtual void update(double dT) = 0;
+
+        void log(XBot::MatLogger::Ptr logger)
+        {
+
+        }
+
+        const Eigen::Vector6d& getFloatingBaseVelocity() const
+        {
+            return _qdot.segment(0,6);
+        }
+
+        const Eigen::Affine3d& getFloatingBasePose()
+        {
+            _model->getFloatingBasePose(_fb_pose);
+            return _fb_pose;
+        }
+
+        const Eigen::Vector6d& getFloatingBaseJoints() const
+        {
+            return _q.segment(0,6);
+        }
+    protected:
+        XBot::ModelInterface::Ptr _model;
+        XBot::ImuSensor::ConstPtr _imu;
+        std::vector<std::string> _contact_links;
+        Eigen::MatrixXd _contact_matrix;
+        Eigen::VectorXd _q;
+        Eigen::VectorXd _qdot;
+        Eigen::Affine3d _fb_pose;
+    };
+}
+
+#endif
--- src/tasks/floating_base/Contact.cpp
+++ src/tasks/floating_base/Contact.cpp
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2017 IIT-ADVR
+ * Authors: Enrico Mingo Hoffman, Arturo Laurenzi
+ * email:  enrico.mingo@iit.it, arturo.laurenzi@iit.it
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU Lesser General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
 #include <OpenSoT/tasks/floating_base/Contact.h>
 
 OpenSoT::tasks::floating_base::Contact::Contact(XBot::ModelInterface &robot,
