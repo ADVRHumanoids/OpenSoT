6fe8b759c20e9d04523db58808a3852621d64f53
EnricoMingo <enricomingo@gmail.com>
2018-03-12 10:53:59 +0100

    Added first version of BackEnda base class

---
+++ include/OpenSoT/solvers/BackEnd.h
@@ -0,0 +1,173 @@
+#ifndef _WB_SOT_SOLVERS_BACK_END_H_
+#define _WB_SOT_SOLVERS_BACK_END_H_
+
+#include <Eigen/Dense>
+#include <XBotInterface/Logger.hpp>
+#include <boost/any.hpp>
+
+#define DEFAULT_EPS_REGULARISATION 2E2
+
+namespace OpenSoT{
+    namespace solvers{
+    class BackEnd{
+    public:
+        BackEnd(const int number_of_variables, const int number_of_constraints);
+        ~BackEnd();
+
+        /**
+         * @brief getSolution return the actual solution of the QP problem
+         * @return solution
+         */
+        const Eigen::VectorXd& getSolution(){return _solution;}
+
+        /**
+         * Getters for internal matrices and Eigen::VectorXds
+         */
+        const Eigen::MatrixXd& getH(){return _H;}
+        const Eigen::VectorXd& getg(){return _g;}
+        const Eigen::MatrixXd& getA(){return _A;}
+        const Eigen::VectorXd& getlA(){return _lA;}
+        const Eigen::VectorXd& getuA(){return _uA;}
+        const Eigen::VectorXd& getl(){return _l;}
+        const Eigen::VectorXd& getu(){return _u;}
+
+        /**
+         * @brief log Tasks, Constraints and Bounds matrices
+         * @param logger a pointer to a MatLogger
+         * @param i an index related to the particular index of the problem
+         */
+        void log(XBot::MatLogger::Ptr logger, int i);
+
+        /**
+         * @brief updateProblem update the whole problem see updateTask(), updateConstraints() and updateBounds()
+         * @param H updated task matrix
+         * @param g updated reference Eigen::VectorXd
+         * @param A update constraint matrix
+         * @param lA update lower constraint Eigen::VectorXd
+         * @param uA update upper constraint Eigen::VectorXd
+         * @param l update lower bounds
+         * @param u update upper bounds
+         * @return if the problem is correctly updated
+         */
+        bool QPOasesProblem::updateProblem(const Eigen::MatrixXd &H, const Eigen::VectorXd &g,
+                                           const Eigen::MatrixXd &A, const Eigen::VectorXd &lA, const Eigen::VectorXd &uA,
+                                           const Eigen::VectorXd &l, const Eigen::VectorXd &u);
+
+        ///VIRTUAL METHODS
+
+        /**
+         * @brief updateTask update internal H and g:
+         * _H = H
+         * _g = g
+         * for now is not possible to have different size of H and g wrt internal ones
+         * @param H updated task matrix
+         * @param g updated reference Eigen::VectorXd
+         * @return true if task is correctly updated
+         */
+        virtual bool updateTask(const Eigen::MatrixXd& H, const Eigen::VectorXd& g);
+
+
+        /**
+         * @brief updateConstraints update internal A, lA and uA
+         * _A = A
+         * _lA = lA
+         * _uA = uA
+         * A, lA and uA can change rows size to allow variable constraints
+         * @param A update constraint matrix
+         * @param lA update lower constraint Eigen::VectorXd
+         * @param uA update upper constraint Eigen::VectorXd
+         * @return true if constraints are correctly updated
+         */
+        virtual bool updateConstraints(const Eigen::Ref<const Eigen::MatrixXd>& A,
+                                       const Eigen::Ref<const Eigen::VectorXd> &lA,
+                                       const Eigen::Ref<const Eigen::VectorXd> &uA);
+
+        /**
+         * @brief updateBounds update internal l and u
+         * _l = l
+         * _u = u
+         * @param l update lower bounds
+         * @param u update upper bounds
+         * @return true if bounds are correctly updated
+         */
+        virtual bool updateBounds(const Eigen::VectorXd& l, const Eigen::VectorXd& u);
+
+        ///PURE VIRTUAL METHODS:
+
+        /**
+         * @brief initProblem initialize the QP problem and get the solution, the dual solution,
+         * bounds and constraints.
+         * The QP problem has the following structure:
+         *
+         *      min = ||Hx - g||
+         *  st.     lA <= Ax <= uA
+         *           l <=  x <= u
+         * @param H Task Matrix
+         * @param g Task references
+         * @param A Constraint Matrix
+         * @param lA lower constraint Eigen::VectorXd
+         * @param uA upper constraint Eigen::VectorXd
+         * @param l lower bounds
+         * @param u upper bounds
+         * @return true if the problem can be solved
+         */
+        virtual bool initProblem(const Eigen::MatrixXd& H, const Eigen::VectorXd& g,
+                                 const Eigen::MatrixXd& A, const Eigen::VectorXd& lA, const Eigen::VectorXd& uA,
+                                 const Eigen::VectorXd& l, const Eigen::VectorXd& u) = 0;
+        /**
+         * @brief solve the QP problem
+         * @return true if the QP problem is solved (implementation should use internally _solution)
+         */
+        virtual bool solve() = 0;
+
+        /**
+         * @brief getOptions return the options of the QP problem
+         * @return options, a boost::any object since we do not know the type of the object which depends on the solver
+         */
+        boost::any getOptions() = 0;
+
+        /**
+         * @brief setOptions of the QP problem.
+         * @param options, a boost::any object since we do not know the type of the object which depends on the solver
+         */
+        void setOptions(const boost::any& options) = 0;
+
+    protected:
+        ///VIRTUAL METHODS
+        /**
+         * @brief _log can be used to log extra information
+         * @param logger a pointer to a Matlogger
+         * @param i an index related to the particular index of the problem
+         */
+        void _log(XBot::MatLogger::Ptr logger, int i){}
+
+        /**
+         * Define a cost function: ||Hx - g||
+         */
+        Eigen::MatrixXd _H;
+        Eigen::VectorXd _g;
+
+        /**
+         * Define a set of constraints weighted with A: lA <= Ax <= uA
+         */
+        Eigen::MatrixXd _A;
+        Eigen::VectorXd _lA;
+        Eigen::VectorXd _uA;
+
+        /**
+         * Define a set of bounds on solution: l <= x <= u
+         */
+        Eigen::VectorXd _l;
+        Eigen::VectorXd _u;
+
+        /**
+         * Solution of the QP problem
+         */
+        Eigen::VectorXd _solution;
+
+    };
+
+    }
+}
+
+#endif
---
+++ src/solvers/BackEnd.cpp
@@ -0,0 +1,122 @@
+#include <OpenSoT/solvers/BackEnd.h>
+
+using namespace OpenSoT::solvers;
+
+BackEnd::BackEnd(const int number_of_variables, const int number_of_constraints)
+{
+    _H.setZero(number_of_variables,number_of_variables);
+    _g.setZero(number_of_variables);
+    _A.setZero(number_of_constraints,number_of_variables);
+    _lA.setZero(number_of_constraints);
+    _uA.setZero(number_of_constraints);
+    _l.setZero(number_of_variables);
+    _u.setZero(number_of_variables);
+}
+
+bool BackEnd::updateConstraints(const Eigen::Ref<const Eigen::MatrixXd>& A,
+                                const Eigen::Ref<const Eigen::VectorXd> &lA,
+                                const Eigen::Ref<const Eigen::VectorXd> &uA)
+{
+    if(!(A.cols() == _H.cols())){
+        XBot::Logger::error("A cols: %i \n", A.cols());
+        XBot::Logger::error("should be: %i \n", _H.cols());
+        return false;}
+    if(!(lA.rows() == A.rows())){
+        XBot::Logger::error("lA size: %i \n", lA.rows());
+        XBot::Logger::error("A rows: %i \n", A.rows());
+        return false;}
+    if(!(lA.rows() == uA.rows())){
+        XBot::Logger::error("lA size: %i \n", lA.rows());
+        XBot::Logger::error("uA size: %i \n", uA.rows());
+        return false;}
+
+    if(A.rows() == _A.rows())
+    {
+        _A = A;
+        _lA = lA;
+        _uA = uA;
+        return true;
+    }
+    else
+        return false;
+}
+
+BackEnd::updateTask(const Eigen::MatrixXd &H, const Eigen::VectorXd &g)
+{
+    if(!(_g.rows() == _H.rows())){
+        XBot::Logger::error("g size: %i \n", _g.rows());
+        XBot::Logger::error("H size: %i \n", _H.rows());
+        assert(_g.rows() == _H.rows());
+        return false;}
+    if(!(_H.cols() == H.cols())){
+        XBot::Logger::error("H cols: %i \n", H.cols());
+        XBot::Logger::error("should be: %i \n", _H.cols());
+        return false;}
+
+    if(_H.rows() == H.rows())
+    {
+        _H = H;
+        _g = g;
+
+        return true;
+    }
+    else
+        return false;
+}
+
+bool BackEnd::updateBounds(const Eigen::VectorXd &l, const Eigen::VectorXd &u)
+{
+    if(!(l.rows() == _l.rows())){
+        XBot::Logger::error("l size: %i \n", l.rows());
+        XBot::Logger::error("should be: %i \n", _l.rows());
+        return false;}
+    if(!(u.rows() == _u.rows())){
+        XBot::Logger::error("u size: %i \n", u.rows());
+        XBot::Logger::error("should be: %i \n", _u.rows());
+        return false;}
+    if(!(l.rows() == u.rows())){
+        XBot::Logger::error("l size: %i \n", l.rows());
+        XBot::Logger::error("u size: %i \n", u.rows());
+        return false;}
+
+    _l = l;
+    _u = u;
+
+    return true;
+}
+
+bool BackEnd::updateProblem(const Eigen::MatrixXd &H, const Eigen::VectorXd &g,
+                            const Eigen::MatrixXd &A, const Eigen::VectorXd &lA, const Eigen::VectorXd &uA,
+                            const Eigen::VectorXd &l, const Eigen::VectorXd &u)
+{
+    bool success = true;
+    success = success && updateBounds(l, u);
+    success = success && updateConstraints(A, lA, uA);
+    success = success && updateTask(H, g);
+    return success;
+}
+
+BackEnd::~BackEnd()
+{
+
+}
+
+void BackEnd::log(XBot::MatLogger::Ptr logger, int i)
+{
+    logger->add("H_"+std::to_string(i), _H);
+    logger->add("g_"+std::to_string(i), _g);
+    if(_A.rows() > 0 && _A.cols() > 0)
+        logger->add("A_"+std::to_string(i), _A);
+    if(_lA.size() > 0)
+        logger->add("lA_"+std::to_string(i), _lA);
+    if(_uA.size() > 0)
+        logger->add("uA_"+std::to_string(i), _uA);
+    if(_l.size() > 0)
+        logger->add("l_"+std::to_string(i), _l);
+    if(_u.size() > 0)
+        logger->add("u_"+std::to_string(i), _u);
+    if(_solution.size() > 0)
+        logger->add("solution_"+std::to_string(i), _solution);
+
+    _log(logger, i);
+}
