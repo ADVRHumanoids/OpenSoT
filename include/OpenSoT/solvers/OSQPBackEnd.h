a7cfb673e79f18fdbed8cb156491b78da6398008
Arturo Laurenzi <arturo.laurenzi@iit.it>
2018-03-13 20:47:54 +0100

    Trying to implement sparsity in a correct way (WIP)

--- include/OpenSoT/solvers/OSQPBackEnd.h
+++ include/OpenSoT/solvers/OSQPBackEnd.h
@@ -11,67 +11,88 @@
 using namespace OpenSoT::utils;
 
 namespace OpenSoT{
-    namespace solvers{
-    class OSQPBackEnd:  public BackEnd{
-    public:
-          typedef MatrixPiler VectorPiler;
+namespace solvers{
+    
+class OSQPBackEnd:  public BackEnd{
+    
+public:
+    
+    typedef MatrixPiler VectorPiler;
 
-          OSQPBackEnd(const int number_of_variables,
-                      const int number_of_constraints);
-          ~OSQPBackEnd();
+    OSQPBackEnd(const int number_of_variables,
+                const int number_of_constraints);
+    
+    ~OSQPBackEnd();
 
-          virtual bool initProblem(const Eigen::MatrixXd &H, const Eigen::VectorXd &g,
-                                                   const Eigen::MatrixXd &A,
-                                                   const Eigen::VectorXd &lA, const Eigen::VectorXd &uA,
-                                                   const Eigen::VectorXd &l, const Eigen::VectorXd &u);
+    virtual bool initProblem(const Eigen::MatrixXd &H, const Eigen::VectorXd &g,
+                             const Eigen::MatrixXd &A,
+                             const Eigen::VectorXd &lA, const Eigen::VectorXd &uA,
+                             const Eigen::VectorXd &l, const Eigen::VectorXd &u);
 
-          virtual bool solve();
+    virtual bool solve();
 
-          virtual boost::any getOptions();
+    virtual boost::any getOptions();
 
-          virtual void setOptions(const boost::any& options);
+    virtual void setOptions(const boost::any& options);
 
-          virtual bool updateTask(const Eigen::MatrixXd& H, const Eigen::VectorXd& g);
+    virtual bool updateTask(const Eigen::MatrixXd& H, const Eigen::VectorXd& g);
+            
+    virtual bool updateConstraints(const Eigen::Ref<const Eigen::MatrixXd>& A, 
+                                const Eigen::Ref<const Eigen::VectorXd>& lA, 
+                                const Eigen::Ref<const Eigen::VectorXd>& uA);
 
-    private:
-        /**
-         * @brief _problem is the internal OSQPWorkspace
-         */
-        boost::shared_ptr<OSQPWorkspace> _workspace;
+    virtual bool updateBounds(const Eigen::VectorXd& l, const Eigen::VectorXd& u);
 
-        /**
-         * @brief _data internal OSQPData
-         */
-        boost::shared_ptr<OSQPData> _data;
+private:
+    
+    typedef Eigen::SparseMatrix<double> SparseMatrix;
+    typedef Eigen::SparseMatrix<double, Eigen::RowMajor> SparseMatrixRowMajor;
+    
+    void __generate_data_struct();
+    void update_osqp_data();
+    
+    void upper_triangular_sparse_update();
+    
+    
+    /**
+    * @brief _problem is the internal OSQPWorkspace
+    */
+    boost::shared_ptr<OSQPWorkspace> _workspace;
 
-        /**
-         * @brief _settings internal OSQPSettings
-         */
-        boost::shared_ptr<OSQPSettings> _settings;
+    /**
+    * @brief _data internal OSQPData
+    */
+    boost::shared_ptr<OSQPData> _data;
 
-        MatrixPiler _Apiled;
-        VectorPiler _lApiled;
-        VectorPiler _uApiled;
+    /**
+    * @brief _settings internal OSQPSettings
+    */
+    boost::shared_ptr<OSQPSettings> _settings;
 
-        Eigen::SparseMatrix<double> _Asp;
-        Eigen::SparseMatrix<double> _Psp;
+    Eigen::VectorXd _lb_piled, _ub_piled;
 
-        Eigen::MatrixXd I;
+    SparseMatrix _Asparse, _Asparse_upper;
+    SparseMatrixRowMajor _Asparse_rowmaj;
+    SparseMatrix _Psparse;
+    Eigen::MatrixXd _Pdense;
+    Eigen::VectorXd _P_values;
 
-        boost::shared_ptr<csc> _Acsc;
-        boost::shared_ptr<csc> _Pcsc;
+    Eigen::MatrixXd _eye;
 
-        /**
-         * @brief toData copies all the internal Eigen Matrices to OSQPData
-         */
-        void toData();
+    boost::shared_ptr<csc> _Acsc;
+    boost::shared_ptr<csc> _Pcsc;
+
+    /**
+    * @brief toData copies all the internal Eigen Matrices to OSQPData
+    */
+    void toData();
 
 //        void print_csc_matrix_raw(csc* a, const std::string& name);
 
-        void setCSCMatrix(csc* a, Eigen::SparseMatrix<double>& A);
+    void setCSCMatrix(csc* a, SparseMatrix& A);
 
-    };
-    }
+};
+}
 }
 
 #endif
--- src/solvers/OSQPBackEnd.cpp
+++ src/solvers/OSQPBackEnd.cpp
@@ -1,21 +1,79 @@
 #include <OpenSoT/solvers/OSQPBackEnd.h>
 #include <osqp/glob_opts.h>
-
+#include <exception>
 using namespace OpenSoT::solvers;
 
 OSQPBackEnd::OSQPBackEnd(const int number_of_variables,
-            const int number_of_constraints):
-    BackEnd(number_of_variables, number_of_constraints),
-    _Apiled(number_of_variables),_lApiled(1), _uApiled(1)
+                         const int number_of_constraints):
+    BackEnd(number_of_variables, number_of_constraints)
 {
+    
+    #ifdef DLONG
+        throw std::runtime_error("DLONG option in OSQP should be set to OFF in CMakeLists!");
+    #endif
+    
+    _eye.setIdentity(number_of_variables, number_of_variables);
+    _P_values.resize(getNumVariables()*(getNumVariables() + 1)/2);
+    
     _settings.reset(new OSQPSettings());
-    _data.reset(new OSQPData());
-
-    I.setIdentity(number_of_variables, number_of_variables);
-
+    _settings->verbose = 0;
+    
     osqp_set_default_settings(_settings.get());
-    _settings.get()->verbose = 0;
+    
+    _data.reset(new OSQPData());
+    _Pcsc.reset(new csc());
+    _Acsc.reset(new csc());
+    
+    _lb_piled.setConstant(getNumVariables() + getNumConstraints(), -1.0);
+    _ub_piled.setConstant(getNumVariables() + getNumConstraints(),  1.0);
+ 
+    __generate_data_struct();
+    
+    
 }
+
+void OSQPBackEnd::__generate_data_struct()
+{
+    /* Set appropriate sparsity pattern to P (upper triangular) */
+    
+    Eigen::MatrixXd sp_pattern, ones;
+    sp_pattern.setZero(getNumVariables(), getNumVariables());
+    ones.setOnes(getNumVariables(), getNumVariables());
+    
+    sp_pattern.triangularView<Eigen::Upper>() = ones;
+    
+    _Psparse = sp_pattern.sparseView();
+    
+    setCSCMatrix(_Pcsc.get(), _Psparse);
+    
+    
+    
+    /* Set appropriate sparsity pattern to A (dense + diagonal) */
+    ones.setOnes(getNumConstraints(), getNumVariables());
+    sp_pattern.setZero(getNumConstraints() + getNumVariables(), getNumVariables());
+    sp_pattern.topRows(getNumConstraints()) = ones;
+    sp_pattern.bottomRows(getNumVariables()) = Eigen::MatrixXd::Identity(getNumVariables(), getNumVariables());
+    
+    _Asparse_upper = ones.sparseView();
+    _Asparse = sp_pattern.sparseView();
+    _Asparse_rowmaj = sp_pattern.sparseView();
+    
+    setCSCMatrix(_Acsc.get(), _Asparse);
+    
+    
+    /* Fill data */
+    
+    _data->n = getNumVariables();
+    _data->m = getNumConstraints() + getNumVariables();
+    _data->l = _lb_piled.data();
+    _data->u = _ub_piled.data();
+    _data->q = _g.data();
+    _data->A = _Acsc.get();
+    _data->P = _Pcsc.get();
+    
+    
+}
+
 
 void OSQPBackEnd::setCSCMatrix(csc* a, Eigen::SparseMatrix<double>& A)
 {
@@ -29,95 +87,83 @@ void OSQPBackEnd::setCSCMatrix(csc* a, Eigen::SparseMatrix<double>& A)
 
 bool OSQPBackEnd::updateTask(const Eigen::MatrixXd &H, const Eigen::VectorXd &g)
 {
-    if(!(_g.rows() == _H.rows())){
-        XBot::Logger::error("g size: %i \n", _g.rows());
-        XBot::Logger::error("H size: %i \n", _H.rows());
-        assert(_g.rows() == _H.rows());
-        return false;}
-    if(!(_H.cols() == H.cols())){
-        XBot::Logger::error("H cols: %i \n", H.cols());
-        XBot::Logger::error("should be: %i \n", _H.cols());
-        return false;}
-
-    if(_H.rows() == H.rows())
+    bool success = BackEnd::updateTask(H, g);
+    
+    if(!success)
     {
-        _H = H.triangularView<Eigen::Upper>();
-        _g = g;
-
-        return true;
-    }
-    else
         return false;
+    }
+    
+    
+    int idx = 0;
+    for(int c = 0; c < getNumVariables(); c++)
+    {
+        _P_values.segment(idx, c+1) = H.col(c).head(c+1);
+        idx += c+1;
+    }
+    
+    
+    return true;
 }
+
+
+
+
+bool OSQPBackEnd::updateConstraints(const Eigen::Ref<const Eigen::MatrixXd>& A, 
+                                const Eigen::Ref<const Eigen::VectorXd>& lA, 
+                                const Eigen::Ref<const Eigen::VectorXd>& uA)
+{
+    bool success = BackEnd::updateConstraints(A, lA, uA);
+    
+    if(!success)
+    {
+        return false;
+    }
+        
+    /* Update values in A upper part (constraints) */
+    memcpy(_Asparse_upper.valuePtr(), A.data(), A.size()*sizeof(double));
+    _Asparse_rowmaj.topRows(getNumConstraints()) = _Asparse_upper;
+    _Asparse = _Asparse_rowmaj;
+    
+    /* Update constraints bounds */
+    _lb_piled.head(getNumConstraints()) = lA;
+    _ub_piled.head(getNumConstraints()) = uA;
+    
+    return true;
+}
+
+bool OSQPBackEnd::updateBounds(const Eigen::VectorXd& l, const Eigen::VectorXd& u)
+{
+    bool success =  OpenSoT::solvers::BackEnd::updateBounds(l, u);
+    
+    if(!success)
+    {
+        return false;
+    }
+    
+    _lb_piled.tail(getNumVariables()) = l;
+    _ub_piled.tail(getNumVariables()) = u;
+    
+    return true;
+}
+
 
 bool OSQPBackEnd::solve()
 {
-    if(_A.rows() > 0)
-    {
-        _Apiled.set(_A);
-        _lApiled.set(_lA);
-        _uApiled.set(_uA);
-
-        if(_l.rows() > 0)
-        {
-            _Apiled.pile(I);
-            _lApiled.pile(_l);
-            _uApiled.pile(_u);
-        }
-    }
-    else if(_l.rows() > 0)
-    {
-        _Apiled.set(I);
-        _lApiled.set(_l);
-        _uApiled.set(_u);
-    }
-
-    _Asp = _Apiled.generate_and_get().sparseView();
-    _Psp = _H.sparseView();
-
-    _Asp.makeCompressed();
-    _Psp.makeCompressed();
-
-    setCSCMatrix(_Acsc.get(), _Asp);
-    setCSCMatrix(_Pcsc.get(), _Psp);
-
+    
+    
     osqp_update_lin_cost(_workspace.get(), _g.data());
-    osqp_update_bounds(_workspace.get(), _lApiled.generate_and_get().data(), _uApiled.generate_and_get().data());
-    _workspace.get()->data->A = _Acsc.get();
-    _workspace.get()->data->P = _Pcsc.get();
-
-
-    ///QUESTO è DA RIMUOVERE!!!
-    _workspace.reset(osqp_setup(_data.get(), _settings.get()));
-
-
-
-    osqp_update_warm_start(_workspace.get(), 1);
-    osqp_warm_start(_workspace.get(), _solution.data(), _workspace->solution->y); //We try with both primal/dual initial guess
-
+    osqp_update_bounds(_workspace.get(), _lb_piled.data(), _ub_piled.data());
+    osqp_update_A(_workspace.get(), _Asparse.valuePtr(), nullptr, _Asparse.nonZeros());
+    osqp_update_P(_workspace.get(), _P_values.data(), nullptr, _P_values.size());
+    
+    
     bool exitflag = osqp_solve(_workspace.get());
-
-    if(exitflag != 0)
-    {
-        osqp_warm_start_x(_workspace.get(), _solution.data()); //We try only with primal initial guess
-
-        exitflag = osqp_solve(_workspace.get());
-
-        if(exitflag != 0)
-        {
-            osqp_update_warm_start(_workspace.get(), 0); //We try without warm start
-            exitflag = osqp_solve(_workspace.get());
-            if(exitflag != 0)
-                return false;
-            _solution = Eigen::Map<Eigen::VectorXd>(_workspace->solution->x, _solution.size());
-            return true;
-        }
-
-        _solution = Eigen::Map<Eigen::VectorXd>(_workspace->solution->x, _solution.size());
-        return true;
-    }
+    
     _solution = Eigen::Map<Eigen::VectorXd>(_workspace->solution->x, _solution.size());
-    return true;
+    
+    return exitflag;
+    
 }
 
 boost::any OSQPBackEnd::getOptions()
@@ -136,77 +182,30 @@ bool OSQPBackEnd::initProblem(const Eigen::MatrixXd &H, const Eigen::VectorXd &g
                                  const Eigen::VectorXd &lA, const Eigen::VectorXd &uA,
                                  const Eigen::VectorXd &l, const Eigen::VectorXd &u)
 {
-#ifdef DLONG
-    XBot::Logger::error("DLONG option in OSQP should be set to OFF in CMakeLists!");
-    return false;
-#endif
-
-    _H = H.triangularView<Eigen::Upper>(); _g = g; _A = A; _lA = lA; _uA = uA; _l = l; _u = u;
-
-    if(_A.rows() > 0)
-    {
-        _Apiled.pile(_A);
-
-        _lApiled.pile(_lA);
-
-        _uApiled.pile(_uA);
-
-    }
-
-    if(_l.rows() > 0)
-    {
-        _Apiled.pile(I);
-        _lApiled.pile(_l);
-        _uApiled.pile(_u);
-    }
-
-    if(!(_lApiled.rows() == _uApiled.rows())){
-        XBot::Logger::error("lApiled size: %i \n", _lApiled.rows());
-        XBot::Logger::error("uApiled size: %i \n", _uApiled.rows());
-        assert(_lApiled.rows() == _uApiled.rows());
-        return false;}
-    if(!(_Apiled.rows() == _lApiled.rows())){
-        XBot::Logger::error("Apiled size: %i \n", _Apiled.rows());
-        XBot::Logger::error("lApiled size: %i \n", _lApiled.rows());
-        assert(_lApiled.rows() == _Apiled.rows());
-        return false;}
-
-    _Asp = _Apiled.generate_and_get().sparseView();
-    _Psp = _H.sparseView(); //_H.sparseView();
-
-    _Asp.makeCompressed();
-    _Acsc.reset(csc_matrix(_Asp.rows(), _Asp.cols(), _Asp.nonZeros(), _Asp.valuePtr(), _Asp.innerIndexPtr(), _Asp.outerIndexPtr()));
-
-    _Psp.makeCompressed();
-    _Pcsc.reset(csc_matrix(_Psp.rows(), _Psp.cols(), _Psp.nonZeros(),
-                           _Psp.valuePtr(),
-                           _Psp.innerIndexPtr(), _Psp.outerIndexPtr()));
-
-    toData();
-
-    _workspace.reset(osqp_setup(_data.get(), _settings.get()));
-
-    bool exitflag = osqp_solve(_workspace.get());
-
-    _solution = Eigen::Map<Eigen::VectorXd>(_workspace->solution->x, _solution.size());
-
-    if(exitflag != 0)
-        return false;
-    return true;
+    bool success = true;
+    success = updateTask(H, g) && success;
+    success = updateConstraints(A, lA, uA) && success;
+    success = updateBounds(l, u) && success;
+    
+    _workspace.reset( osqp_setup(_data.get(), _settings.get()) );
+    
+    success = solve() && success;
+    
+    return success;
 }
 
 
 void OSQPBackEnd::toData()
 {
     _data->n = _H.rows(); //number of variables
-    _data->m = _Apiled.rows(); //number of constraints + bounds
+    _data->m = _A.rows() + _data->n; //number of constraints + bounds
 
     _data->P = _Pcsc.get(); //Hessian
     _data->A = _Acsc.get(); //Constraints
 
     _data->q = _g.data(); //Gradient
-    _data->l = _lApiled.generate_and_get().data(); //Constraints lower
-    _data->u = _uApiled.generate_and_get().data(); //Constraints upper
+    _data->l = _lb_piled.data(); //Constraints lower
+    _data->u = _ub_piled.data(); //Constraints lower
 
 }
 
