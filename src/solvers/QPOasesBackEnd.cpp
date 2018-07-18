59b878ccdc33ed69f27707ae2e1028965d88931c
EnricoMingo <enricomingo@gmail.com>
2018-07-18 15:10:37 +0200

    Fix wrong eps output

--- src/solvers/QPOasesBackEnd.cpp
+++ src/solvers/QPOasesBackEnd.cpp
@@ -314,7 +314,7 @@ void QPOasesBackEnd::checkInfeasibility()
 
 void QPOasesBackEnd::_printProblemInformation()
 {
-    XBot::Logger::info("eps Regularisation factor: %d \n", _problem->getOptions().epsRegularisation);
+    XBot::Logger::info("eps Regularisation factor: %f \n", _problem->getOptions().epsRegularisation);
     XBot::Logger::info("qpOASES # OF CONSTRAINTS: %i\n", _problem->getNC());
     XBot::Logger::info("qpOASES # OF VARIABLES: %i\n", _problem->getNV());
 }
