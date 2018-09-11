9110ba6d715b2685cc66a05c06193262a31ec72c
EnricoMingo <enricomingo@gmail.com>
2018-09-11 19:50:41 +0200

    removed examples folder (still using yarp)

--- examples/KlamptController/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <example_klampt_controller.h>
-
-int main(int argc, char** argv)
-{
-    ExampleKlamptController controller;
-    KlamptController::JntCommand cmq = controller.computeControl(controller.getPosture());
-}
