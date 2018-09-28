4234b25462f2041a49ca57a20289a3ea07017b11
EnricoMingo <enricomingo@gmail.com>
2018-09-28 19:37:21 +0200

    deprecated function

--- include/OpenSoT/tasks/velocity/Gaze.h
+++ include/OpenSoT/tasks/velocity/Gaze.h
@@ -53,6 +53,7 @@ public:
      * @param desiredGaze pose of the object to observe in base_link
      */
     void setGaze(const Eigen::Affine3d& desiredGaze);
+    [[deprecated]]
     void setGaze(const Eigen::MatrixXd& desiredGaze);
     void setGaze(const KDL::Frame& desiredGaze);
 
