693bb425150b9a058f4db5bd0ccad97a575867b5
EnricoMingo <enricomingo@gmail.com>
2018-10-09 17:01:01 +0200

    Added helper class for inverse dynamics

---
+++ include/OpenSoT/utils/InverseDynamics.h
@@ -0,0 +1,76 @@
+#ifndef _OPENSOT_INVERSE_DYNAMICS_H_
+#define _OPENSOT_INVERSE_DYNAMICS_H_
+
+#include <OpenSoT/utils/Affine.h>
+#include <XBotInterface/ModelInterface.h>
+#include <OpenSoT/utils/AutoStack.h>
+
+namespace OpenSoT {
+namespace utils{
+
+/**
+ * @brief The InverseDynamics class is a utility to help the computation of wholebody inverse dynamics controllers with contacts
+ */
+class InverseDynamics{
+
+public:
+    typedef boost::shared_ptr<InverseDynamics> Ptr;
+
+    /**
+     * @brief InverseDynamics constructor
+     * @param links_in_contact a vector of string each one representing a contact
+     * @param model
+     */
+    InverseDynamics(const std::vector<std::string> links_in_contact,
+                    XBot::ModelInterface& model);
+
+    /**
+     * @brief getJointsAccelerationAffine return the Affine related to the joint accelerations
+     * @return _qddot
+     */
+    const AffineHelper& getJointsAccelerationAffine() const;
+
+    /**
+     * @brief getContactsWrenchAffine return a vector of Affines each related to a contact wrench
+     * @return _contacts_wrench
+     */
+    const std::vector<AffineHelper>& getContactsWrenchAffine() const;
+
+    /**
+     * @brief getSerializer return a pointer to the object which contains all the variables of the Inverse Dynamics
+     * @return
+     */
+    const boost::shared_ptr<OpenSoT::OptvarHelper> getSerializer() const;
+
+    /**
+     * @brief computedTorque given the solution from the solver which contains all the optimization variables, returns the torque
+     * to send to the robot
+     * @param x optimized variables
+     * @param tau computed torques
+     */
+    bool computedTorque(const Eigen::VectorXd& x, Eigen::VectorXd& tau);
+
+    /**
+     * @brief log internal variables: wrenches, tau and qddot
+     * @param logger
+     */
+    void log(XBot::MatLogger::Ptr& logger);
+
+private:
+    AffineHelper    _qddot;
+    std::vector<AffineHelper> _contacts_wrench;
+    std::vector<std::string> _links_in_contact;
+    XBot::ModelInterface& _model;
+    boost::shared_ptr<OpenSoT::OptvarHelper> _serializer;
+
+    Eigen::VectorXd _qddot_val;
+    std::vector<Eigen::Vector6d> _contacts_wrench_val;
+    std::vector<Eigen::MatrixXd> _Jc;
+    Eigen::VectorXd _tau_val;
+
+};
+
+}
+}
+
+#endif
