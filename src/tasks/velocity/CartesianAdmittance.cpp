8ad2035fd92da06e20f479bbd3d90deddc0dea0a
EnricoMingo <enricomingo@gmail.com>
2019-01-09 11:20:45 +0100

    fix

--- src/tasks/velocity/CartesianAdmittance.cpp
+++ src/tasks/velocity/CartesianAdmittance.cpp
@@ -37,7 +37,7 @@ CartesianAdmittance::CartesianAdmittance(std::string task_id,
 
     _lambda = 0.01; //This was found by experiments
 
-    _tmp.reserve(CHANNELS);
+    _tmp.assign(0., CHANNELS);
 }
 
 void CartesianAdmittance::_update(const Eigen::VectorXd &x)
