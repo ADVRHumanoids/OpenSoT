e9255fe1b62b84a906ef48e17c2fc4c5b1c96df3
Arturo Laurenzi <arturo.laurenzi@iit.it>
2019-04-20 17:21:44 +0200

    Added a static constraint that enforces tau = J'F

---
+++ src/constraints/force/StaticConstraint.cpp
@@ -0,0 +1,34 @@
+#include <OpenSoT/constraints/force/StaticConstraint.h>
+
+
+OpenSoT::constraints::force::StaticConstraint::StaticConstraint(const XBot::ModelInterface& robot,
+                                                              std::vector<std::string> contact_links,
+                                                              std::vector<AffineHelper> forces,
+                                                              OpenSoT::AffineHelper robot_torque):
+    Constraint("StaticConstraint", forces[0].getInputSize()),
+    _robot(robot),
+    _contact_links(contact_links),
+    _forces(forces),
+    _robot_torque(robot_torque)
+{
+    update(Eigen::VectorXd());
+}
+
+void OpenSoT::constraints::force::StaticConstraint::update(const Eigen::VectorXd& x)
+{
+    _constr.setZero(getXSize(), _robot.getActuatedJointNum());
+    
+    for(int i = 0; i < _contact_links.size(); i++)
+    {
+        _robot.getJacobian(_contact_links.at(i), _J);
+        _constr = _constr + _J.transpose().bottomRows(_robot.getActuatedJointNum()) * _forces[i];
+    }
+    
+    _constr = _constr + _robot_torque;
+    
+    _robot.computeGravityCompensation(_gcomp);
+    
+    _Aeq = _constr.getM();
+    _beq = _gcomp.tail(_robot.getActuatedJointNum()) - _constr.getq();
+    
+}
