ac9afcc5f22af83968a703674e8450da681b5057
EnricoMingo <enricomingo@gmail.com>
2021-03-30 18:09:16 +0200

    Added AffineConstraint (needs to be tested)

--- include/OpenSoT/utils/AffineUtils.h
+++ include/OpenSoT/utils/AffineUtils.h
@@ -2,8 +2,10 @@
 #define _AFFINE_UTILS_H_
 
 #include <OpenSoT/tasks/Aggregated.h>
+#include <OpenSoT/constraints/Aggregated.h>
 #include <OpenSoT/utils/Affine.h>
 #include <OpenSoT/tasks/GenericTask.h>
+#include <OpenSoT/constraints/GenericConstraint.h>
 
 
 namespace OpenSoT
@@ -31,6 +33,30 @@ private:
     OpenSoT::tasks::GenericTask::Ptr _internal_generic_task;
     OpenSoT::tasks::Aggregated::TaskPtr _internal_task;
 
+};
+
+class AffineConstraint: public Constraint<Eigen::MatrixXd, Eigen::VectorXd>
+{
+public:
+    typedef boost::shared_ptr<AffineConstraint> Ptr;
+
+    AffineConstraint(const OpenSoT::constraints::Aggregated::ConstraintPtr& constraint,
+               const AffineHelper& var);
+
+    virtual void update(const Eigen::VectorXd& x);
+    ~AffineConstraint();
+
+    static AffineConstraint::Ptr toAffine(const OpenSoT::constraints::Aggregated::ConstraintPtr& constraint,
+                                          const AffineHelper& var);
+
+    OpenSoT::constraints::Aggregated::ConstraintPtr& getConstraint(){ return _internal_constraint; }
+
+private:
+    OpenSoT::constraints::GenericConstraint::Ptr _internal_generic_constraint;
+    OpenSoT::constraints::Aggregated::ConstraintPtr _internal_constraint;
+
+    AffineHelper _constraint_affine; //this is used to map the constraint into an affine
+    AffineHelper _var;
 };
 
 
