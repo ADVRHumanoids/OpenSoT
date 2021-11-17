967c2ea9b3eadc2837d038af98ed83502e0b78b1
Arturo Laurenzi <arturo.laurenzi@iit.it>
2021-11-17 19:25:49 +0100

    ported last test

--- tests/solvers/TestQPOases_FF.cpp
+++ tests/solvers/TestQPOases_FF.cpp
@@ -563,10 +563,9 @@ TEST_P(testQPOases_CoMAndPosturalFF, testCoMFF)
 {
     bool hasInitialError = GetParam();
 
-    std::string robotology_root = std::getenv("ROBOTOLOGY_ROOT");
-    std::string relative_path = "/external/OpenSoT/tests/configs/coman/configs/config_coman_floating_base.yaml";
+    std::string relative_path = OPENSOT_TEST_PATH "configs/coman/configs/config_coman_floating_base.yaml";
 
-    std::string _path_to_cfg = robotology_root + relative_path;
+    std::string _path_to_cfg = relative_path;
 
     XBot::ModelInterface::Ptr _model_ptr = XBot::ModelInterface::getModel(_path_to_cfg);
 
@@ -830,10 +829,9 @@ TEST_P(testQPOases_CoMAndPosturalFF, testPosturalFF)
 {
     bool hasInitialError = GetParam();
 
-    std::string robotology_root = std::getenv("ROBOTOLOGY_ROOT");
-    std::string relative_path = "/external/OpenSoT/tests/configs/coman/configs/config_coman_RBDL.yaml";
+    std::string relative_path = OPENSOT_TEST_PATH "configs/coman/configs/config_coman_RBDL.yaml";
 
-    std::string _path_to_cfg = robotology_root + relative_path;
+    std::string _path_to_cfg = relative_path;
 
     XBot::ModelInterface::Ptr _model_ptr = XBot::ModelInterface::getModel(_path_to_cfg);
 
