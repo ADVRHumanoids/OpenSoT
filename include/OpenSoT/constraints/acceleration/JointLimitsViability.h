3cbb76995aaea599e03f4a3fc72358f8d09437a0
EnricoMingo <enricomingo@gmail.com>
2023-04-03 14:54:34 +0200

    first implementation, needs to be tested

---
+++ include/OpenSoT/constraints/acceleration/JointLimitsViability.h
@@ -0,0 +1,104 @@
+/*
+ * Copyright (C) Unmanned Systems and Robotics Lab
+ * Author: Andrea Testa
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU Lesser General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+#ifndef __BOUNDS_ACCELERATION_JOINTLIMITS_VIABILITY_AFFINE_H__
+#define __BOUNDS_ACCELERATION_JOINTLIMITS_VIABILITY_AFFINE_H__
+
+#include <OpenSoT/Constraint.h>
+#include <Eigen/Dense>
+#include <OpenSoT/utils/Affine.h>
+#include <OpenSoT/constraints/GenericConstraint.h>
+
+ namespace OpenSoT {
+    namespace constraints {
+        namespace acceleration{
+            /**
+             * @brief The JointLimits class implements bounds on joints positions using internally the
+             * GenericConstraint and affine variables. It is based on the paper:
+             *  "Joint position and velocity bounds in discrete-time acceleration/torque control of robot manipulators", by: Andrea Del Prete
+             */
+            class JointLimitsViability: public Constraint<Eigen::MatrixXd, Eigen::VectorXd> {
+            public:
+                typedef std::shared_ptr<JointLimitsViability> Ptr;
+            private:
+
+                Eigen::VectorXd _jointLimitsMin;
+                Eigen::VectorXd _jointLimitsMax;
+
+                Eigen::VectorXd __upperBound;
+                Eigen::VectorXd __lowerBound;
+
+
+                Eigen::VectorXd _jointAccMax, _jointVelMax;
+
+                XBot::ModelInterface& _robot;
+
+                Eigen::VectorXd _q, _qdot;
+
+                GenericConstraint::Ptr _generic_constraint_internal;
+
+
+                /**
+                 * @brief _dt
+                 **/
+                double _dt;
+
+                Eigen::VectorXd _ddq_LB_pos, _ddq_UB_pos, _ddq_UBLB_pos;
+                Eigen::VectorXd _ddq_M1, _ddq_M2, _ddq_M3, _ddq_m2, _ddq_m3;
+                void accBoundsFromPosLimits();
+                double _a;
+                Eigen::VectorXd _ddq_LB_via, _ddq_UB_via, _ddq_UBLB_via;
+                Eigen::VectorXd _b_1, _c_1, _ddq_1, _delta_1, _b_2, _c_2, _delta_2;
+                void accBoundsFromViability();
+                Eigen::VectorXd _ddq_LB_vel, _ddq_UB_vel, _ddq_UBLB_vel, _ddq_UBLB;
+                void computeJointAccBounds();
+
+
+
+
+            public:
+                /**
+                 * @brief JointLimitsViability
+                 * @param robot
+                 * @param qddot
+                 * @param jointBoundMax
+                 * @param jointBoundMin
+                 * @param jointAccMax
+                 * @param dt
+                 */
+                JointLimitsViability(XBot::ModelInterface& robot,
+                            const AffineHelper& qddot,
+                            const Eigen::VectorXd &jointBoundMax,
+                            const Eigen::VectorXd &jointBoundMin,
+                            const Eigen::VectorXd &jointVelMax,
+                            const Eigen::VectorXd &jointAccMax,
+                            const double dt);
+
+
+                void update(const Eigen::VectorXd& x);
+
+                void setJointAccMax(const Eigen::VectorXd& jointAccMax);
+                void setJointVelMax(const Eigen::VectorXd& jointVelMax);
+
+
+
+            };
+           }
+        }
+
+ }
+
+#endif
---
+++ src/constraints/acceleration/JointLimitsViability.cpp
@@ -0,0 +1,207 @@
+/*
+ * Copyright (C) Unmanned Systems and Robotics Lab
+ * Author: Andrea Testa
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU Lesser General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+#include <OpenSoT/constraints/acceleration/JointLimitsViability.h>
+#include <memory>
+
+using namespace OpenSoT::constraints::acceleration;
+
+JointLimitsViability::JointLimitsViability(XBot::ModelInterface& robot,
+                                            const AffineHelper& qddot,
+                                            const Eigen::VectorXd &jointBoundMax,
+                                            const Eigen::VectorXd &jointBoundMin,
+                                            const Eigen::VectorXd &jointVelMax,
+                                            const Eigen::VectorXd &jointAccMax,
+                                            const double dt):
+     Constraint("joint_limits_viability", qddot.getInputSize()),
+     _jointLimitsMax(jointBoundMax),
+     _jointLimitsMin(jointBoundMin),
+     _jointVelMax(jointVelMax),
+     _jointAccMax(jointAccMax),
+     _robot(robot),
+     _dt(dt)
+{
+    if(qddot.getOutputSize() != _jointLimitsMax.size())
+        throw std::runtime_error("_qddot.getOutputSize() != _jointLimitsMax.size()");
+    if(qddot.getOutputSize() != _jointLimitsMin.size())
+        throw std::runtime_error("_qddot.getOutputSize() != _jointLimitsMin.size()");
+    if(_jointAccMax.size() != _jointLimitsMin.size())
+        throw std::runtime_error("_jointAccMax.size() != _jointLimitsMin.size()");
+    if(_jointVelMax.size() != _jointLimitsMin.size())
+        throw std::runtime_error("_jointVelMax.size() != _jointLimitsMin.size()");
+    /* calling update to generate bounds */
+
+    _ddq_LB_pos.setZero(jointBoundMax.size());
+    _ddq_UB_pos.setZero(jointBoundMax.size());
+    _ddq_LB_via.setZero(jointBoundMax.size());
+    _ddq_UB_via.setZero(jointBoundMax.size());
+    _ddq_LB_vel.setZero(jointBoundMax.size());
+    _ddq_UB_vel.setZero(jointBoundMax.size());
+    _ddq_UBLB_pos.setZero(2 * jointBoundMax.size());
+    _ddq_UBLB_via.setZero(2 * jointBoundMax.size());
+    _ddq_UBLB_vel.setZero(2 * jointBoundMax.size());
+    _ddq_UBLB.setZero(2 * jointBoundMax.size());
+    _a = _dt*_dt;
+
+
+    _generic_constraint_internal = std::make_shared<OpenSoT::constraints::GenericConstraint>(
+                "internal_generic_constraint", qddot,
+                _jointAccMax,
+                -_jointAccMax,
+                OpenSoT::constraints::GenericConstraint::Type::CONSTRAINT);
+
+    update(Eigen::VectorXd(1));
+}
+
+void JointLimitsViability::update(const Eigen::VectorXd &x)
+{
+    _robot.getJointPosition(_q);
+    _robot.getJointVelocity(_qdot);
+
+    __upperBound =  _jointAccMax;
+    __lowerBound = -_jointAccMax;
+
+    accBoundsFromPosLimits();
+    accBoundsFromViability();
+    computeJointAccBounds();
+
+    __upperBound = _ddq_UBLB.segment(0, _jointLimitsMax.size());
+    __lowerBound = _ddq_UBLB.segment(_jointLimitsMax.size()-1, _jointLimitsMax.size());
+
+    _generic_constraint_internal->setBounds(__upperBound, __lowerBound);
+
+    _generic_constraint_internal->update(x);
+
+    _Aineq = _generic_constraint_internal->getAineq();
+    _bLowerBound = _generic_constraint_internal->getbLowerBound();
+    _bUpperBound = _generic_constraint_internal->getbUpperBound();
+}
+
+void JointLimitsViability::computeJointAccBounds()
+{
+    _ddq_UB_vel = (_jointVelMax - _qdot)/_dt;
+    _ddq_LB_vel = (-_jointVelMax - _qdot)/_dt;
+
+    _ddq_UBLB_vel.segment(0, _ddq_UB_vel.size()) = _ddq_UB_vel;
+    _ddq_UBLB_vel.segment(_ddq_UB_vel.size()-1, _ddq_LB_vel.size()) = _ddq_LB_vel;
+
+    for(unsigned int it = 0; it < _jointLimitsMax.size(); ++it)
+    {
+        _ddq_UBLB[it] = std::min(std::min(std::min(_ddq_UBLB_pos[it], _ddq_UBLB_vel[it]), _ddq_UBLB_via[it]), _jointAccMax[it]);
+        if(_ddq_UBLB[it] < -_jointAccMax[it])\
+        {
+            _ddq_UBLB[it] = -_jointAccMax[it];
+        }
+    }
+
+    for(unsigned int it = _jointLimitsMax.size()-1; it < _jointLimitsMax.size(); ++it)
+    {
+        _ddq_UBLB[it] = std::max(std::max(std::max(_ddq_UBLB_pos[it], _ddq_UBLB_vel[it]), _ddq_UBLB_via[it]), -_jointAccMax[it-_jointLimitsMax.size()]);
+        if(_ddq_UBLB[it] > _jointLimitsMax[it-_jointLimitsMax.size()])
+        {
+            _ddq_UBLB[it] = _jointLimitsMax[it-_jointLimitsMax.size()];
+         }
+    }
+}
+
+void JointLimitsViability::accBoundsFromPosLimits()
+{
+    _ddq_M1 = -_qdot/_dt;
+    _ddq_M2 = -(_qdot.array() * _qdot.array()) / (2.0*(_jointLimitsMax.array() - _q.array()));
+    _ddq_M3 = 2.0*(_jointLimitsMax - _q - _dt*_qdot)/(_dt*_dt);
+    _ddq_m2 = (_qdot.array()*_qdot.array())/(2.0*(_q.array() - _jointLimitsMin.array()));
+    _ddq_m3 = 2.0*(_jointLimitsMin - _q - _dt*_qdot)/(_dt*_dt);
+
+    for(unsigned int it = 0; it < _jointLimitsMax.size(); ++it)
+    {
+        if(_qdot[it] > 0)
+        {
+            _ddq_LB_pos[it] = _ddq_m3[it];
+            if(_ddq_M3[it] > _ddq_M1[it])
+            {
+                _ddq_UB_pos[it] = _ddq_M3[it];
+            }
+            else
+            {
+                _ddq_UB_pos[it] = std::min(_ddq_M1[it], _ddq_M2[it]);
+            }
+        }
+        else
+        {
+            _ddq_UB_pos[it] = _ddq_M3[it];
+            if(_ddq_m3[it] < _ddq_M1[it])
+            {
+                _ddq_LB_pos[it] = _ddq_m3[it];
+            }
+            else
+            {
+                _ddq_LB_pos[it] = std::max(_ddq_M1[it], _ddq_m2[it]);
+            }
+        }
+    }
+
+    _ddq_UBLB_pos.segment(0, _ddq_UB_pos.size()) = _ddq_UB_pos;
+    _ddq_UBLB_pos.segment(_ddq_UB_pos.size()-1, _ddq_LB_pos.size()) = _ddq_LB_pos;
+}
+
+void JointLimitsViability::accBoundsFromViability()
+{
+    _b_1 = _dt*(2.0*_qdot - (-_jointAccMax)*_dt);
+    _c_1 = _qdot.array()*_qdot.array() + 2.0*(-_jointAccMax).array()*(_jointLimitsMax.array() - _q.array() - _dt*_qdot.array());
+    _ddq_1 = -_qdot/_dt;
+    _delta_1 = _b_1.array()*_b_1.array() - 4.0*_a*_c_1.array();
+
+    for(unsigned int it = 0; it < _jointLimitsMax.size(); ++it)
+    {
+        if(_delta_1[it] >= 0)
+        {
+            _ddq_UB_via[it] = std::max(_ddq_1[it], ( -_b_1[it] + std::sqrt(_delta_1[it]) )/(2.0*_a) );
+        }
+        else
+        {
+            _ddq_UB_via[it] = _ddq_1[it];
+        }
+    }
+
+    _b_2 = _dt*(2.0*_qdot - _jointAccMax*_dt);
+    _c_2 = _qdot.array()*_qdot.array() - 2.0*_jointAccMax.array()*(_q.array() + _dt*_qdot.array() - _jointLimitsMin.array());
+    _delta_2 = _b_2.array()*_b_2.array() - 4.0*_a*_c_2.array();
+
+    for(unsigned int it = 0; it < _jointLimitsMax.size(); ++it)
+    {
+        if(_delta_2[it] >= 0)
+        {
+            _ddq_LB_via[it] = std::min(_ddq_1[it], ( -_b_2[it] - std::sqrt(_delta_2[it]) )/(2.0*_a) );
+        }
+        else
+        {
+            _ddq_LB_via[it] = _ddq_1[it];
+        }
+    }
+
+    _ddq_UBLB_via.segment(0, _ddq_UB_via.size()) = _ddq_UB_via;
+    _ddq_UBLB_via.segment(_ddq_UB_via.size()-1, _ddq_LB_via.size()) = _ddq_LB_via;
+}
+
+void JointLimitsViability::setJointAccMax(const Eigen::VectorXd &jointAccMax)
+{
+    _jointAccMax = jointAccMax;
+}
+
+void JointLimitsViability::setJointVelMax(const Eigen::VectorXd& jointVelMax)
+{
+    _jointVelMax = jointVelMax;
+}
