c8da1d7e90c9f91617b33815c90da26f61dce1ab
EnricoMingo <enricomingo@gmail.com>
2023-04-17 21:07:24 +0200

    Added Joint Limits implementation on Exponential Control Barrier Function

---
+++ include/OpenSoT/constraints/acceleration/JointLimitsECBF.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2023 Unmanned Systems and Robotics Lab
+ * Author: Enrico Mingo Hoffman
+ * email:  enricomingo@gmail.com
+ * Permission is granted to copy, distribute, and/or modify this program
+ * under the terms of the GNU Lesser General Public License, version 2 or any
+ * later version published by the Free Software Foundation.
+ *
+ * A copy of the license can be found at
+ * https://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details
+*/
+
+#ifndef __BOUNDS_ACCELERATION_JOINTLIMITS_ECBF_H__
+#define __BOUNDS_ACCELERATION_JOINTLIMITS_ECBF_H__
+
+#include <OpenSoT/Constraint.h>
+#include <Eigen/Dense>
+#include <OpenSoT/utils/Affine.h>
+#include <OpenSoT/constraints/GenericConstraint.h>
+
+namespace OpenSoT {
+   namespace constraints {
+       namespace acceleration{
+            /**
+             * @brief The JointLimitsECBF class implements Joint Limits using Exponential Control Barrier Functions (ECBF)
+             * as in the paper "Humanoid Self-Collision Avoidance Using Whole-Body Control with Control Barrier Functions",
+             * by Charles Khazoom, Daniel Gonzalez-Diaz, Yanran Ding, and Sangbae Kim
+             */
+            class JointLimitsECBF: public Constraint<Eigen::MatrixXd, Eigen::VectorXd> {
+            public:
+                typedef std::shared_ptr<JointLimitsECBF> Ptr;
+            private:
+
+                Eigen::VectorXd _jointLimitsMin;
+                Eigen::VectorXd _jointLimitsMax;
+
+                Eigen::VectorXd __upperBound;
+                Eigen::VectorXd __lowerBound;
+                Eigen::VectorXd _ll, _uu;
+
+                XBot::ModelInterface& _robot;
+
+                Eigen::VectorXd _q, _qdot;
+
+                GenericConstraint::Ptr _generic_constraint_internal;
+
+                Eigen::VectorXd _a1, _a2;
+
+                Eigen::VectorXd _ones;
+
+            public:
+                JointLimitsECBF(XBot::ModelInterface& robot,
+                            const AffineHelper& qddot,
+                            const Eigen::VectorXd &jointBoundMax,
+                            const Eigen::VectorXd &jointBoundMin);
+
+                void update(const Eigen::VectorXd& x);
+
+                void setAlpha1(const Eigen::VectorXd& a1);
+                void setAlpha1(const double a1);
+                void setAlpha2(const Eigen::VectorXd& a2);
+                void setAlpha2(const double a2);
+
+
+            };
+       }
+   }
+}
+#endif
---
+++ src/constraints/acceleration/JointLimitsECBF.cpp
@@ -0,0 +1,77 @@
+#include <OpenSoT/constraints/acceleration/JointLimitsECBF.h>
+
+using namespace OpenSoT::constraints::acceleration;
+
+JointLimitsECBF::JointLimitsECBF(XBot::ModelInterface &robot,
+                                 const AffineHelper &qddot,
+                                 const Eigen::VectorXd &jointBoundMax,
+                                 const Eigen::VectorXd &jointBoundMin):
+    Constraint("joint_limits_ecbf", qddot.getInputSize()),
+    _jointLimitsMax(jointBoundMax),
+    _jointLimitsMin(jointBoundMin),
+    _robot(robot)
+{
+    _generic_constraint_internal = std::make_shared<OpenSoT::constraints::GenericConstraint>(
+                "internal_generic_constraint", qddot,
+                Eigen::VectorXd::Zero(jointBoundMin.size()),
+                Eigen::VectorXd::Zero(jointBoundMax.size()),
+                OpenSoT::constraints::GenericConstraint::Type::CONSTRAINT);
+
+
+    _a1.setOnes(_jointLimitsMax.size());
+    _a2.setOnes(_jointLimitsMax.size());
+    _ones.setOnes(_jointLimitsMax.size());
+
+    update(Eigen::VectorXd(1));
+}
+
+void JointLimitsECBF::update(const Eigen::VectorXd &x)
+{
+    _robot.getJointPosition(_q);
+    _robot.getJointVelocity(_qdot);
+
+    __upperBound = -(_a1 + _a2).array() * _qdot.array() -(_a1.array() * _a2.array())*(_q.array() - _jointLimitsMin.array());
+    __lowerBound = -(_a1 + _a2).array() * _qdot.array() +(_a1.array() * _a2.array())*(_jointLimitsMax.array() - _q.array());
+
+
+    for(unsigned int i = 0; i < _jointLimitsMax.size(); ++i)
+    {
+        if(__upperBound[i] < __lowerBound[i])
+        {
+            double ub = __upperBound[i];
+            __upperBound[i] = __lowerBound[i];
+            __lowerBound[i] = ub;
+        }
+    }
+
+
+    _generic_constraint_internal->setBounds(__upperBound, __lowerBound);
+
+    _generic_constraint_internal->update(x);
+
+    _Aineq = _generic_constraint_internal->getAineq();
+    _bLowerBound = _generic_constraint_internal->getbLowerBound();
+    _bUpperBound = _generic_constraint_internal->getbUpperBound();
+}
+
+void JointLimitsECBF::setAlpha1(const Eigen::VectorXd &a1)
+{
+    _a1 = a1;
+}
+
+void JointLimitsECBF::setAlpha1(const double a1)
+{
+    _a1 = a1 * _ones;
+}
+
+void JointLimitsECBF::setAlpha2(const Eigen::VectorXd &a2)
+{
+    _a2 = a2;
+}
+
+void JointLimitsECBF::setAlpha2(const double a2)
+{
+    _a2 = a2 * _ones;
+}
+
+
