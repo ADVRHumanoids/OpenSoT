6edc41302eeb6bac6932b64eca96bef84cf4ad18
EnricoMingo <enricomingo@gmail.com>
2023-07-15 12:01:09 +0200

    model yaml file is now retrieved using CMakeLists variable

--- tests/solvers/TestEigenSVD_StaticWalk_FloatingBase.cpp
+++ tests/solvers/TestEigenSVD_StaticWalk_FloatingBase.cpp
@@ -21,9 +21,8 @@
 #include <XBotInterface/Logger.hpp>
 #include <matlogger2/matlogger2.h>
 
-std::string robotology_root = std::getenv("ROBOTOLOGY_ROOT");
-std::string relative_path = "/external/OpenSoT/tests/configs/coman/configs/config_coman_floating_base.yaml";
-std::string _path_to_cfg = robotology_root + relative_path;
+std::string relative_path = OPENSOT_TEST_PATH "configs/coman/configs/config_coman_floating_base.yaml";
+std::string _path_to_cfg = relative_path;
 
 bool IS_ROSCORE_RUNNING;
 
