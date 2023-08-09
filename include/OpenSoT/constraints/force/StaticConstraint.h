0f4e95cc00538c828007e413af7750179a47734c
EnricoMingo <enricomingo@gmail.com>
2023-08-09 14:06:25 +0200

    updated documentation

--- include/OpenSoT/constraints/force/StaticConstraint.h
+++ include/OpenSoT/constraints/force/StaticConstraint.h
@@ -5,11 +5,24 @@
 
 namespace OpenSoT { namespace constraints { namespace force {
     
+   /**
+     * @brief The StaticConstraint class computes the static (only gravity is taken into account) distribution of
+     * contact wrenches [6 x 1] given a list of links in contact:
+     *
+     *      \boldsymbol{\tau} + \mathbf{J}_c^T\mathbf{F} = \mathbf{g}
+     *
+     */
     class StaticConstraint : public Constraint<Eigen::MatrixXd, Eigen::VectorXd> 
     {
         
     public:
-        
+        /**
+         * @brief StaticConstraint constructor
+         * @param robot model
+         * @param contact_links list of link frames in contact
+         * @param forces variables
+         * @param robot_torque variables
+         */
         StaticConstraint(const XBot::ModelInterface& robot, 
                         std::vector<std::string> contact_links,
                         std::vector<AffineHelper> forces,
