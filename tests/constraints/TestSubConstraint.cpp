0328c67047ece0af04de07162b5f3cdf0f224f08
EnricoMingo <enricomingo@gmail.com>
2024-02-14 14:54:36 +0100

    Ported TestSubCOnstraint

--- tests/constraints/TestSubConstraint.cpp
+++ tests/constraints/TestSubConstraint.cpp
@@ -4,33 +4,24 @@
 #include <OpenSoT/constraints/velocity/CartesianVelocity.h>
 #include <xbot2_interface/xbotinterface2.h>
 #include <OpenSoT/utils/AutoStack.h>
+#include "../common.h"
 
-std::string relative_path = OPENSOT_TEST_PATH "configs/coman/configs/config_coman_RBDL.yaml";
-std::string _path_to_cfg = relative_path;
 #define toRad(X) (X * M_PI/180.0)
 
 using namespace OpenSoT::constraints;
 
 namespace{
-class TestSubConstraint: public ::testing::Test
+class TestSubConstraint: public TestBase
 {
 protected:
 
     OpenSoT::constraints::velocity::JointLimits::Ptr _joint_limits;
     OpenSoT::constraints::velocity::CartesianVelocity::Ptr _vcom_constraint;
 
-    TestSubConstraint()
+    TestSubConstraint() : TestBase("coman_floating_base")
 
     {
-        _model_ptr = XBot::ModelInterface::getModel(_path_to_cfg);
-
-        if(_model_ptr)
-            std::cout<<"pointer address: "<<_model_ptr.get()<<std::endl;
-        else
-            std::cout<<"pointer is NULL "<<_model_ptr.get()<<std::endl;
-
-        q.resize(_model_ptr->getJointNum());
-        q.setZero();
+        q = _model_ptr->getNeutralQ();
         q[_model_ptr->getDofIndex("LHipSag")] = toRad(-23.5);
         q[_model_ptr->getDofIndex("LHipLat")] = toRad(2.0);
         q[_model_ptr->getDofIndex("LHipYaw")] = toRad(-4.0);
@@ -46,14 +37,14 @@ protected:
         q[_model_ptr->getDofIndex("RAnkSag")] = toRad(-26.6);
 
 
-        Eigen::VectorXd qmin(_model_ptr->getJointNum()), qmax(_model_ptr->getJointNum());
+        Eigen::VectorXd qmin, qmax;
         _model_ptr->getJointLimits(qmin, qmax);
-        _joint_limits = std::make_shared<velocity::JointLimits>(q, qmax, qmin);
+        _joint_limits = std::make_shared<velocity::JointLimits>(*_model_ptr, qmax, qmin);
 
         Eigen::Vector3d vcom_max;
         vcom_max<<10., 20., 30.;
         _vcom_constraint = std::make_shared<velocity::CartesianVelocity>(vcom_max, 0.01,
-                                                                         std::make_shared<OpenSoT::tasks::velocity::CoM>(q, *_model_ptr));
+                                                                         std::make_shared<OpenSoT::tasks::velocity::CoM>(*_model_ptr));
 
     }
 
@@ -68,7 +59,6 @@ protected:
 
     }
 
-    XBot::ModelInterface::Ptr _model_ptr;
     Eigen::VectorXd q;
 
 };
@@ -76,7 +66,7 @@ protected:
 TEST_F(TestSubConstraint, testSubBounds)
 {
     std::list<unsigned int> indices;
-    for(unsigned int i = 6; i < this->_model_ptr->getJointNum(); ++i)
+    for(unsigned int i = 6; i < this->_model_ptr->getNv(); ++i)
         indices.push_back(i);
 
     OpenSoT::SubConstraint::Ptr sub_postural = this->_joint_limits%indices;
@@ -89,7 +79,7 @@ TEST_F(TestSubConstraint, testSubBounds)
     std::cout<<"upper bound[:]: "<<this->_joint_limits->getUpperBound().transpose()<<std::endl;
     std::cout<<"upper bound[6:end]: "<<sub_postural->getbUpperBound().transpose()<<std::endl;
 
-    std::cout<<"Aineq: "<<sub_postural->getAineq()<<std::endl;
+    std::cout<<"Aineq: \n"<<sub_postural->getAineq()<<std::endl;
 
     EXPECT_EQ(sub_postural->getbLowerBound().size(), indices.size());
     EXPECT_EQ(sub_postural->getbUpperBound().size(), indices.size());
@@ -99,16 +89,16 @@ TEST_F(TestSubConstraint, testSubBounds)
     EXPECT_EQ(sub_postural->getAineq().cols(), this->_joint_limits->getLowerBound().size());
     EXPECT_EQ(sub_postural->getAineq().rightCols(indices.size()), Eigen::MatrixXd(indices.size(), indices.size()).setIdentity());
 
-    this->q.setRandom();
+    this->q = _model_ptr->generateRandomQ();
 
-    sub_postural->update(this->q);
+    sub_postural->update(Eigen::VectorXd(0));
     std::cout<<"lower bound[:]: "<<this->_joint_limits->getLowerBound().transpose()<<std::endl;
     std::cout<<"lower bound[6:end]: "<<sub_postural->getbLowerBound().transpose()<<std::endl;
 
     std::cout<<"upper bound[:]: "<<this->_joint_limits->getUpperBound().transpose()<<std::endl;
     std::cout<<"upper bound[6:end]: "<<sub_postural->getbUpperBound().transpose()<<std::endl;
 
-    std::cout<<"Aineq: "<<sub_postural->getAineq()<<std::endl;
+    std::cout<<"Aineq: \n"<<sub_postural->getAineq()<<std::endl;
 
     EXPECT_EQ(sub_postural->getbLowerBound().size(), indices.size());
     EXPECT_EQ(sub_postural->getbUpperBound().size(), indices.size());
@@ -125,7 +115,7 @@ TEST_F(TestSubConstraint, testSubInequalityConstraint)
 
     OpenSoT::SubConstraint::Ptr sub_vcom = this->_vcom_constraint%indices;
 
-    sub_vcom->update(this->q);
+    sub_vcom->update(Eigen::VectorXd(0));
 
     std::cout<<"lower bound[:]: "<<this->_vcom_constraint->getbLowerBound().transpose()<<std::endl;
     std::cout<<"lower bound[0:1]: "<<sub_vcom->getbLowerBound().transpose()<<std::endl;
@@ -139,15 +129,15 @@ TEST_F(TestSubConstraint, testSubInequalityConstraint)
     EXPECT_EQ(sub_vcom->getbLowerBound().size(), indices.size());
     EXPECT_EQ(sub_vcom->getbUpperBound().size(), indices.size());
     EXPECT_EQ(sub_vcom->getAineq().rows(), indices.size());
-    EXPECT_EQ(sub_vcom->getAineq().cols(), this->_model_ptr->getJointNum());
+    EXPECT_EQ(sub_vcom->getAineq().cols(), this->_model_ptr->getNv());
 
     EXPECT_EQ(sub_vcom->getbLowerBound(), this->_vcom_constraint->getbLowerBound().segment(0, indices.size()));
     EXPECT_EQ(sub_vcom->getbUpperBound(), this->_vcom_constraint->getbUpperBound().segment(0, indices.size()));
     EXPECT_EQ(sub_vcom->getAineq(), this->_vcom_constraint->getAineq().block(0,0,2,2));
 
-    this->q.setRandom();
+    this->q = _model_ptr->generateRandomQ();
 
-    sub_vcom->update(this->q);
+    sub_vcom->update(Eigen::VectorXd(0));
     std::cout<<"update q..."<<std::endl;
 
     std::cout<<"lower bound[:]: "<<this->_vcom_constraint->getbLowerBound().transpose()<<std::endl;
@@ -162,7 +152,7 @@ TEST_F(TestSubConstraint, testSubInequalityConstraint)
     EXPECT_EQ(sub_vcom->getbLowerBound().size(), indices.size());
     EXPECT_EQ(sub_vcom->getbUpperBound().size(), indices.size());
     EXPECT_EQ(sub_vcom->getAineq().rows(), indices.size());
-    EXPECT_EQ(sub_vcom->getAineq().cols(), this->_model_ptr->getJointNum());
+    EXPECT_EQ(sub_vcom->getAineq().cols(), this->_model_ptr->getNv());
 
     EXPECT_EQ(sub_vcom->getbLowerBound(), this->_vcom_constraint->getbLowerBound().segment(0, indices.size()));
     EXPECT_EQ(sub_vcom->getbUpperBound(), this->_vcom_constraint->getbUpperBound().segment(0, indices.size()));
