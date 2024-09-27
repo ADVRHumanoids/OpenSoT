8438d18ab5d10054fdee25a8fb9fb7f0f6457aa0
EnricoMingo <enricomingo@gmail.com>
2024-09-27 14:35:47 +0200

    fix

--- src/constraints/velocity/OmniWheels4X.cpp
+++ src/constraints/velocity/OmniWheels4X.cpp
@@ -69,8 +69,9 @@ OmniWheels4X::OmniWheels4X(const double l1, const double l2, const double r,
 
 void OmniWheels4X::update()
 {
-    _robot.getPose(_base_link, _w_T_b);
-    _Aineq.rightCols(_x_size-6).noalias() = _w_T_b.linear() * _J.rightCols(_x_size-6);
+    // _robot.getPose(_base_link, _w_T_b);
+    // _Aineq.rightCols(_x_size-6).noalias() = _w_T_b.linear() * _J.rightCols(_x_size-6);
+    _Aineq.rightCols(_x_size-6).noalias() = _J.rightCols(_x_size-6);
 }
 
 
