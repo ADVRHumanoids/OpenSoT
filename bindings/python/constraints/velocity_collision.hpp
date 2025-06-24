ea1df1884a37957ef57ee2b65cb5f286100672c1
EnricoMingo <enricomingo@gmail.com>
2025-06-24 17:15:27 +0200

    Fix merge

--- bindings/python/constraints/velocity_collision.hpp
+++ bindings/python/constraints/velocity_collision.hpp
@@ -7,10 +7,43 @@
 namespace py = pybind11;
 using namespace OpenSoT::constraints::velocity;
 
+
+
+using CollisionAvoidanceC = OpenSoT::constraints::velocity::CollisionAvoidance;
+
+std::shared_ptr<CollisionAvoidanceC> make_collision_avoidance(
+    const XBot::ModelInterface& model, int max_pairs, 
+    std::string collision_urdf_str, std::string collision_srdf_str)
+{
+    urdf::ModelSharedPtr collision_urdf;
+    if(!collision_urdf_str.empty()){
+        collision_urdf = std::make_shared<urdf::Model>();
+        if(!collision_urdf->initString(collision_urdf_str)){
+            throw std::runtime_error("Failed to parse collision_urdf string");
+        }
+    }
+
+    srdf::ModelSharedPtr collision_srdf;
+    if(!collision_srdf_str.empty()){
+        collision_srdf = std::make_shared<srdf::Model>();
+        urdf::ModelConstSharedPtr active_urdf = collision_urdf ? collision_urdf : model.getUrdf();
+        if(!collision_srdf->initString(*active_urdf, collision_srdf_str)){
+            throw std::runtime_error("Failed to parse collision_srdf string");
+        }
+    }
+
+    return std::make_shared<CollisionAvoidanceC>(model, max_pairs, collision_urdf, collision_srdf);
+}
+
+
 void pyVelocityCollisionAvoidance(py::module& m) {
+    
+    using CollisionAvoidance = OpenSoT::constraints::velocity::CollisionAvoidance;
+
     py::class_<CollisionAvoidance, std::shared_ptr<CollisionAvoidance>, OpenSoT::Constraint<Eigen::MatrixXd, Eigen::VectorXd>>(m, "CollisionAvoidance")
-    .def(py::init<const XBot::ModelInterface&, int, urdf::ModelConstSharedPtr, srdf::ModelConstSharedPtr>(),
-         py::arg(), py::arg("max_pairs") = -1, py::arg("collision_urdf") = nullptr, py::arg("collision_srdf") = nullptr)
+        .def(py::init(&make_collision_avoidance),
+             py::arg(), py::arg("max_pairs") = -1, py::arg("collision_urdf") = "", py::arg("collision_srdf") = "")
+        .def("setInfeasiblePairWeight", &CollisionAvoidance::setInfeasiblePairWeight)
         .def("getLinkPairThreshold", &CollisionAvoidance::getLinkPairThreshold)
         .def("getDetectionThreshold", &CollisionAvoidance::getDetectionThreshold)
         .def("setLinkPairThreshold", &CollisionAvoidance::setLinkPairThreshold)
