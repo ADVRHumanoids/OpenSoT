fixed signs

bindings/python/examples/lipm_ms.py
@@ -71,7 +71,7 @@ for i in range(Ns):
     xdot0 = AffineHelper.pile(rdot, rddot)
 
     integration_ = euler(x0, xdot0, x1, dt)
-    integration.append(GenericTask(f"integration_{i}", integration_.getM(), integration_.getq()))
+    integration.append(GenericTask(f"integration_{i}", integration_.getM(), -integration_.getq()))
 
 
 integration_constraint = AggregatedTask(integration, variables.getSize())
@@ -79,11 +79,11 @@ integration_constraint = AggregatedTask(integration, variables.getSize())
 #plt.show()
 
 xinit = variables.getVariable("x0") + np.array([0., 0., 0, 0.])
-initial_state = GenericTask("initial_state", xinit.getM(), xinit.getq())
+initial_state = GenericTask("initial_state", xinit.getM(), -xinit.getq())
 
 zmp_tasks = list()
 for i in range(Ns):
-    min_ui = GenericTask("zmp_tracking", variables.getVariable(f"u{i}").getM(), variables.getVariable(f"u{i}").getq())
+    min_ui = GenericTask("zmp_tracking", variables.getVariable(f"u{i}").getM(), -variables.getVariable(f"u{i}").getq())
     min_ui.setWeight(1e1 * np.array([[1, 0], [0, 1]]))
     zmp_tasks.append(min_ui)
 zmp_tracking_task = AggregatedTask(zmp_tasks, variables.getSize())