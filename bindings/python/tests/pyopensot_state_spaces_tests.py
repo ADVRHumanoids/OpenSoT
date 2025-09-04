d1a11c70c06e6340cd9f228f3dbc3a4102990e23
vagtsiats <vagtsiats12@gmail.com>
2025-09-04 19:13:17 +0200

    Implemented quaternionSpace integrate() and added the "helping functions"

--- bindings/python/tests/pyopensot_state_spaces_tests.py
+++ bindings/python/tests/pyopensot_state_spaces_tests.py
@@ -52,3 +52,18 @@ x1 = R7.integrate(x, dx)
 print(f"integrated x: {x1}")
 utest.assertTrue((x1 == x+dx).all())
 
+
+# ------------------------
+
+
+SE3 = CompositeSpace([Quaternion, R3])
+
+x = np.array([1,0,0,0, 0,2,1])
+print(f"x: {x}")
+dx = np.array([0,1,0, 1,1,1])
+print(f"dx: {dx}")
+
+x1 = SE3.integrate(x, dx)
+print(f"integrated x: {x1}")
+
+#TODO - do proper testing
